cf87c1aa25d21ce1d2fee42ea4a9a372afb40577
Hooder <ahooder@protonmail.com>
2022-08-16 00:34:58 +0200

    okay this is enough

--- src/main/java/rs117/hd/data/materials/Overlay.java
+++ src/main/java/rs117/hd/data/materials/Overlay.java
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2021, 117 <https://twitter.com/117scape>
  * All rights reserved.
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *
