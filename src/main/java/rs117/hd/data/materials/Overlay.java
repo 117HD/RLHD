117HD/RLHD

commit c619b307188d0801663a3f584c9f0b5277632a0b

Overlay & Underlay cleanup (#39)
Co-authored-by: Hooder <ahooder@protonmail.com>

src/main/java/rs117/hd/data/materials/TileOverrideBuilder.java
@@ -1,0 +1,52 @@
+package rs117.hd.data.materials;
+
+import lombok.NonNull;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import rs117.hd.HdPluginConfig;
+import rs117.hd.data.WaterType;
+import rs117.hd.data.environments.Area;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+@Setter
+@Accessors(fluent = true)
+class TileOverrideBuilder<T> {
+    public Integer[] ids = {};
+    public Area area = Area.ALL;
+    public GroundMaterial groundMaterial = GroundMaterial.NONE;
+    public WaterType waterType = WaterType.NONE;
+    public boolean blended = true;
+    public boolean blendedAsType = false;
+    public int hue = -1;
+    public int shiftHue = 0;
+    public int saturation = -1;
+    public int shiftSaturation = 0;
+    public int lightness = -1;
+    public int shiftLightness = 0;
+    public T replacement;
+    public Function<HdPluginConfig, Boolean> replacementCondition;
+
+    TileOverrideBuilder<T> apply(Consumer<TileOverrideBuilder<T>> consumer) {
+        consumer.accept(this);
+        return this;
+    }
+
+    TileOverrideBuilder<T> ids(Integer... ids) {
+        this.ids = ids;
+        return this;
+    }
+
+    TileOverrideBuilder<T> waterType(WaterType waterType) {
+        this.waterType = waterType;
+        this.groundMaterial = waterType.getGroundMaterial();
+        return this;
+    }
+
+    TileOverrideBuilder<T> replaceWithIf(@NonNull T replacement, @NonNull Function<HdPluginConfig, Boolean> condition) {
+        this.replacement = replacement;
+        this.replacementCondition = condition;
+        return this;
+    }
+}

src/main/java/rs117/hd/model/ModelPusher.java
@@ -414,12 +414,10 @@ public class ModelPusher
                     int overlayId = client.getScene().getOverlayIds()[tileZ][tileX][tileY];
                     int underlayId = client.getScene().getUnderlayIds()[tileZ][tileX][tileY];
                     if (overlayId != 0) {
-                        Overlay overlay = Overlay.getOverlay(overlayId, tile, client);
-                        overlay = proceduralGenerator.getSeasonalOverlay(overlay);
+                        Overlay overlay = Overlay.getOverlay(overlayId, tile, client, config);
                         tileColorHSL = proceduralGenerator.recolorOverlay(overlay, tileColorHSL);
                     } else {
-                        Underlay underlay = Underlay.getUnderlay(underlayId, tile, client);
-                        underlay = proceduralGenerator.getSeasonalUnderlay(underlay);
+                        Underlay underlay = Underlay.getUnderlay(underlayId, tile, client,config);
                         tileColorHSL = proceduralGenerator.recolorUnderlay(underlay, tileColorHSL);
                     }
 
@@ -441,8 +439,7 @@ public class ModelPusher
                         tileColorHSL = HDUtils.colorIntToHSL(tile.getSceneTileModel().getTriangleColorA()[faceColorIndex]);
 
                         int underlayId = client.getScene().getUnderlayIds()[tileZ][tileX][tileY];
-                        Underlay underlay = Underlay.getUnderlay(underlayId, tile, client);
-                        underlay = proceduralGenerator.getSeasonalUnderlay(underlay);
+                        Underlay underlay = Underlay.getUnderlay(underlayId, tile, client,config);
                         tileColorHSL = proceduralGenerator.recolorUnderlay(underlay, tileColorHSL);
 
                         color1H = color2H = color3H = tileColorHSL[0];

commit e06dd1f2aad7fe14732430c7d1d99f7fcef88c55

Fix Overlay & Underlay bugs introduced in #39

src/main/java/rs117/hd/data/materials/Overlay.java
@@ -26,7 +26,6 @@ package rs117.hd.data.materials;
 
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.ListMultimap;
-import lombok.Getter;
 import net.runelite.api.Client;
 import net.runelite.api.Tile;
 import net.runelite.api.coords.LocalPoint;
@@ -39,7 +38,6 @@ import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
-@Getter
 public enum Overlay {
     // Tutorial Island
     TUTORIAL_ISLAND_KITCHEN_TILE_1(9, Area.TUTORIAL_ISLAND_KITCHEN, GroundMaterial.MARBLE_1_SEMIGLOSS, p -> p.blended(false)),
@@ -145,20 +143,20 @@ public enum Overlay {
     // Edgeville
     EDGEVILLE_BANK_TILE_1(3, Area.EDGEVILLE_BANK, GroundMaterial.MARBLE_2_GLOSS, p -> p
         .blended(false)
-        .blendedAsType(true)
+        .blendedAsOpposite(true)
         .lightness(22)),
     EDGEVILLE_BANK_TILE_2(4, Area.EDGEVILLE_BANK, GroundMaterial.MARBLE_2_GLOSS, p -> p
         .blended(false)
-        .blendedAsType(true)
+        .blendedAsOpposite(true)
         .lightness(30)),
     EDGEVILLE_BANK_TILING_FIX_1(10, Area.EDGEVILLE_BANK_TILING, GroundMaterial.MARBLE_2_GLOSS, p -> p
         .blended(false)
-        .blendedAsType(true)
+        .blendedAsOpposite(true)
         .lightness(22)),
     EDGEVILLE_BANK_PERIMETER_FIX(10, Area.EDGEVILLE_BANK_PERIMETER_FIX, GroundMaterial.MARBLE_2_GLOSS, p -> p.lightness(30)),
     EDGEVILLE_BANK_PERIMETER(10, Area.EDGEVILLE_BANK, GroundMaterial.MARBLE_2_GLOSS, p -> p
         .blended(false)
-        .blendedAsType(true)
+        .blendedAsOpposite(true)
         .lightness(30)),
     EDGEVILLE_BANK_SURROUNDING_PATH(10, Area.EDGEVILLE_BANK_SURROUNDING_PATH, GroundMaterial.VARROCK_PATHS),
     EDGEVILLE_DORIS_HOUSE_FLOOR(119, Area.EDGEVILLE_DORIS_HOUSE, GroundMaterial.TILE_SMALL),
@@ -300,15 +298,15 @@ public enum Overlay {
     FOSSIL_ISLAND_HILL_TEXTURE_FIX(11, Area.FOSSIL_ISLAND_HILL_TEXTURE_FIX, GroundMaterial.VARIED_DIRT),
 
     // Zeah
-    XERICS_LOOKOUT_TILE(50, Area.XERICS_LOOKOUT, GroundMaterial.TILES_2x2_2, p -> p.blended(false).ids(2)),
+    XERICS_LOOKOUT_TILE(Area.XERICS_LOOKOUT, GroundMaterial.TILES_2x2_2, p -> p.blended(false).ids(50, 2)),
 
     HOSIDIUS_STONE_FLOOR(123, Area.HOSIDIUS, GroundMaterial.FALADOR_PATHS),
     BLOOD_ALTAR_BLOOD(72, Area.BLOOD_ALTAR, WaterType.BLOOD),
-    SHAYZIEN_PAVED_AREA(2, Area.SHAYZIEN, GroundMaterial.GRAVEL, p -> p.blended(false).ids(-117)),
+    SHAYZIEN_PAVED_AREA(Area.SHAYZIEN, GroundMaterial.GRAVEL, p -> p.blended(false).ids(2, -117)),
 
-    SHAYZIEN_COMBAT_RING_FLOOR(30, Area.SHAYZIEN_COMBAT_RING, GroundMaterial.CARPET, p -> p
+    SHAYZIEN_COMBAT_RING_FLOOR(Area.SHAYZIEN_COMBAT_RING, GroundMaterial.CARPET, p -> p
         .blended(false)
-        .ids(37, 72, 73)),
+        .ids(30, 37, 72, 73)),
 
     MESS_HALL_KITCHEN_TILE_1(30, Area.MESS_HALL_KITCHEN, GroundMaterial.MARBLE_1_SEMIGLOSS, p -> p.blended(false)),
     MESS_HALL_KITCHEN_TILE_2(99, Area.MESS_HALL_KITCHEN, GroundMaterial.MARBLE_2_SEMIGLOSS, p -> p.blended(false)),
@@ -336,7 +334,7 @@ public enum Overlay {
 
     // Mind Altar
     MIND_ALTAR_TILE_1(3, Area.MIND_ALTAR, GroundMaterial.MARBLE_1_SEMIGLOSS, p -> p.blended(false)),
-    MIND_ALTAR_TILE_4(4, Area.MIND_ALTAR, GroundMaterial.MARBLE_2_SEMIGLOSS, p -> p.blended(false).ids(10)),
+    MIND_ALTAR_TILE_4(Area.MIND_ALTAR, GroundMaterial.MARBLE_2_SEMIGLOSS, p -> p.blended(false).ids(4, 10)),
 
     // Dragon Slayer II
     DS2_SHIPS_WATER(6, Area.DS2_SHIPS, WaterType.WATER_FLAT),
@@ -353,17 +351,17 @@ public enum Overlay {
     BA_WAITING_ROOM_NUMBERS(89, Area.BARBARIAN_ASSAULT_WAITING_ROOMS, GroundMaterial.DIRT, p -> p.blended(false)),
 
     // POHs
-    POH_DESERT_INDOORS(26, Area.PLAYER_OWNED_HOUSE, GroundMaterial.TILES_2x2_2, p -> p.blended(false).ids(99)),
+    POH_DESERT_INDOORS(Area.PLAYER_OWNED_HOUSE, GroundMaterial.TILES_2x2_2, p -> p.blended(false).ids(26, 99)),
 
     // Random events
     PRISON_PETE_TILE_1(2, Area.RANDOM_EVENT_PRISON_PETE, GroundMaterial.MARBLE_1, p -> p.blended(false)),
     PRISON_PETE_TILE_2(-125, Area.RANDOM_EVENT_PRISON_PETE, GroundMaterial.MARBLE_2, p -> p.blended(false)),
 
     // GOTR Entrance fix
-    TEMPLE_OF_THE_EYE_ENTRANCE(0, Area.TEMPLE_OF_THE_EYE_ENTRANCE_FIX, GroundMaterial.DIRT, p -> p
+    TEMPLE_OF_THE_EYE_ENTRANCE(Area.TEMPLE_OF_THE_EYE_ENTRANCE_FIX, GroundMaterial.DIRT, p -> p
         .shiftLightness(-10)
         .blended(false)
-        .ids(-5)),
+        .ids(-53, 0)),
 
     // Elid Cave fix
     ELID_CAVE_WATER_FIX(-126, Area.ELID_CAVE, WaterType.WATER),
@@ -377,17 +375,17 @@ public enum Overlay {
     ANCIENT_CAVERN_UPPER_WATER(41, Area.ANCIENT_CAVERN_UPPER, WaterType.WATER_FLAT),
 
     // default overlays
-    OVERLAY_WATER(-128, WaterType.WATER, p -> p.ids(-105, -98, 6, 41, 104)),
-    OVERLAY_DIRT(-124, GroundMaterial.DIRT, p -> p.ids(-84, -83, 14, 15, 21, 22, 23, 60, 77, 81, 82, 88, 89, 101, 102, 107, 108, 110, 115, 123)),
-    OVERLAY_GRAVEL(-76, GroundMaterial.GRAVEL, p -> p.ids(2, 3, 4, 6, 8, 10, 119)),
-    OVERLAY_VARROCK_PATHS(-85, GroundMaterial.VARROCK_PATHS, p -> p.ids(-77, 11)),
-    OVERLAY_SWAMP_WATER(-100, WaterType.SWAMP_WATER, p -> p.ids(7)),
-    OVERLAY_WOOD_PLANKS(5, GroundMaterial.WOOD_PLANKS_1, p -> p.ids(35, 52)),
-    OVERLAY_SAND(25, GroundMaterial.SAND, p -> p.ids(26)),
-    OVERLAY_BRICK_BROWN(27, GroundMaterial.SAND, p -> p.ids(46).blended(false)),
-    OVERLAY_SNOW(30, GroundMaterial.SNOW_2, p -> p.ids(33)),
-    OVERLAY_VARIED_DIRT(49, GroundMaterial.VARIED_DIRT, p -> p.ids(83)),
-    OVERLAY_SAND_BRICK(-49, GroundMaterial.SAND_BRICK, p -> p.ids(84)),
+    OVERLAY_WATER(WaterType.WATER, p -> p.ids(-128, -105, -98, 6, 41, 104)),
+    OVERLAY_DIRT(GroundMaterial.DIRT, p -> p.ids(-124, -84, -83, 14, 15, 21, 22, 23, 60, 77, 81, 82, 88, 89, 101, 102, 107, 108, 110, 115, 123)),
+    OVERLAY_GRAVEL(GroundMaterial.GRAVEL, p -> p.ids(-76, 2, 3, 4, 6, 8, 10, 119)),
+    OVERLAY_VARROCK_PATHS(GroundMaterial.VARROCK_PATHS, p -> p.ids(-85, -77, 11)),
+    OVERLAY_SWAMP_WATER(WaterType.SWAMP_WATER, p -> p.ids(-100, 7)),
+    OVERLAY_WOOD_PLANKS(GroundMaterial.WOOD_PLANKS_1, p -> p.ids(5, 35, 52)),
+    OVERLAY_SAND(GroundMaterial.SAND, p -> p.ids(25, 26)),
+    OVERLAY_BRICK_BROWN(GroundMaterial.SAND, p -> p.ids(27, 46).blended(false)),
+    OVERLAY_SNOW(GroundMaterial.SNOW_2, p -> p.ids(30, 33)),
+    OVERLAY_VARIED_DIRT(GroundMaterial.VARIED_DIRT, p -> p.ids(49, 83)),
+    OVERLAY_SAND_BRICK(GroundMaterial.SAND_BRICK, p -> p.ids(-49, 84)),
     OVERLAY_N122(-122, GroundMaterial.TILES_2x2_2_GLOSS),
     OVERLAY_N119(-119, GroundMaterial.FALADOR_PATHS),
     OVERLAY_N93(-93, GroundMaterial.CARPET),
@@ -407,7 +405,7 @@ public enum Overlay {
     public final GroundMaterial groundMaterial;
     public final WaterType waterType;
     public final boolean blended;
-    public final boolean blendedAsOverlay;
+    public final boolean blendedAsUnderlay;
     public final int hue;
     public final int shiftHue;
     public final int saturation;
@@ -417,14 +415,6 @@ public enum Overlay {
     public final Overlay replacementOverlay;
     public final Function<HdPluginConfig, Boolean> replacementCondition;
 
-    Overlay(int id, WaterType waterType, Consumer<TileOverrideBuilder<Overlay>> consumer) {
-        this(p -> p.ids(id).waterType(waterType).blended(false).apply(consumer));
-    }
-
-    Overlay(int id, Area area, WaterType waterType) {
-        this(p -> p.ids(id).waterType(waterType).area(area).blended(false));
-    }
-
     Overlay(int id, GroundMaterial material) {
         this(p -> p.ids(id).groundMaterial(material));
     }
@@ -433,12 +423,28 @@ public enum Overlay {
         this(p -> p.ids(id).groundMaterial(material).area(area));
     }
 
+    Overlay(int id, Area area, WaterType waterType) {
+        this(p -> p.ids(id).waterType(waterType).area(area).blended(false));
+    }
+
     Overlay(int id, GroundMaterial material, Consumer<TileOverrideBuilder<Overlay>> consumer) {
         this(p -> p.ids(id).groundMaterial(material).apply(consumer));
     }
 
     Overlay(int id, Area area, GroundMaterial material, Consumer<TileOverrideBuilder<Overlay>> consumer) {
         this(p -> p.ids(id).groundMaterial(material).area(area).apply(consumer));
+    }
+
+    Overlay(GroundMaterial material, Consumer<TileOverrideBuilder<Overlay>> consumer) {
+        this(p -> p.groundMaterial(material).apply(consumer));
+    }
+
+    Overlay(WaterType waterType, Consumer<TileOverrideBuilder<Overlay>> consumer) {
+        this(p -> p.waterType(waterType).blended(false).apply(consumer));
+    }
+
+    Overlay(Area area, GroundMaterial material, Consumer<TileOverrideBuilder<Overlay>> consumer) {
+        this(p -> p.groundMaterial(material).area(area).apply(consumer));
     }
 
     Overlay(Consumer<TileOverrideBuilder<Overlay>> consumer) {
@@ -451,7 +457,7 @@ public enum Overlay {
         this.groundMaterial = builder.groundMaterial;
         this.area = builder.area;
         this.blended = builder.blended;
-        this.blendedAsOverlay = builder.blendedAsType;
+        this.blendedAsUnderlay = builder.blendedAsOpposite;
         this.hue = builder.hue;
         this.shiftHue = builder.shiftHue;
         this.saturation = builder.saturation;

src/main/java/rs117/hd/data/materials/TileOverrideBuilder.java
@@ -7,6 +7,7 @@ import rs117.hd.HdPluginConfig;
 import rs117.hd.data.WaterType;
 import rs117.hd.data.environments.Area;
 
+import java.util.Arrays;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
@@ -18,7 +19,7 @@ class TileOverrideBuilder<T> {
     public GroundMaterial groundMaterial = GroundMaterial.NONE;
     public WaterType waterType = WaterType.NONE;
     public boolean blended = true;
-    public boolean blendedAsType = false;
+    public boolean blendedAsOpposite = false;
     public int hue = -1;
     public int shiftHue = 0;
     public int saturation = -1;
@@ -34,6 +35,12 @@ class TileOverrideBuilder<T> {
     }
 
     TileOverrideBuilder<T> ids(Integer... ids) {
+        if (this.ids.length > 0)
+            throw new IllegalStateException(
+                "Attempted to overwrite IDs " + Arrays.toString(this.ids) +
+                " with IDs " + Arrays.toString(ids) +
+                " in " + TileOverrideBuilder.class.getSimpleName() + "." +
+                "This is likely a mistake.");
         this.ids = ids;
         return this;
     }

src/main/java/rs117/hd/data/materials/Underlay.java
@@ -39,26 +39,25 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 
 public enum Underlay {
-
     // Default
     // Lumbridge
     LUMBRIDGE_CASTLE_TILE(56, Area.LUMBRIDGE_CASTLE_BASEMENT, GroundMaterial.MARBLE_2_SEMIGLOSS, p -> p.blended(false)),
 
     // Edgeville
-    EDGEVILLE_PATH_OVERLAY_48(48, Area.EDGEVILLE_PATH_OVERLAY, GroundMaterial.VARROCK_PATHS_LIGHT, p -> p
-        .blendedAsType(true)
+    EDGEVILLE_PATH_OVERLAY_48(Area.EDGEVILLE_PATH_OVERLAY, GroundMaterial.VARROCK_PATHS_LIGHT, p -> p
+        .blendedAsOpposite(true)
         .hue(0)
         .shiftLightness(8)
         .saturation(0)
-        .ids(50, 64)),
+        .ids(48, 50, 64)),
 
     // Varrock
     VARROCK_JULIETS_HOUSE_UPSTAIRS(8, Area.VARROCK_JULIETS_HOUSE, GroundMaterial.NONE, p -> p.blended(false)),
     // A Soul's Bane
-    TOLNA_DUNGEON_ANGER_FLOOR(58, Area.TOLNA_DUNGEON_ANGER, GroundMaterial.DIRT, p -> p.ids(58)),
+    TOLNA_DUNGEON_ANGER_FLOOR(Area.TOLNA_DUNGEON_ANGER, GroundMaterial.DIRT, p -> p.ids(58, 58)),
 
     // Burthorpe
-    WARRIORS_GUILD_FLOOR_1(55, Area.WARRIORS_GUILD, GroundMaterial.VARROCK_PATHS, p -> p.ids(56)),
+    WARRIORS_GUILD_FLOOR_1(Area.WARRIORS_GUILD, GroundMaterial.VARROCK_PATHS, p -> p.ids(55, 56)),
 
     // Catherby
     CATHERBY_BEACH_SAND(62, Area.CATHERBY, GroundMaterial.SAND),
@@ -66,10 +65,10 @@ public enum Underlay {
     // Al Kharid
     MAGE_TRAINING_ARENA_FLOOR_PATTERN(56, Area.MAGE_TRAINING_ARENA, GroundMaterial.TILES_2x2_2_GLOSS, p -> p.blended(false)),
 
-    KHARID_SAND_1(61, Area.KHARID_DESERT_REGION, GroundMaterial.SAND, p -> p
+    KHARID_SAND_1(Area.KHARID_DESERT_REGION, GroundMaterial.SAND, p -> p
         .saturation(3)
         .hue(6)
-        .ids(62, 67, 68, -127, 126, 49, 58, 63, 64, 50)),
+        .ids(61, 62, 67, 68, -127, 126, 49, 58, 63, 64, 50)),
 
     // Burthorpe games room
     GAMES_ROOM_INNER_FLOOR(64, Area.GAMES_ROOM_INNER, GroundMaterial.CARPET, p -> p.blended(false)),
@@ -79,10 +78,10 @@ public enum Underlay {
     CRANDOR_SAND(-110, Area.CRANDOR, GroundMaterial.SAND, p -> p.saturation(3).hue(6)),
 
     // God Wars Dungeon (GWD)
-    GOD_WARS_DUNGEON_SNOW_1(58, Area.GOD_WARS_DUNGEON, GroundMaterial.SNOW_1, p -> p.ids(59)),
+    GOD_WARS_DUNGEON_SNOW_1(Area.GOD_WARS_DUNGEON, GroundMaterial.SNOW_1, p -> p.ids(58, 59)),
 
     // TzHaar
-    INFERNO_1(-118, Area.THE_INFERNO, GroundMaterial.VARIED_DIRT, p -> p.ids(61, -115, -111, -110, 1, 61, 62, 72, 118, 122)),
+    INFERNO_1(Area.THE_INFERNO, GroundMaterial.VARIED_DIRT, p -> p.ids(-118, 61, -115, -111, -110, 1, 61, 62, 72, 118, 122)),
 
     TZHAAR(72, Area.TZHAAR, GroundMaterial.VARIED_DIRT_SHINY, p -> p.shiftLightness(2)),
 
@@ -99,10 +98,10 @@ public enum Underlay {
         .shiftLightness(3)),
 
     // Zanaris
-    COSMIC_ENTITYS_PLANE_ABYSS(72, Area.COSMIC_ENTITYS_PLANE, GroundMaterial.NONE, p -> p
+    COSMIC_ENTITYS_PLANE_ABYSS(Area.COSMIC_ENTITYS_PLANE, GroundMaterial.NONE, p -> p
         .lightness(0)
         .blended(false)
-        .ids(2)),
+        .ids(2, 72)),
 
     // Death's office
     DEATHS_OFFICE_TILE(-110, Area.DEATHS_OFFICE, GroundMaterial.TILES_2x2_1_SEMIGLOSS),
@@ -115,30 +114,30 @@ public enum Underlay {
     MIND_ALTAR_TILE(55, Area.MIND_ALTAR, GroundMaterial.MARBLE_1_SEMIGLOSS, p -> p.blended(false)),
 
     // Cutscenes
-    CANOE_CUTSCENE_GRASS_1(48, Area.CANOE_CUTSCENE, GroundMaterial.GRASS_SCROLLING, p -> p.ids(50, 63)),
+    CANOE_CUTSCENE_GRASS_1(Area.CANOE_CUTSCENE, GroundMaterial.GRASS_SCROLLING, p -> p.ids(48, 50, 63)),
 
     WINTER_GRASS(-999, GroundMaterial.SNOW_1, p -> p.hue(0).saturation(0).shiftLightness(40).blended(true)),
     WINTER_DIRT(-999, GroundMaterial.DIRT, p -> p.hue(0).saturation(0).shiftLightness(40).blended(true)),
 
     // default underlays
 
-    OVERWORLD_UNDERLAY_GRASS(10, Area.OVERWORLD, GroundMaterial.OVERWORLD_GRASS_1, p -> p
-        .ids(25, 33, 34, 40, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 62, 63, 67, 70, 75, 93, 96, 97, 103, 114, 115, 126)
+    OVERWORLD_UNDERLAY_GRASS(Area.OVERWORLD, GroundMaterial.OVERWORLD_GRASS_1, p -> p
+        .ids(10, 25, 33, 34, 40, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 62, 63, 67, 70, 75, 93, 96, 97, 103, 114, 115, 126)
         .replaceWithIf(WINTER_GRASS, HdPluginConfig::winterTheme)),
 
-    OVERWORLD_UNDERLAY_DIRT(-111, Area.OVERWORLD, GroundMaterial.OVERWORLD_DIRT, p -> p
-        .ids(-110, 64, 65, 66, 80, 92, 94)
+    OVERWORLD_UNDERLAY_DIRT(Area.OVERWORLD, GroundMaterial.OVERWORLD_DIRT, p -> p
+        .ids(-111, -110, 64, 65, 66, 80, 92, 94)
         .replaceWithIf(WINTER_DIRT, HdPluginConfig::winterTheme)),
 
-    OVERWORLD_UNDERLAY_SAND(-127, GroundMaterial.SAND, p -> p.ids(-118, 61, 68)),
+    OVERWORLD_UNDERLAY_SAND(GroundMaterial.SAND, p -> p.ids(-127, -118, 61, 68)),
 
-    OVERWORLD_DIRT(-111, GroundMaterial.DIRT, p -> p.ids(-110, 64, 66, 80, 92, 94)),
+    OVERWORLD_DIRT(GroundMaterial.DIRT, p -> p.ids(-111, -110, 64, 66, 80, 92, 94)),
 
-    UNDERLAY_10(10, GroundMaterial.GRASS_1, p -> p.ids(25, 33, 34, 40, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 62, 63, 67, 70, 75, 93, 96, 97, 103, 103, 114, 115, 126)),
+    UNDERLAY_10(GroundMaterial.GRASS_1, p -> p.ids(10, 25, 33, 34, 40, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 62, 63, 67, 70, 75, 93, 96, 97, 103, 103, 114, 115, 126)),
 
     UNDERLAY_58(58, GroundMaterial.SNOW_1),
 
-    UNDERLAY_72(72, GroundMaterial.VARIED_DIRT, p -> p.ids(90)),
+    UNDERLAY_72(GroundMaterial.VARIED_DIRT, p -> p.ids(72, 90)),
 
     NONE(-1, GroundMaterial.DIRT);
 
@@ -147,7 +146,7 @@ public enum Underlay {
     public final GroundMaterial groundMaterial;
     public final WaterType waterType;
     public final boolean blended;
-    public final boolean blendedAsUnderlay;
+    public final boolean blendedAsOverlay;
     public final int hue;
     public final int shiftHue;
     public final int saturation;
@@ -161,20 +160,28 @@ public enum Underlay {
         this(p -> p.ids(id).groundMaterial(material));
     }
 
-    Underlay(int id, GroundMaterial material, Consumer<TileOverrideBuilder<Underlay>> consumer) {
-        this(p -> p.ids(id).groundMaterial(material).apply(consumer));
+    Underlay(int id, Area area, GroundMaterial material) {
+        this(p -> p.ids(id).groundMaterial(material).area(area));
     }
 
     Underlay(int id, Consumer<TileOverrideBuilder<Underlay>> consumer) {
         this(p -> p.ids(id).apply(consumer));
     }
 
+    Underlay(int id, GroundMaterial material, Consumer<TileOverrideBuilder<Underlay>> consumer) {
+        this(p -> p.ids(id).groundMaterial(material).apply(consumer));
+    }
+
     Underlay(int id, Area area, GroundMaterial material, Consumer<TileOverrideBuilder<Underlay>> consumer) {
         this(p -> p.ids(id).groundMaterial(material).area(area).apply(consumer));
     }
 
-    Underlay(int id, Area area, GroundMaterial material) {
-        this(p -> p.ids(id).groundMaterial(material).area(area));
+    Underlay(GroundMaterial material, Consumer<TileOverrideBuilder<Underlay>> consumer) {
+        this(p -> p.groundMaterial(material).apply(consumer));
+    }
+
+    Underlay(Area area, GroundMaterial material, Consumer<TileOverrideBuilder<Underlay>> consumer) {
+        this(p -> p.groundMaterial(material).area(area).apply(consumer));
     }
 
     Underlay(Consumer<TileOverrideBuilder<Underlay>> consumer) {
@@ -187,7 +194,7 @@ public enum Underlay {
         this.groundMaterial = builder.groundMaterial;
         this.area = builder.area;
         this.blended = builder.blended;
-        this.blendedAsUnderlay = builder.blendedAsType;
+        this.blendedAsOverlay = builder.blendedAsOpposite;
         this.hue = builder.hue;
         this.shiftHue = builder.shiftHue;
         this.saturation = builder.saturation;

src/main/java/rs117/hd/model/ModelPusher.java
@@ -417,7 +417,7 @@ public class ModelPusher
                         Overlay overlay = Overlay.getOverlay(overlayId, tile, client, config);
                         tileColorHSL = proceduralGenerator.recolorOverlay(overlay, tileColorHSL);
                     } else {
-                        Underlay underlay = Underlay.getUnderlay(underlayId, tile, client,config);
+                        Underlay underlay = Underlay.getUnderlay(underlayId, tile, client, config);
                         tileColorHSL = proceduralGenerator.recolorUnderlay(underlay, tileColorHSL);
                     }
 
@@ -439,7 +439,7 @@ public class ModelPusher
                         tileColorHSL = HDUtils.colorIntToHSL(tile.getSceneTileModel().getTriangleColorA()[faceColorIndex]);
 
                         int underlayId = client.getScene().getUnderlayIds()[tileZ][tileX][tileY];
-                        Underlay underlay = Underlay.getUnderlay(underlayId, tile, client,config);
+                        Underlay underlay = Underlay.getUnderlay(underlayId, tile, client, config);
                         tileColorHSL = proceduralGenerator.recolorUnderlay(underlay, tileColorHSL);
 
                         color1H = color2H = color3H = tileColorHSL[0];

src/main/java/rs117/hd/scene/ProceduralGenerator.java
@@ -294,9 +294,9 @@ public class ProceduralGenerator
 			if (vertexOverlays[vertex] != 0)
 			{
 				Overlay overlay = Overlay.getOverlay(vertexOverlays[vertex], tile, client, config);
-				GroundMaterial groundMaterial = overlay.getGroundMaterial();
+				GroundMaterial groundMaterial = overlay.groundMaterial;
 				material = groundMaterial.getRandomMaterial(z, worldX, worldY);
-				isOverlay = !overlay.blendedAsOverlay;
+				isOverlay = !overlay.blendedAsUnderlay;
 				colorHSL = recolorOverlay(overlay, colorHSL);
 			}
 			else if (vertexUnderlays[vertex] != 0)
@@ -304,7 +304,7 @@ public class ProceduralGenerator
 				Underlay underlay = Underlay.getUnderlay(vertexUnderlays[vertex], tile, client, config);
 				GroundMaterial groundMaterial = underlay.groundMaterial;
 				material = groundMaterial.getRandomMaterial(z, worldX, worldY);
-				isOverlay = underlay.blendedAsUnderlay;
+				isOverlay = underlay.blendedAsOverlay;
 				colorHSL = recolorUnderlay(underlay, colorHSL);
 			}
 
@@ -845,7 +845,7 @@ public class ProceduralGenerator
 		{
 			if (client.getScene().getOverlayIds()[tileZ][tileX][tileY] != 0)
 			{
-				waterType = Overlay.getOverlay(client.getScene().getOverlayIds()[tileZ][tileX][tileY], tile, client, config).getWaterType();
+				waterType = Overlay.getOverlay(client.getScene().getOverlayIds()[tileZ][tileX][tileY], tile, client, config).waterType;
 			}
 			else
 			{
@@ -881,7 +881,7 @@ public class ProceduralGenerator
 		{
 			if (isOverlayFace(tile, face))
 			{
-				waterType = Overlay.getOverlay(client.getScene().getOverlayIds()[tileZ][tileX][tileY], tile, client, config).getWaterType();
+				waterType = Overlay.getOverlay(client.getScene().getOverlayIds()[tileZ][tileX][tileY], tile, client, config).waterType;
 			}
 			else
 			{
@@ -1079,16 +1079,16 @@ public class ProceduralGenerator
 
 	public int[] recolorOverlay(Overlay overlay, int[] colorHSL)
 	{
-		colorHSL[0] = overlay.getHue() >= 0 ? overlay.getHue() : colorHSL[0];
-		colorHSL[0] += overlay.getShiftHue();
+		colorHSL[0] = overlay.hue >= 0 ? overlay.hue : colorHSL[0];
+		colorHSL[0] += overlay.shiftHue;
 		colorHSL[0] = Ints.constrainToRange(colorHSL[0], 0, 63);
 
-		colorHSL[1] = overlay.getSaturation() >= 0 ? overlay.getSaturation() : colorHSL[1];
-		colorHSL[1] += overlay.getShiftSaturation();
+		colorHSL[1] = overlay.saturation >= 0 ? overlay.saturation : colorHSL[1];
+		colorHSL[1] += overlay.shiftSaturation;
 		colorHSL[1] = Ints.constrainToRange(colorHSL[1], 0, 7);
 
-		colorHSL[2] = overlay.getLightness() >= 0 ? overlay.getLightness() : colorHSL[2];
-		colorHSL[2] += overlay.getShiftLightness();
+		colorHSL[2] = overlay.lightness >= 0 ? overlay.lightness : colorHSL[2];
+		colorHSL[2] += overlay.shiftLightness;
 		colorHSL[2] = Ints.constrainToRange(colorHSL[2], 0, 127);
 
 		return colorHSL;
@@ -1126,7 +1126,7 @@ public class ProceduralGenerator
 
 		if (client.getScene().getOverlayIds()[z][x][y] != 0)
 		{
-			if (!Overlay.getOverlay(client.getScene().getOverlayIds()[z][x][y], tile, client, config).isBlended())
+			if (!Overlay.getOverlay(client.getScene().getOverlayIds()[z][x][y], tile, client, config).blended)
 			{
 				return true;
 			}

src/main/java/rs117/hd/scene/SceneUploader.java
@@ -411,7 +411,7 @@ class SceneUploader
 				if (client.getScene().getOverlayIds()[tileZ][tileX][tileY] != 0)
 				{
 					Overlay overlay = Overlay.getOverlay(client.getScene().getOverlayIds()[tileZ][tileX][tileY], tile, client, config);
-					groundMaterial = overlay.getGroundMaterial();
+					groundMaterial = overlay.groundMaterial;
 
 					swColor = HDUtils.colorHSLToInt(proceduralGenerator.recolorOverlay(overlay, HDUtils.colorIntToHSL(swColor)));
 					seColor = HDUtils.colorHSLToInt(proceduralGenerator.recolorOverlay(overlay, HDUtils.colorIntToHSL(seColor)));
@@ -782,7 +782,7 @@ class SceneUploader
 				if (proceduralGenerator.isOverlayFace(tile, face))
 				{
 					Overlay overlay = Overlay.getOverlay(client.getScene().getOverlayIds()[tileZ][tileX][tileY], tile, client, config);
-					groundMaterial = overlay.getGroundMaterial();
+					groundMaterial = overlay.groundMaterial;
 
 					colorA = HDUtils.colorHSLToInt(proceduralGenerator.recolorOverlay(overlay, HDUtils.colorIntToHSL(colorA)));
 					colorB = HDUtils.colorHSLToInt(proceduralGenerator.recolorOverlay(overlay, HDUtils.colorIntToHSL(colorB)));