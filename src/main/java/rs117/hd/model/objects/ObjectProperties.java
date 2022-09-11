117HD/RLHD

commit 799e601e76f7a1cf789f6f1b8ec403fe710c7cd5

Zeah tile blending fixes (#56)
* Kourend Castle Entrance Blend Fix

Fixes the bad blending at the entrance of Kourend Castle

* Great Statue Blend Fix

Fixes bad blending around the Great Statue of Kourend

* Hosidius Well Blending Fix

* Hosidius floor texture

changes the hosidius floor back to the osrs original; was a good look i though and mixes up the textures for the world as a whole

* Hosidius stairs blending fix

* Kourend paths, walls, and steps

* Shayzien path fix

src/main/java/rs117/hd/model/objects/ObjectProperties.java
@@ -49,6 +49,8 @@ public enum ObjectProperties
 
 	// Wooden Fences
 	WOODEN_FENCES(Material.WOOD_GRAIN,new Properties().setFlatNormals(true),  814, 980, 981, 991, 992,993, 1007, 1008, 1558, 1559, 1560,1561,1562,1563,1564, 1565, 1566, 1567, 1739, 1740, 5432, 5433,5434,5435,5436,5437, 5438, 7055,7527, 9511, 9623),
+	// Stone
+	STONE_STEPS(Material.GRUNGE_2, 3684),
 
 	// Lumbridge
 	LUMBRIDGE_CASTLE_WALLS(Material.NONE, new Properties().setFlatNormals(true), 1651, 1911, 1912, 1913),
@@ -99,6 +101,9 @@ public enum ObjectProperties
 	APE_ATOLL_DUNGEON_WALLS(Material.GRUNGE_1, 4898),
 
 	// Zeah
+	KOUREND_PATHS(Material.GRUNGE_1, 13705, 13706, 13707, 13708, 13709, 13710, 13711, 13712, 13713, 13714, 13715, 13716, 1371742722),
+	KOUREND_PATH_STEPS(Material.GRUNGE_2, 30599, 30600),
+	KOUREND_WALLS(Material.GRUNGE_1,  new Properties().setFlatNormals(true), 23800, 23801, 23802, 23803, 23804, 23805, 23806, 23807, 23808, 23809, 23810),
 	SHAYZIEN_SHAY_SHRINE(Material.MARBLE_3_GLOSS, CAT_SHRINE),
 	SHAYZIEN_HERO_STATUE(Material.GRUNGE_1, DRAGON_STATUE),
 	SHAYZIEN_DRAGON_STATUE(Material.MARBLE_2_GLOSS, SHAYZIEN_STATUE_42178),
@@ -107,6 +112,7 @@ public enum ObjectProperties
 	SHAYZIEN_WALLS_ROCK(Material.ROCK_1, new Properties().setFlatNormals(true), 14543, 14544, 14545, 14546, 14547, 14548, 14549, 14550, 14551, 14558, 42110, 42111, 42116),
 	SHAYZIEN_WALLS_WOOD(Material.GRUNGE_1, new Properties().setFlatNormals(true), 42107, 42108, 42109, 42112, 42113),
 	SHAYZIEN_STAIRS(Material.GRUNGE_1, new Properties().setFlatNormals(true), 42193, 42195, 42196),
+	MOUNT_KARUULM_WALLS(Material.NONE,  new Properties().setFlatNormals(true), 34347, 34384, 34385, 34386, 34387, 34388, 34389, 34390, 34391, 34392, 34393, 34394, 34395),
 	STATUE_BUST(Material.MARBLE_1_GLOSS, 26129),
 
 	// Castle Wars

src/main/java/rs117/hd/data/environments/Area.java
@@ -699,6 +699,7 @@ public enum Area
 	),
 	SHAYZIEN_SHAYZIA_RUIN(1577, 3587, 1594, 3604),
 	SHAYZIEN_GRAVEYARD_OF_HEROES(1472, 3583, 1515, 3541),
+	SHAYZIEN_EAST_ENTRANCE_BLEND_FIX(1556, 3577, 1556, 3575),
 	SHAYZIEN(
 		new AABB(1467, 3678, 1540, 3607), // encampment
 		new AABB(1540, 3613, 1559, 3650), // encampment
@@ -827,6 +828,14 @@ public enum Area
 	MM2_AIRSHIP_PLATFORM(2056, 5375, 2109, 5442),
 	APE_ATOLL(2685, 2819, 2819, 2684),
 
+
+	// Zeah
+	KOUREND_CASTLE_ENTRANCE_FIX(1623, 3677, 1623, 3669),
+	GREAT_KOUREND_STATUE(1641, 3678, 1631, 3668),
+	HOSIDIUS_WELL(1764, 3600, 1761, 3597),
+	HOSIDIUS_STAIRS(
+		new AABB(1763, 3608, 1762, 3607)
+	),
 	// Fishing Trawler
 	FISHING_TRAWLER_BOAT_PORT_KHAZARD(2669, 3183, 2673, 3166),
 	FISHING_TRAWLER_BOAT_FLOODED(2012, 4826, 2021, 4824),

src/main/java/rs117/hd/data/materials/GroundMaterial.java
@@ -70,6 +70,7 @@ public enum GroundMaterial
 	UNDERWATER_GENERIC(Material.DIRT_1, Material.DIRT_2),
 
 	WOOD_PLANKS_1(Material.WOOD_PLANKS_1),
+	CLEAN_WOOD_FLOOR(Material.CLEAN_WOOD_FLOOR),
 
 	HD_LAVA(Material.HD_LAVA_1, Material.HD_LAVA_2, Material.HD_LAVA_1, Material.HD_LAVA_1, Material.HD_LAVA_2, Material.HD_MAGMA_1, Material.HD_MAGMA_2),
 

src/main/java/rs117/hd/data/materials/Overlay.java
@@ -298,6 +298,13 @@ public enum Overlay {
     FOSSIL_ISLAND_HILL_TEXTURE_FIX(11, Area.FOSSIL_ISLAND_HILL_TEXTURE_FIX, GroundMaterial.VARIED_DIRT),
 
     // Zeah
+    // Great Kourend
+    KOUREND_CASTLE_BLEND_FIX(11, Area.KOUREND_CASTLE_ENTRANCE_FIX, GroundMaterial.VARROCK_PATHS, p -> p.blended(false)),
+    KOUREND_GREAT_STATUE_BLEND_FIX_1(108, Area.GREAT_KOUREND_STATUE, GroundMaterial.GRASS_1, p -> p.blended(true)),
+    KOUREND_GREAT_STATUE_BLEND_FIX_2(11, Area.GREAT_KOUREND_STATUE, GroundMaterial.GRASS_1, p -> p.blended(false)),
+    HOSIDIUS_WELL_BLEND_FIX(-119, Area.HOSIDIUS_WELL, GroundMaterial.FALADOR_PATHS, p -> p.blended(false)),
+    HOSIDIUS_STAIRS_BLEND_FIX(-119, Area.HOSIDIUS_STAIRS, GroundMaterial.FALADOR_PATHS, p -> p.blended(false)),
+    SHAYZIEN_EAST_PATH_FIX(11, Area.SHAYZIEN_EAST_ENTRANCE_BLEND_FIX, GroundMaterial.VARROCK_PATHS, p -> p.blended(false)),
     XERICS_LOOKOUT_TILE(Area.XERICS_LOOKOUT, GroundMaterial.TILES_2x2_2, p -> p.blended(false).ids(50, 2)),
 
     HOSIDIUS_STONE_FLOOR(123, Area.HOSIDIUS, GroundMaterial.FALADOR_PATHS),
@@ -380,7 +387,8 @@ public enum Overlay {
     OVERLAY_GRAVEL(GroundMaterial.GRAVEL, p -> p.ids(-76, 2, 3, 4, 6, 8, 10, 119)),
     OVERLAY_VARROCK_PATHS(GroundMaterial.VARROCK_PATHS, p -> p.ids(-85, -77, 11)),
     OVERLAY_SWAMP_WATER(WaterType.SWAMP_WATER, p -> p.ids(-100, 7)),
-    OVERLAY_WOOD_PLANKS(GroundMaterial.WOOD_PLANKS_1, p -> p.ids(5, 35, 52)),
+    OVERLAY_WOOD_PLANKS(GroundMaterial.WOOD_PLANKS_1, p -> p.ids(5, 35)),
+    OVERLAY_CLEAN_WOOD_PLANKS(GroundMaterial.CLEAN_WOOD_FLOOR, p -> p.ids(52).shiftLightness(-4)),
     OVERLAY_SAND(GroundMaterial.SAND, p -> p.ids(25, 26)),
     OVERLAY_BRICK_BROWN(GroundMaterial.BRICK_BROWN, p -> p.ids(27, 46).blended(false)),
     OVERLAY_SNOW(GroundMaterial.SNOW_2, p -> p.ids(30, 33)),

commit dfbf8560e45eb1e772cf51b968c37c65fbfc0e90

Fix incorrect object ID introduced by #56

src/main/java/rs117/hd/model/objects/ObjectProperties.java
@@ -101,7 +101,7 @@ public enum ObjectProperties
 	APE_ATOLL_DUNGEON_WALLS(Material.GRUNGE_1, 4898),
 
 	// Zeah
-	KOUREND_PATHS(Material.GRUNGE_1, 13705, 13706, 13707, 13708, 13709, 13710, 13711, 13712, 13713, 13714, 13715, 13716, 1371742722),
+	KOUREND_PATHS(Material.GRUNGE_1, 13705, 13706, 13707, 13708, 13709, 13710, 13711, 13712, 13713, 13714, 13715, 13716, 13717, 42722),
 	KOUREND_PATH_STEPS(Material.GRUNGE_2, 30599, 30600),
 	KOUREND_WALLS(Material.GRUNGE_1,  new Properties().setFlatNormals(true), 23800, 23801, 23802, 23803, 23804, 23805, 23806, 23807, 23808, 23809, 23810),
 	SHAYZIEN_SHAY_SHRINE(Material.MARBLE_3_GLOSS, CAT_SHRINE),