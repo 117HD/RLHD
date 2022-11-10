6c6552cd7fe83032fca3f1ef6a1012f391800ed1
Mark7625 <72366279+Mark7625@users.noreply.github.com>
2022-11-10 19:33:10 +0000

    Update AreaManager.java

--- src/main/java/rs117/hd/scene/area/AreaManager.java
+++ src/main/java/rs117/hd/scene/area/AreaManager.java
@@ -2,17 +2,13 @@ package rs117.hd.scene.area;
 
 import com.google.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.GameState;
 import net.runelite.client.callback.ClientThread;
 import rs117.hd.HdPlugin;
-import rs117.hd.scene.model_overrides.ModelOverride;
 import rs117.hd.utils.Env;
-import rs117.hd.utils.ModelHash;
 import rs117.hd.utils.ResourcePath;
 
 import javax.inject.Singleton;
 import java.io.IOException;
-import java.util.List;
 
 import static rs117.hd.utils.ResourcePath.path;
 
