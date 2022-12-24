4d1333de21580e154584b70c14639ca1177eb949
SirFancyBacon <m.johnsondelta@gmail.com>
2022-12-23 19:01:42 -0500

    Burgh de Rott upgrade (#148)
    * adjust fences
    
    * Wooden plank walls, floors, and bridges
    
    * uv tweaks
    
    * adjustments
    
    * crates
    
    * Adds water to puddles
    
    * Removed duplicate entries
    
    * Formatting and puddle changes
    
    Co-authored-by: Hooder <ahooder@protonmail.com>

--- src/main/java/rs117/hd/data/materials/Material.java
+++ src/main/java/rs117/hd/data/materials/Material.java
@@ -250,9 +250,8 @@ public enum Material
 
 	CARPET,
 	FINE_CARPET(CARPET, p -> p
-			.setBrightness(1.4f)
-			.setTextureScale(0.5f,0.5f)
-	),
+		.setBrightness(1.4f)
+		.setTextureScale(0.5f,0.5f)),
 
 	FALADOR_PATH_BRICK(p -> p
 		.setSpecular(0.3f, 30)),
@@ -312,6 +311,7 @@ public enum Material
 		.setNormalMap(WOOD_GRAIN_2_N)),
 	WOOD_GRAIN_3,
 	DOCK_FENCE,
+	DOCK_FENCE_DARK(DOCK_FENCE, p -> p.setBrightness(0.6f)),
 
 	HD_INFERNAL_CAPE(p -> p
 		.replaceIf(INFERNAL_CAPE, HdPluginConfig::hdInfernalTexture)
@@ -347,34 +347,27 @@ public enum Material
 	WALL_STONE(p -> p.setNormalMap(WALL_STONE_N)),
 	METALLIC_1,
 	METALLIC_1_SEMIGLOSS(METALLIC_1, p -> p
-			.setSpecular(0.3f, 80)
-	),
+		.setSpecular(0.3f, 80)),
 	METALLIC_1_GLOSS(METALLIC_1, p -> p
-			.setSpecular(0.7f, 80)
-	),
+		.setSpecular(0.7f, 80)),
 	METALLIC_1_HIGHGLOSS(METALLIC_1, p -> p
-			.setSpecular(1.1f, 80)
-	),
+		.setSpecular(1.1f, 80)),
 	METALLIC_2(METALLIC_1, p -> p.setBrightness(1.8f)),
 	METALLIC_2_SEMIGLOSS(METALLIC_2, p -> p
-			.setSpecular(0.3f, 80)
-	),
+		.setSpecular(0.3f, 80)),
 	METALLIC_2_GLOSS(METALLIC_2, p -> p
-			.setSpecular(0.7f, 80)
-	),
+		.setSpecular(0.7f, 80)),
 	METALLIC_2_HIGHGLOSS(METALLIC_2, p -> p
-			.setSpecular(1.1f, 80)
-	),
+		.setSpecular(1.1f, 80)),
 	METALLIC_NONE_GLOSS(NONE, p -> p
-			.setSpecular(0.7f, 80)
-	),
+		.setSpecular(0.7f, 80)),
 	WATTLE_1,
 	ICE_1(GRUNGE_2, p -> p
-			.setSpecular(1.1f,200)
-	),
+		.setSpecular(1.1f,200)),
 	SLIME_GRUNGE(GRUNGE_1, p -> p
-			.setSpecular(4.1f,60)
-	),
+		.setSpecular(4.1f,60)),
+	WATER_PUDDLE(NONE, p -> p
+		.setSpecular(1.5f, 80)),
 
 	// Seasonal
 	WINTER_WILLOW_LEAVES(p -> p
