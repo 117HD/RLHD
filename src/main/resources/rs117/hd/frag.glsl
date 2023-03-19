117HD/RLHD

commit 8ba2f6aa3f704941324c0c58d4fac5ebd0b491c0

Add ModelOverride options for disabling shadow casting & receiving separately

src/main/resources/rs117/hd/frag.glsl
@@ -603,7 +603,7 @@ void main() {
 
 
         float shadow = 0;
-        if (((vMaterialData[0] >> MATERIAL_FLAG_DISABLE_SHADOWS) & 1) == 0)
+        if ((vMaterialData[0] >> MATERIAL_FLAG_DISABLE_SHADOW_RECEIVING & 1) == 0)
             shadow = sampleShadowMap(fragPos, waterTypeIndex, vec2(0), lightDotNormals);
         shadow = max(shadow, selfShadowing);
         float inverseShadow = 1 - shadow;

src/main/java/rs117/hd/model/ModelPusher.java
@@ -9,10 +9,12 @@ import net.runelite.client.callback.ClientThread;
 import net.runelite.client.util.LinkBrowser;
 import rs117.hd.HdPlugin;
 import rs117.hd.HdPluginConfig;
+import rs117.hd.data.WaterType;
 import rs117.hd.data.materials.Material;
 import rs117.hd.data.materials.Overlay;
 import rs117.hd.data.materials.Underlay;
 import rs117.hd.data.materials.UvType;
+import rs117.hd.scene.SceneUploader;
 import rs117.hd.scene.model_overrides.InheritTileColorType;
 import rs117.hd.scene.model_overrides.ModelOverride;
 import rs117.hd.scene.model_overrides.ObjectType;
@@ -334,9 +336,10 @@ public class ModelPusher {
     }
 
     private float[] getNormalDataForFace(Model model, @NonNull ModelOverride modelOverride, int face) {
-        if (modelOverride.flatNormals || model.getFaceColors3()[face] == -1) {
-            return zeroFloats;
-        }
+		int terrainData = SceneUploader.packTerrainData(false, 0, WaterType.NONE, 0);
+		if (terrainData == 0 && (modelOverride.flatNormals || model.getFaceColors3()[face] == -1)) {
+			return zeroFloats;
+		}
 
         final int triA = model.getFaceIndices1()[face];
         final int triB = model.getFaceIndices2()[face];
@@ -348,15 +351,15 @@ public class ModelPusher {
         twelveFloats[0] = xVertexNormals[triA];
         twelveFloats[1] = yVertexNormals[triA];
         twelveFloats[2] = zVertexNormals[triA];
-        twelveFloats[3] = 0;
+        twelveFloats[3] = terrainData;
         twelveFloats[4] = xVertexNormals[triB];
         twelveFloats[5] = yVertexNormals[triB];
         twelveFloats[6] = zVertexNormals[triB];
-        twelveFloats[7] = 0;
+        twelveFloats[7] = terrainData;
         twelveFloats[8] = xVertexNormals[triC];
         twelveFloats[9] = yVertexNormals[triC];
         twelveFloats[10] = zVertexNormals[triC];
-        twelveFloats[11] = 0;
+        twelveFloats[11] = terrainData;
 
         return twelveFloats;
     }
@@ -389,19 +392,17 @@ public class ModelPusher {
         }
 
         UvType uvType = modelOverride.uvType;
-        if (uvType == UvType.VANILLA) {
-            if (material == Material.NONE) {
-                return faceTextures == null ?
-                    null : // the whole model is untextured
-                    zeroFloats; // this face is untextured
-            }
+        if (uvType == UvType.VANILLA && !isVanillaUVMapped) {
+			uvType = UvType.GEOMETRY;
+		}
 
-            if (!isVanillaUVMapped) {
-                uvType = UvType.GEOMETRY;
-            }
-        }
+		int materialData = packMaterialData(material, modelOverride, uvType, false);
+		if (materialData == 0) {
+			return faceTextures == null ?
+				null : // the whole model is untextured
+				zeroFloats; // this face is untextured
+		}
 
-        int materialData = packMaterialData(material, modelOverride, uvType, false);
         twelveFloats[3] = twelveFloats[7] = twelveFloats[11] = materialData;
 
         switch (uvType) {
@@ -470,13 +471,14 @@ public class ModelPusher {
     }
 
     public int packMaterialData(Material material, @NonNull ModelOverride modelOverride, UvType uvType, boolean isOverlay) {
-        return
-            (material.ordinal() & (1 << 9) - 1) << 5
-            | (uvType == UvType.VANILLA ? 1 : 0) << 4
-            | (isOverlay ? 1 : 0) << 3
-            | (modelOverride.flatNormals ? 1 : 0) << 2
-            | (uvType.worldUvs ? 1 : 0) << 1
-            | (modelOverride.disableShadows ? 1 : 0);
+        return // This needs to return zero by default, since we often fall back to writing all zeroes to UVs
+            (material.ordinal() & (1 << 25) - 1) << 6
+			| (!modelOverride.receiveShadows ? 1 : 0) << 5
+			| (!modelOverride.castShadows ? 1 : 0) << 4
+			| (modelOverride.flatNormals ? 1 : 0) << 3
+			| (uvType.worldUvs ? 1 : 0) << 2
+			| (uvType == UvType.VANILLA ? 1 : 0) << 1
+			| (isOverlay ? 1 : 0);
     }
 
     private boolean isBakedGroundShading(int face, int heightA, int heightB, int heightC, byte[] faceTransparencies, short[] faceTextures) {

src/main/java/rs117/hd/scene/SceneUploader.java
@@ -456,10 +456,10 @@ class SceneUploader
 			if (proceduralGenerator.vertexIsOverlay.containsKey(swVertexKey) && proceduralGenerator.vertexIsUnderlay.containsKey(swVertexKey))
 				swVertexIsOverlay = true;
 
-			int swTerrainData = packTerrainData(0, waterType, tileZ);
-			int seTerrainData = packTerrainData(0, waterType, tileZ);
-			int nwTerrainData = packTerrainData(0, waterType, tileZ);
-			int neTerrainData = packTerrainData(0, waterType, tileZ);
+			int swTerrainData = packTerrainData(true, 0, waterType, tileZ);
+			int seTerrainData = packTerrainData(true, 0, waterType, tileZ);
+			int nwTerrainData = packTerrainData(true, 0, waterType, tileZ);
+			int neTerrainData = packTerrainData(true, 0, waterType, tileZ);
 
 			normalBuffer.ensureCapacity(24);
 			normalBuffer.put(neNormals[0], neNormals[2], neNormals[1], neTerrainData);
@@ -576,10 +576,10 @@ class SceneUploader
 
 			WaterType waterType = proceduralGenerator.tileWaterType(tile, sceneTilePaint);
 
-			int swTerrainData = packTerrainData(Math.max(1, swDepth), waterType, tileZ);
-			int seTerrainData = packTerrainData(Math.max(1, seDepth), waterType, tileZ);
-			int nwTerrainData = packTerrainData(Math.max(1, nwDepth), waterType, tileZ);
-			int neTerrainData = packTerrainData(Math.max(1, neDepth), waterType, tileZ);
+			int swTerrainData = packTerrainData(true, Math.max(1, swDepth), waterType, tileZ);
+			int seTerrainData = packTerrainData(true, Math.max(1, seDepth), waterType, tileZ);
+			int nwTerrainData = packTerrainData(true, Math.max(1, nwDepth), waterType, tileZ);
+			int neTerrainData = packTerrainData(true, Math.max(1, neDepth), waterType, tileZ);
 
 			normalBuffer.ensureCapacity(24);
 			normalBuffer.put(neNormals[0], neNormals[2], neNormals[1], neTerrainData);
@@ -803,9 +803,9 @@ class SceneUploader
 				vertexCIsOverlay = true;
 			}
 
-			int aTerrainData = packTerrainData(0, waterType, tileZ);
-			int bTerrainData = packTerrainData(0, waterType, tileZ);
-			int cTerrainData = packTerrainData(0, waterType, tileZ);
+			int aTerrainData = packTerrainData(true, 0, waterType, tileZ);
+			int bTerrainData = packTerrainData(true, 0, waterType, tileZ);
+			int cTerrainData = packTerrainData(true, 0, waterType, tileZ);
 
 			normalBuffer.ensureCapacity(12);
 			normalBuffer.put(normalsA[0], normalsA[2], normalsA[1], aTerrainData);
@@ -911,9 +911,9 @@ class SceneUploader
 
 				WaterType waterType = proceduralGenerator.faceWaterType(tile, face, sceneTileModel);
 
-				int aTerrainData = packTerrainData(Math.max(1, depthA), waterType, tileZ);
-				int bTerrainData = packTerrainData(Math.max(1, depthB), waterType, tileZ);
-				int cTerrainData = packTerrainData(Math.max(1, depthC), waterType, tileZ);
+				int aTerrainData = packTerrainData(true, Math.max(1, depthA), waterType, tileZ);
+				int bTerrainData = packTerrainData(true, Math.max(1, depthB), waterType, tileZ);
+				int cTerrainData = packTerrainData(true, Math.max(1, depthC), waterType, tileZ);
 
 				normalBuffer.ensureCapacity(12);
 				normalBuffer.put(normalsA[0], normalsA[2], normalsA[1], aTerrainData);
@@ -943,10 +943,9 @@ class SceneUploader
 		return new int[]{bufferLength, uvBufferLength, underwaterTerrain};
 	}
 
-	private int packTerrainData(int waterDepth, WaterType waterType, int plane)
+	public static int packTerrainData(boolean isTerrain, int waterDepth, WaterType waterType, int plane)
 	{
-		byte isTerrain = 0b1;
-		return waterDepth << 8 | waterType.ordinal() << 3 | plane << 1 | isTerrain;
+		return waterDepth << 8 | waterType.ordinal() << 3 | plane << 1 | (isTerrain ? 1 : 0);
 	}
 
 	private boolean shouldSkipTile(int worldX, int worldY) {

src/main/java/rs117/hd/scene/model_overrides/ModelOverride.java
@@ -33,7 +33,8 @@ public class ModelOverride
     public int uvOrientation = 0;
     public boolean flatNormals = false;
     public boolean removeBakedLighting = false;
-    public boolean disableShadows = false;
+    public boolean castShadows = true;
+    public boolean receiveShadows = true;
     public TzHaarRecolorType tzHaarRecolorType = TzHaarRecolorType.NONE;
     public InheritTileColorType inheritTileColorType = InheritTileColorType.NONE;
 

src/main/resources/rs117/hd/opengl/compute/constants.cl
@@ -1,2 +1,2 @@
 // This is kept separate from constants.glsl to avoid accidentally breaking compatibility with macOS
-#define MATERIAL_FLAG_IS_VANILLA_TEXTURED 4
+#define MATERIAL_FLAG_IS_VANILLA_TEXTURED 1

src/main/resources/rs117/hd/shadow_vert.glsl
@@ -58,7 +58,7 @@ void main()
     float alpha = 1 - float(vPosition.w >> 24 & 0xff) / 255.;
     int waterTypeIndex = terrainData >> 3 & 0x1F;
 
-    int isShadowDisabled = materialData >> MATERIAL_FLAG_DISABLE_SHADOWS & 1;
+    int isShadowDisabled = materialData >> MATERIAL_FLAG_DISABLE_SHADOW_CASTING & 1;
     int isGroundPlane = when_eq(terrainData & 0xF, 1); // isTerrain && plane == 0
     int isTransparent = when_lt(alpha, SHADOW_OPACITY_THRESHOLD);
     int isWaterSurfaceOrUnderwaterTile = when_gt(waterTypeIndex, 0);

src/main/resources/rs117/hd/utils/constants.glsl
@@ -1,9 +1,10 @@
 // Any changes here may need to be reflected in OpenCL's constants.cl
 // They are kept separate to avoid accidentally breaking OpenCL compatibility
 #define SHADOW_OPACITY_THRESHOLD 0.81
-#define MATERIAL_FLAG_BITS 5
-#define MATERIAL_FLAG_IS_VANILLA_TEXTURED 4
-#define MATERIAL_FLAG_IS_OVERLAY 3
-#define MATERIAL_FLAG_FLAT_NORMALS 2
-#define MATERIAL_FLAG_WORLD_UVS 1
-#define MATERIAL_FLAG_DISABLE_SHADOWS 0
+#define MATERIAL_FLAG_BITS 6
+#define MATERIAL_FLAG_DISABLE_SHADOW_RECEIVING 5
+#define MATERIAL_FLAG_DISABLE_SHADOW_CASTING 4
+#define MATERIAL_FLAG_FLAT_NORMALS 3
+#define MATERIAL_FLAG_WORLD_UVS 2
+#define MATERIAL_FLAG_IS_VANILLA_TEXTURED 1
+#define MATERIAL_FLAG_IS_OVERLAY 0

commit 67642117b88df89f0887e6ee05376dd3bd230a1d

Fix tile blending shader bug introduced by 8ba2f6a

src/main/resources/rs117/hd/frag.glsl
@@ -449,9 +449,9 @@ void main() {
         texColor3.rgb *= material3.brightness;
 
         ivec3 isOverlay = ivec3(
-            vMaterialData[0] >> 3 & 1,
-            vMaterialData[1] >> 3 & 1,
-            vMaterialData[2] >> 3 & 1
+            vMaterialData[0] >> MATERIAL_FLAG_IS_OVERLAY & 1,
+            vMaterialData[1] >> MATERIAL_FLAG_IS_OVERLAY & 1,
+            vMaterialData[2] >> MATERIAL_FLAG_IS_OVERLAY & 1
         );
         int overlayCount = isOverlay[0] + isOverlay[1] + isOverlay[2];
         ivec3 isUnderlay = ivec3(1) - isOverlay;