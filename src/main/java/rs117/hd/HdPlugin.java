117HD/RLHD

commit 1f33ab6a343cad90e75787d76e6d9406326b0fb9

Clear staging & model buffers directly after use instead of post frame

src/main/java/rs117/hd/HdPlugin.java
@@ -435,7 +435,7 @@ public class HdPlugin extends Plugin implements DrawCallbacks
 
 	public int[] camTarget = new int[3];
 
-	private boolean hasStarted;
+	private boolean running;
 	private boolean hasLoggedIn;
 	private boolean lwjglInitted;
 
@@ -616,7 +616,7 @@ public class HdPlugin extends Plugin implements DrawCallbacks
 				modelOverrideManager.startUp();
 				modelPusher.startUp();
 
-				hasStarted = true;
+				running = true;
 
 				if (client.getGameState() == GameState.LOGGED_IN)
 				{
@@ -639,7 +639,7 @@ public class HdPlugin extends Plugin implements DrawCallbacks
 	@Override
 	protected void shutDown()
 	{
-		hasStarted = false;
+		running = false;
 
 		FileWatcher.destroy();
 		developerTools.deactivate();
@@ -1446,26 +1446,36 @@ public class HdPlugin extends Plugin implements DrawCallbacks
 	@Override
 	public void postDrawScene()
 	{
-		// Upload buffers
+		if (!running)
+			return;
+
+		// Geometry buffers
 		stagingBufferVertices.flip();
 		stagingBufferUvs.flip();
 		stagingBufferNormals.flip();
+		updateBuffer(hStagingBufferVertices, GL_ARRAY_BUFFER,
+			dynamicOffsetVertices * VERTEX_SIZE, stagingBufferVertices.getBuffer(),
+			GL_STREAM_DRAW, CL_MEM_READ_ONLY);
+		updateBuffer(hStagingBufferUvs, GL_ARRAY_BUFFER,
+			dynamicOffsetUvs * UV_SIZE, stagingBufferUvs.getBuffer(),
+			GL_STREAM_DRAW, CL_MEM_READ_ONLY);
+		updateBuffer(hStagingBufferNormals, GL_ARRAY_BUFFER,
+			dynamicOffsetVertices * NORMAL_SIZE, stagingBufferNormals.getBuffer(),
+			GL_STREAM_DRAW, CL_MEM_READ_ONLY);
+		stagingBufferVertices.clear();
+		stagingBufferUvs.clear();
+		stagingBufferNormals.clear();
+
+		// Model buffers
 		modelBufferUnordered.flip();
 		modelBufferSmall.flip();
 		modelBufferLarge.flip();
-
-		// temp buffers
-		updateBuffer(hStagingBufferVertices, GL_ARRAY_BUFFER,
-			dynamicOffsetVertices * VERTEX_SIZE, stagingBufferVertices.getBuffer(), GL_STREAM_DRAW, CL_MEM_READ_ONLY);
-		updateBuffer(hStagingBufferUvs, GL_ARRAY_BUFFER,
-			dynamicOffsetUvs * UV_SIZE, stagingBufferUvs.getBuffer(), GL_STREAM_DRAW, CL_MEM_READ_ONLY);
-		updateBuffer(hStagingBufferNormals, GL_ARRAY_BUFFER,
-			dynamicOffsetVertices * NORMAL_SIZE, stagingBufferNormals.getBuffer(), GL_STREAM_DRAW, CL_MEM_READ_ONLY);
-
-		// model buffers
 		updateBuffer(hModelBufferLarge, GL_ARRAY_BUFFER, modelBufferLarge.getBuffer(), GL_STREAM_DRAW, CL_MEM_READ_ONLY);
 		updateBuffer(hModelBufferSmall, GL_ARRAY_BUFFER, modelBufferSmall.getBuffer(), GL_STREAM_DRAW, CL_MEM_READ_ONLY);
 		updateBuffer(hModelBufferUnordered, GL_ARRAY_BUFFER, modelBufferUnordered.getBuffer(), GL_STREAM_DRAW, CL_MEM_READ_ONLY);
+		modelBufferUnordered.clear();
+		modelBufferSmall.clear();
+		modelBufferLarge.clear();
 
 		// Output buffers
 		updateBuffer(hRenderBufferVertices,
@@ -1535,6 +1545,8 @@ public class HdPlugin extends Plugin implements DrawCallbacks
 		glDispatchCompute(numModelsLarge, 1, 1);
 
 		checkGLErrors();
+
+		numModelsUnordered = numModelsSmall = numModelsLarge = 0;
 	}
 
 	@Override
@@ -2106,14 +2118,7 @@ public class HdPlugin extends Plugin implements DrawCallbacks
 				glBindFramebuffer(GL_READ_FRAMEBUFFER, awtContext.getFramebuffer(false));
 			}
 
-			stagingBufferVertices.clear();
-			stagingBufferUvs.clear();
-			stagingBufferNormals.clear();
-			modelBufferUnordered.clear();
-			modelBufferSmall.clear();
-			modelBufferLarge.clear();
 			frameModelInfoMap.clear();
-			numModelsUnordered = numModelsSmall = numModelsLarge = 0;
 		}
 
 		// Texture on UI
@@ -2239,7 +2244,7 @@ public class HdPlugin extends Plugin implements DrawCallbacks
 	@Subscribe
 	public void onGameStateChanged(GameStateChanged gameStateChanged)
 	{
-		if (!hasStarted)
+		if (!running)
 			return;
 
 		switch (gameStateChanged.getGameState()) {

commit 40be936265d3e875019522338be689eb0c747065

Reset model counters also when OpenCL is in use
Model buffer counters were erroneously not reset when OpenCL was in use,
as it would exit the function early. This leads to heavy compute shader
very quickly, and was breaking the plugin entirely on macOS.

Broken since 1f33ab6a343cad90e75787d76e6d9406326b0fb9.

src/main/java/rs117/hd/HdPlugin.java
@@ -1513,42 +1513,41 @@ public class HdPlugin extends Plugin implements DrawCallbacks
 				hModelBufferUnordered, hModelBufferSmall, hModelBufferLarge,
 				hStagingBufferVertices, hStagingBufferUvs, hStagingBufferNormals,
 				hRenderBufferVertices, hRenderBufferUvs, hRenderBufferNormals);
-
-			checkGLErrors();
-			return;
 		}
+		else
+		{
+			/*
+			 * Compute is split into three separate programs: 'unordered', 'small', and 'large'
+			 * to save on GPU resources. Small will sort <= 512 faces, large will do <= 4096.
+			 */
 
-		/*
-		 * Compute is split into three separate programs: 'unordered', 'small', and 'large'
-		 * to save on GPU resources. Small will sort <= 512 faces, large will do <= 4096.
-		 */
+			// Bind UBO to compute programs
+			glUniformBlockBinding(glSmallComputeProgram, uniBlockCameraComputeSmall, 0);
+			glUniformBlockBinding(glLargeComputeProgram, uniBlockCameraComputeLarge, 0);
 
-		// Bind UBO to compute programs
-		glUniformBlockBinding(glSmallComputeProgram, uniBlockCameraComputeSmall, 0);
-		glUniformBlockBinding(glLargeComputeProgram, uniBlockCameraComputeLarge, 0);
+			// Bind shared buffers
+			glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, hStagingBufferVertices.glBufferId);
+			glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 2, hStagingBufferUvs.glBufferId);
+			glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 3, hStagingBufferNormals.glBufferId);
+			glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 4, hRenderBufferVertices.glBufferId);
+			glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 5, hRenderBufferUvs.glBufferId);
+			glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 6, hRenderBufferNormals.glBufferId);
 
-		// Bind shared buffers
-		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, hStagingBufferVertices.glBufferId);
-		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 2, hStagingBufferUvs.glBufferId);
-		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 3, hStagingBufferNormals.glBufferId);
-		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 4, hRenderBufferVertices.glBufferId);
-		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 5, hRenderBufferUvs.glBufferId);
-		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 6, hRenderBufferNormals.glBufferId);
+			// unordered
+			glUseProgram(glUnorderedComputeProgram);
+			glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, hModelBufferUnordered.glBufferId);
+			glDispatchCompute(numModelsUnordered, 1, 1);
 
-		// unordered
-		glUseProgram(glUnorderedComputeProgram);
-		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, hModelBufferUnordered.glBufferId);
-		glDispatchCompute(numModelsUnordered, 1, 1);
+			// small
+			glUseProgram(glSmallComputeProgram);
+			glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, hModelBufferSmall.glBufferId);
+			glDispatchCompute(numModelsSmall, 1, 1);
 
-		// small
-		glUseProgram(glSmallComputeProgram);
-		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, hModelBufferSmall.glBufferId);
-		glDispatchCompute(numModelsSmall, 1, 1);
-
-		// large
-		glUseProgram(glLargeComputeProgram);
-		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, hModelBufferLarge.glBufferId);
-		glDispatchCompute(numModelsLarge, 1, 1);
+			// large
+			glUseProgram(glLargeComputeProgram);
+			glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, hModelBufferLarge.glBufferId);
+			glDispatchCompute(numModelsLarge, 1, 1);
+		}
 
 		checkGLErrors();
 