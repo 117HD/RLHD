8acd2b977deb08afaddbe8d6f4db8bf892f70999
SirFancyBacon <m.johnsondelta@gmail.com>
2023-05-23 19:04:05 -0400

    Set to falador paths

--- src/main/java/rs117/hd/data/materials/Overlay.java
+++ src/main/java/rs117/hd/data/materials/Overlay.java
@@ -193,7 +193,7 @@ public enum Overlay {
     KHARID_PATHS_3(25, Area.KHARID_DESERT_REGION, GroundMaterial.DIRT, p -> p.saturation(3).hue(6)),
     GIANTS_PLATEAU_CABBAGE_PATCH_FIX(14, Area.GIANTS_PLATEAU_CABBAGE_PATCH, GroundMaterial.DIRT, p -> p.blended(false)),
     PVP_ARENA_PATH(227, Area.PVP_ARENA, GroundMaterial.DIRT),
-    DESERT_TREASURE_INTERIOR_FLOOR(GroundMaterial.MARBLE_2_GLOSS, p -> p
+    DESERT_TREASURE_INTERIOR_FLOOR(GroundMaterial.FALADOR_PATHS, p -> p
             .area(Area.DESERT_TREASURE_PYRAMID)
             .ids(21)
     ),
