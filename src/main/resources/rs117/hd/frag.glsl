117HD/RLHD

commit 3bb40cc0c9c64ed3bc18a070b4445bb0dbadcd1e

Fix normal mapping for all surfaces
- Fix brick texture normals
- Fix portal nexus normals

src/main/resources/rs117/hd/frag.glsl
@@ -71,6 +71,7 @@ flat in vec4 vColor[3];
 flat in vec3 vUv[3];
 flat in int vMaterialData[3];
 flat in int vTerrainData[3];
+flat in mat2x3 TB;
 
 in FragmentData {
     vec3 position;
@@ -136,14 +137,6 @@ void main() {
     if (isWater) {
         outputColor = sampleWater(waterTypeIndex, viewDir);
     } else {
-        // Source: https://www.geeks3d.com/20130122/normal-mapping-without-precomputed-tangent-space-vectors/
-        vec3 N = IN.normal;
-        vec3 C1 = cross(vec3(0, 0, 1), N);
-        vec3 C2 = cross(vec3(0, 1, 0), N);
-        vec3 T = normalize(length(C1) > length(C2) ? C1 : C2);
-        vec3 B = cross(N, T);
-        mat3 TBN = mat3(T, B, N);
-
         vec2 uv1 = vUv[0].xy;
         vec2 uv2 = vUv[1].xy;
         vec2 uv3 = vUv[2].xy;
@@ -159,6 +152,25 @@ void main() {
         uv1 = (uv1 - .5) / material1.textureScale + .5;
         uv2 = (uv2 - .5) / material2.textureScale + .5;
         uv3 = (uv3 - .5) / material3.textureScale + .5;
+
+        // get flowMap map
+        vec2 flowMapUv = uv1 - animationFrame(material1.flowMapDuration);
+        float flowMapStrength = material1.flowMapStrength;
+        if (isUnderwater)
+        {
+            // Distort underwater textures
+            flowMapUv = worldUvs(1.5) + animationFrame(10 * waterType.duration) * vec2(1, -1);
+            flowMapStrength = 0.075;
+        }
+
+        vec2 uvFlow = texture(textureArray, vec3(flowMapUv, flowMap)).xy;
+        uv1 += uvFlow * flowMapStrength;
+        uv2 += uvFlow * flowMapStrength;
+        uv3 += uvFlow * flowMapStrength;
+
+        // Set up tangent-space transformation matrix
+        vec3 N = normalize(IN.normal);
+        mat3 TBN = mat3(TB[0], TB[1], N);
 
         float selfShadowing = 0;
         vec3 fragPos = IN.position;
@@ -179,21 +191,6 @@ void main() {
 
         fragPos += TBN * vec3(fragDelta, 0);
         #endif
-
-        // get flowMap map
-        vec2 flowMapUv = uv1 - animationFrame(material1.flowMapDuration);
-        float flowMapStrength = material1.flowMapStrength;
-        if (isUnderwater)
-        {
-            // Distort underwater textures
-            flowMapUv = worldUvs(1.5) + animationFrame(10 * waterType.duration) * vec2(1, -1);
-            flowMapStrength = 0.075;
-        }
-
-        vec2 uvFlow = texture(textureArray, vec3(flowMapUv, flowMap)).xy;
-        uv1 += uvFlow * flowMapStrength;
-        uv2 += uvFlow * flowMapStrength;
-        uv3 += uvFlow * flowMapStrength;
 
         // get vertex colors
         vec4 flatColor = vec4(0.5, 0.5, 0.5, 1.0);
@@ -338,9 +335,9 @@ void main() {
         outputColor = mix(underlayColor, overlayColor, overlayMix);
 
         // normals
-        vec3 n1 = sampleNormalMap(material1, uv1, IN.normal, TBN);
-        vec3 n2 = sampleNormalMap(material2, uv2, IN.normal, TBN);
-        vec3 n3 = sampleNormalMap(material3, uv3, IN.normal, TBN);
+        vec3 n1 = sampleNormalMap(material1, uv1, TBN);
+        vec3 n2 = sampleNormalMap(material2, uv2, TBN);
+        vec3 n3 = sampleNormalMap(material3, uv3, TBN);
         vec3 normals = normalize(n1 * IN.texBlend.x + n2 * IN.texBlend.y + n3 * IN.texBlend.z);
 
         float lightDotNormals = dot(normals, lightDir);

src/main/resources/rs117/hd/geom.glsl
@@ -51,6 +51,7 @@ flat out vec4 vColor[3];
 flat out vec3 vUv[3];
 flat out int vMaterialData[3];
 flat out int vTerrainData[3];
+flat out mat2x3 TB;
 
 out FragmentData {
     vec3 position;
@@ -75,6 +76,19 @@ void main() {
     vec3 N = normalize(cross(T, B));
 
     computeUvs(vMaterialData[0], vec3[](gPosition[0], gPosition[1], gPosition[2]), vUv);
+
+    // Calculate tangent-space vectors
+    mat2 uvToTri = inverse(mat2(
+        vUv[1].xy - vUv[0].xy,
+        vUv[2].xy - vUv[0].xy
+    )) * -1; // Flip UV direction, since OSRS UVs are oriented strangely
+    mat2x3 triToWorld = mat2x3(
+        gPosition[1] - gPosition[0],
+        gPosition[2] - gPosition[0]
+    );
+    TB = triToWorld * uvToTri;
+    TB[0] = normalize(TB[0]);
+    TB[1] = normalize(TB[1]);
 
     for (int i = 0; i < 3; i++) {
         OUT.position = gPosition[i];

src/main/resources/rs117/hd/utils/normals.glsl
@@ -23,12 +23,12 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-vec3 sampleNormalMap(Material mat, vec2 uv, const vec3 surfaceNormal, const mat3 TBN) {
-    if (mat.normalMap == -1)
-        return surfaceNormal;
+vec3 sampleNormalMap(const Material material, const vec2 uv, const mat3 TBN) {
+    if (material.normalMap == -1)
+        return TBN[2];
 
     // Sample normal map texture, swapping Y and Z to match the coordinate system in OSRS
-    vec3 n = texture(textureArray, vec3(uv, mat.normalMap)).xyz;
+    vec3 n = texture(textureArray, vec3(uv, material.normalMap)).xyz;
     // Undo automatic sRGB to linear conversion, since we want the raw values
     n = linearToSrgb(n);
     // Scale and shift normal so it can point in both directions

commit 829c5f83ea85b620ebbb5791b4966078d98f3157

Fix for older Intel-based Macs
3bb40cc0c9c64ed3bc18a070b4445bb0dbadcd1e broke the shader for older
Intel-based Macs, including the following GPUs (not exhaustive):
- Intel(R) Iris(TM) Graphics 6100
- Intel(R) Iris(TM) Plus Graphics 640
- Intel(R) Iris(TM) Plus Graphics 645
- Intel(R) Iris(TM) Plus Graphics 650

It seems like these GPUs don't tolerate passing non-square matrices
between different shader stages.

src/main/resources/rs117/hd/frag.glsl
@@ -71,7 +71,8 @@ flat in vec4 vColor[3];
 flat in vec3 vUv[3];
 flat in int vMaterialData[3];
 flat in int vTerrainData[3];
-flat in mat2x3 TB;
+flat in vec3 T;
+flat in vec3 B;
 
 in FragmentData {
     vec3 position;
@@ -168,7 +169,7 @@ void main() {
 
         // Set up tangent-space transformation matrix
         vec3 N = normalize(IN.normal);
-        mat3 TBN = mat3(TB[0], TB[1], N * min(length(TB[0]), length(TB[1])));
+        mat3 TBN = mat3(T, B, N * min(length(T), length(B)));
 
         float selfShadowing = 0;
         vec3 fragPos = IN.position;

src/main/resources/rs117/hd/geom.glsl
@@ -50,7 +50,8 @@ flat out vec4 vColor[3];
 flat out vec3 vUv[3];
 flat out int vMaterialData[3];
 flat out int vTerrainData[3];
-flat out mat2x3 TB;
+flat out vec3 T;
+flat out vec3 B;
 
 out FragmentData {
     vec3 position;
@@ -69,11 +70,6 @@ void main() {
         vTerrainData[i] = gTerrainData[i];
     }
 
-    mat2x3 triToWorld = mat2x3(
-        gPosition[1] - gPosition[0],
-        gPosition[2] - gPosition[0]
-    );
-
     computeUvs(vMaterialData[0], vec3[](gPosition[0], gPosition[1], gPosition[2]), vUv);
 
     // Calculate tangent-space vectors
@@ -83,10 +79,15 @@ void main() {
     );
     if (determinant(triToUv) == 0)
         triToUv = mat2(1);
-
     mat2 uvToTri = inverse(triToUv) * -1; // Flip UV direction, since OSRS UVs are oriented strangely
-    TB = triToWorld * uvToTri; // Preserve scale in order for displacement to interact properly with shadow mapping
-    vec3 N = normalize(cross(triToWorld[0], triToWorld[1]));
+    mat2x3 triToWorld = mat2x3(
+        gPosition[1] - gPosition[0],
+        gPosition[2] - gPosition[0]
+    );
+    mat2x3 TB = triToWorld * uvToTri; // Preserve scale in order for displacement to interact properly with shadow mapping
+    T = TB[0];
+    B = TB[1];
+    vec3 N = normalize(cross(T, B));
 
     for (int i = 0; i < 3; i++) {
         OUT.position = gPosition[i];