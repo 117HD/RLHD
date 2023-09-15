50a6e5e8f70f1f24ee7497bbe5cc58307e739126
Hooder <ahooder@protonmail.com>
2023-09-15 02:55:39 +0200

    Fix hillskew issue on AMD Mesa drivers
    Apparently this was already fixed by RuneLite, but I unfortunately
    didn't stumble across the commit until after I found it again...
    https://github.com/runelite/runelite/commit/5bf71b15dae5c4f3200a2c9f0f39a5d669a0402b

--- src/main/java/rs117/hd/HdPlugin.java
+++ src/main/java/rs117/hd/HdPlugin.java
@@ -1371,8 +1371,8 @@ public class HdPlugin extends Plugin implements DrawCallbacks {
 
 		texTileHeightMap = glGenTextures();
 		glBindTexture(GL_TEXTURE_3D, texTileHeightMap);
-		glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 		glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 		glTexParameteri(GL_TEXTURE_3D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 		glTexImage3D(GL_TEXTURE_3D, 0, GL_R16I,
