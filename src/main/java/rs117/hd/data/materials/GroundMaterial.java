c13ebf37d9163d30ab4e771c9ae65fb82b4dd428
SirFancyBacon <m.johnsondelta@gmail.com>
2023-10-14 23:06:14 -0400

    adjust materials for floor

--- src/main/java/rs117/hd/data/materials/GroundMaterial.java
+++ src/main/java/rs117/hd/data/materials/GroundMaterial.java
@@ -109,7 +109,14 @@ public enum GroundMaterial {
 	PACKED_EARTH(Material.DIRT_1, Material.GRAVEL, Material.DIRT_1, Material.DIRT_1, Material.DIRT_2),
 	GRASSY_DIRT(Material.GRASS_1, Material.DIRT_1, Material.GRASS_2, Material.DIRT_2, Material.GRASS_3),
 	OOZE_FLOOR(Material.GRUNGE_1_SHINY, Material.GRAVEL_SHINY_LIGHT, Material.GRUNGE_2_SHINY),
-	GUARDIANS_RIFT_CAVE_FLOOR(Material.GRAVEL_LIGHT, Material.ROCK_2, Material.ROCK_1, Material.GRAVEL_LIGHT)
+	GUARDIANS_RIFT_CAVE_FLOOR(
+		Material.GRUNGE_1,
+		Material.GRAVEL_LIGHT,
+		Material.ROCK_1_LIGHT,
+		Material.GRUNGE_1,
+		Material.ROCK_1_LIGHT,
+		Material.GRAVEL_LIGHT
+	)
 	;
 
 	private final Material[] materials;
