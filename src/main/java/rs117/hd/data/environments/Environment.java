1c98b63956e27fa1a5cf8a0fdbefdf7b41f3405b
Hooder <ahooder@protonmail.com>
2023-11-15 17:33:45 +0100

    Remove empty environments

--- src/main/java/rs117/hd/data/environments/Environment.java
+++ src/main/java/rs117/hd/data/environments/Environment.java
@@ -32,7 +32,6 @@ import static rs117.hd.utils.ColorUtils.rgb;
 @Getter
 public enum Environment
 {
-
 	EVIL_BOB_ISLAND(Area.EVIL_BOB_ISLAND, new Properties()
 		.setFogColor("#B8D6FF")
 		.setFogDepth(70)
@@ -41,6 +40,7 @@ public enum Environment
 		.setDirectionalColor("#F5BC67")
 		.setDirectionalStrength(1.0f)
 	),
+
 	// Wilderness
 	REVENANT_CAVES(Area.REVENANT_CAVES, new Properties()
 		.setFogColor("#081F1C")
@@ -375,7 +375,6 @@ public enum Environment
 		.setGroundFog(-150, -350, 0.5f)
 	),
 
-	LUMBRIDGE(Area.LUMBRIDGE, new Properties()),
 	LUMBRIDGE_BASEMENT(Area.LUMBRIDGE_CASTLE_BASEMENT, new Properties()
 			.setFogColor("#070606")
 			.setFogDepth(84)
@@ -422,7 +421,6 @@ public enum Environment
 		.setDirectionalStrength(0.0f)
 		.setLightDirection(260f, 10f)
 	),
-	DRAYNOR(Area.DRAYNOR, new Properties()),
 
 	MISTHALIN_MYSTERY_MANOR(Area.MISTHALIN_MYSTERY_MANOR, new Properties()
 		.setFogColor(15, 14, 13)
@@ -461,7 +459,6 @@ public enum Environment
 	TUTORIAL_SOUL_WARS_RED_TEAM(Area.SOUL_WARS_RED_BASE_TUTORIAL, new Properties()
 		.setFogColor(28, 21, 13)
 	),
-	TUTORIAL_ISLE_OF_SOULS(Area.ISLE_OF_SOULS_TUTORIAL, new Properties()),
 
 	SMOKE_DUNGEON(Area.SMOKE_DUNGEON, new Properties()
 		.setFogColor(0, 0, 0)
@@ -533,10 +530,6 @@ public enum Environment
 		.setDirectionalStrength(2.5f)
 	),
 
-	WHITE_WOLF_MOUNTAIN(Area.WHITE_WOLF_MOUNTAIN, new Properties()),
-
-	KEEP_LE_FAYE(Area.KEEP_LE_FAYE, new Properties()),
-
 	MOUNTAIN_CAMP_ENTRY_PATH(Area.MOUNTAIN_CAMP_ENTRY_PATH, new Properties()
 		.setFogColor(178, 187, 197)
 		.setFogDepth(50)
@@ -591,8 +584,6 @@ public enum Environment
 		.setDirectionalStrength(1.0f)
 		.setLightDirection(240f, 190f)
 	),
-	KARAMJA(Area.KARAMJA, new Properties()),
-
 
 	UNGAEL(Area.UNGAEL, new Properties()
 		.setFogColor(226, 230, 237)
@@ -696,9 +687,6 @@ public enum Environment
 		.setLightDirection(260f, 10f)
 	),
 
-	// Tree Gnome Stronghold
-	TREE_GNOME_STRONGHOLD(Area.TREE_GNOME_STRONGHOLD, new Properties()),
-
 	// Castle Wars
 	CASTLE_WARS_UNDERGROUND(Area.CASTLE_WARS_UNDERGROUND, new Properties()
 		.setFogColor("#190D02")
@@ -720,12 +708,11 @@ public enum Environment
 		.setDirectionalColor("#C5B8B6")
 		.setGroundFog(-0, -250, 0.3f)
 	),
-	LMS_ARENA_DESERTED_ISLAND(Area.LMS_ARENA_DESERTED_ISLAND, new Properties()),
 
 	// Zeah
 	MOUNT_KARUULM(Area.MOUNT_KARUULM, new Properties()
-			.setAmbientStrength(1.0f)
-			.setDirectionalStrength(3.0f)
+		.setAmbientStrength(1.0f)
+		.setDirectionalStrength(3.0f)
 	),
 	KARUULM_SLAYER_DUNGEON(Area.KARUULM_SLAYER_DUNGEON, new Properties()
 		.setFogColor("#051E22")
@@ -744,7 +731,6 @@ public enum Environment
 		.setDirectionalColor("#FFFFFF")
 		.setDirectionalStrength(1.0f)
 	),
-	MOUNT_QUIDAMORTEM(Area.MOUNT_QUIDAMORTEM, new Properties()),
 	KEBOS_LOWLANDS(Area.KEBOS_LOWLANDS, new Properties()
 		.setFogColor(41, 44, 16)
 		.setFogDepth(50)
@@ -864,7 +850,7 @@ public enum Environment
 	),
 
 	// The Gauntlet
-	THE_GAUNTLET(Area.THE_GAUNTLET, new Properties()
+	THE_GAUNTLET_NORMAL(Area.THE_GAUNTLET_NORMAL, new Properties()
 		.setFogColor("#090606")
 		.setFogDepth(20)
 		.setAmbientColor("#D2C0B7")
@@ -892,13 +878,6 @@ public enum Environment
 		.setLightDirection(260f, 10f)
 	),
 
-	// Islands
-	BRAINDEATH_ISLAND(Area.BRAINDEATH_ISLAND, new Properties()),
-
-	// Ape Atoll
-	// Monkey Madness 2
-	MM2_AIRSHIP_PLATFORM(Area.MM2_AIRSHIP_PLATFORM, new Properties()),
-
 	// POHs
 	PLAYER_OWNED_HOUSE_SNOWY(Area.PLAYER_OWNED_HOUSE_SNOWY, new Properties()
 		.setFogColor("#AEBDE0")
@@ -908,7 +887,6 @@ public enum Environment
 		.setDirectionalColor("#F4E5C9")
 		.setDirectionalStrength(2.5f)
 	),
-	PLAYER_OWNED_HOUSE(Area.PLAYER_OWNED_HOUSE, new Properties()),
 
 	// Blackhole
 	BLACKHOLE(Area.BLACKHOLE, new Properties()
@@ -920,9 +898,6 @@ public enum Environment
 		.setLightDirection(260f, 10f)
 		.setAllowSkyOverride(false)
 	),
-
-	// Fishing Trawler
-	FISHING_TRAWLER(Area.FISHING_TRAWLER, new Properties()),
 
 	// Camdozaal (Below Ice Mountain)
 	CAMDOZAAL(Area.CAMDOZAAL, new Properties()
@@ -1050,9 +1025,6 @@ public enum Environment
 	),
 
 	// Runecrafting altars
-	NATURE_ALTAR(Area.NATURE_ALTAR, new Properties()),
-	WATER_ALTAR(Area.WATER_ALTAR, new Properties()),
-	AIR_ALTAR(Area.AIR_ALTAR, new Properties()),
 	COSMIC_ALTAR(Area.COSMIC_ALTAR, new Properties()
 		.setFogColor("#000000")
 		.setFogDepth(40)
@@ -1079,24 +1051,11 @@ public enum Environment
 	),
 
 	// Random events
-	CLASSROOM(Area.RANDOM_EVENT_CLASSROOM, new Properties()),
-	FREAKY_FORESTER(Area.RANDOM_EVENT_FREAKY_FORESTER, new Properties()),
-	GRAVEDIGGER(Area.RANDOM_EVENT_GRAVEDIGGER, new Properties()),
 	DRILL_DEMON(Area.RANDOM_EVENT_DRILL_DEMON, new Properties()
 		.setFogColor("#696559")
 	),
 
-	// Clan halls
-	CLAN_HALL(Area.CLAN_HALL, new Properties()),
-
 	// Standalone and miscellaneous areas
-	LIGHTHOUSE(Area.LIGHTHOUSE, new Properties()),
-	SORCERESSS_GARDEN(Area.SORCERESSS_GARDEN, new Properties()),
-	PURO_PURO(Area.PURO_PURO, new Properties()),
-	RATCATCHERS_HOUSE(Area.RATCATCHERS_HOUSE, new Properties()),
-	CANOE_CUTSCENE(Area.CANOE_CUTSCENE, new Properties()),
-	FISHER_KINGS_REALM(Area.FISHER_KINGS_REALM, new Properties()),
-	ENCHANTED_VALLEY(Area.ENCHANTED_VALLEY, new Properties()),
 	GIANTS_FOUNDRY(Area.GIANTS_FOUNDRY, new Properties()
 		.setFogColor(0, 0, 0)
 		.setFogDepth(12)
@@ -1259,6 +1218,10 @@ public enum Environment
 		.setLightDirection(260f, 10f)
 		.setWaterColor(102, 234, 255)
 	),
+	NONE(Area.NONE, new Properties()
+		.setFogColor("#ff00ff") // never meant to be rendered
+	),
+
 	AUTUMN(Area.NONE, new Properties()
 		.setFogColor("#fddcb4")
 		.setFogDepth(18)
@@ -1275,9 +1238,6 @@ public enum Environment
 		.setAmbientStrength(3.5f)
 		.setDirectionalColor("#FFFFFF")
 		.setDirectionalStrength(1.5f)
-	),
-	NONE(Area.NONE, new Properties()
-		.setFogColor("#ff00ff") // never meant to be rendered
 	),
 	;
 
