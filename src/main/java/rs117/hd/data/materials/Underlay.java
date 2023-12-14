d90b897607a491fc1e06908c4938dc7e5af99034
SirFancyBacon <m.johnsondelta@gmail.com>
2023-12-14 11:22:58 -0500

    Define ground

--- src/main/java/rs117/hd/data/materials/Underlay.java
+++ src/main/java/rs117/hd/data/materials/Underlay.java
@@ -563,6 +563,7 @@ public enum Underlay {
 		.blended(true)),
 
 	SHIP_SAILING_WATER(p -> p.area(Area.SHIP_SAILING).ids(75).waterType(WaterType.WATER_FLAT)),
+	MOLES_LAIR_GROUND(p -> p.ids(63, 65).area(Area.GIANT_MOLES_LAIR).groundMaterial(GroundMaterial.VARIED_DIRT)),
 
 	// Cutscenes
 	CANOE_CUTSCENE_GRASS(Area.CANOE_CUTSCENE, GroundMaterial.GRASS_SCROLLING, p -> p.ids(48, 50, 63)),
