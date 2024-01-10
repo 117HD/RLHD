fa52516d42dfe003d9a4ad934bc2acf742aa5616
SirFancyBacon <m.johnsondelta@gmail.com>
2024-01-09 22:07:44 -0500

    Fix floor

--- src/main/java/rs117/hd/data/materials/Underlay.java
+++ src/main/java/rs117/hd/data/materials/Underlay.java
@@ -317,6 +317,7 @@ public enum Underlay {
 	TZHAAR(72, Area.TZHAAR, GroundMaterial.VARIED_DIRT_SHINY, p -> p.shiftLightness(2)),
 
 	// Morytania
+	CROMBWICK_MANOR_FLOOR(p -> p.ids(10).area(Area.CROMBWICK_MANOR).groundMaterial(GroundMaterial.HD_WOOD_PLANKS_1)),
 	VER_SINHAZA_WATER_FIX(p -> p.ids(54).area(Area.VER_SINHAZA_WATER_FIX).waterType(WaterType.WATER).blended(false)),
 	MEIYERDITCH_MINES(111, Area.MEIYERDITCH_MINES, GroundMaterial.ROCKY_CAVE_FLOOR),
 	BARROWS_DIRT(GroundMaterial.DIRT, p -> p
