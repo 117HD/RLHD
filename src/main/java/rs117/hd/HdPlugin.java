117HD/RLHD

commit e7097c4b37917503381b883a2c5bceb6cab62d4a

Fix model cache issue with box UVs
Since box UVs depend on vertex positions, we need to include the vertex
hash in the UV hash.

Additionally, this clarifies that model override rotation only works
properly for static scene models.

src/main/java/rs117/hd/HdPlugin.java
@@ -2807,10 +2807,8 @@ public class HdPlugin extends Plugin implements DrawCallbacks {
 				modelHasher.setModel(model);
 				// Disable model batching for models which have been excluded from the scene buffer,
 				// because we want to avoid having to fetch the model override
-				if (configModelBatching && offsetModel.getSceneId() != SceneUploader.EXCLUDED_FROM_SCENE_BUFFER) {
-					batchHash = modelHasher.calculateVertexCacheHash(ModelOverride.NONE);
+				if (configModelBatching && offsetModel.getSceneId() != SceneUploader.EXCLUDED_FROM_SCENE_BUFFER)
 					modelOffsets = frameModelInfoMap.get(batchHash);
-				}
 			}
 			if (enableDetailedTimers)
 				frameTimer.end(Timer.MODEL_BATCHING);

src/main/java/rs117/hd/model/ModelHasher.java
@@ -13,6 +13,8 @@ public class ModelHasher {
 	@Inject
 	private HdPlugin plugin;
 
+	public long vertexHash;
+
 	private Model model;
 	private int faceCount;
 	private long faceColorsOneHash;
@@ -134,9 +136,11 @@ public class ModelHasher {
 				}
 			}
 		}
+
+		vertexHash = calculateVertexCacheHash();
 	}
 
-	public long calculateVertexCacheHash(@NonNull ModelOverride modelOverride) {
+	public long calculateVertexCacheHash() {
 		long h = faceCount;
 		h = h * 31L + faceColorsOneHash;
 		h = h * 31L + faceColorsTwoHash;
@@ -154,7 +158,6 @@ public class ModelHasher {
 		h = h * 31L + model.getOverrideHue();
 		h = h * 31L + model.getOverrideSaturation();
 		h = h * 31L + model.getOverrideLuminance();
-		h = h * 31L + modelOverride.rotate;
 		return h;
 	}
 
@@ -173,6 +176,7 @@ public class ModelHasher {
 		long h = faceCount;
 		h = h * 31L + (modelOverride.uvType == UvType.VANILLA || modelOverride.retainVanillaUvs ? textureTrianglesHash : 0);
 		h = h * 31L + (modelOverride.uvType.orientationDependent ? orientation : 0);
+		h = h * 31L + (modelOverride.uvType == UvType.BOX ? vertexHash : 0);
 		h = h * 31L + modelOverride.hashCode();
 		h = h * 31L + faceTexturesHash;
 		return h;

src/main/java/rs117/hd/model/ModelPusher.java
@@ -215,7 +215,7 @@ public class ModelPusher {
 		if (shouldCache) {
 			assert client.isClientThread() : "Model caching isn't thread-safe";
 
-			vertexHash = modelHasher.calculateVertexCacheHash(modelOverride);
+			vertexHash = modelHasher.vertexHash;
 			IntBuffer vertexData = this.modelCache.getIntBuffer(vertexHash);
 			foundCachedVertexData = vertexData != null && vertexData.remaining() == bufferSize;
 			if (foundCachedVertexData) {

commit 23fb1eaf1552cdee0445e4e27991af85ae04baa7

Fix model batching bug introduced by e7097c4b

src/main/java/rs117/hd/HdPlugin.java
@@ -2817,8 +2817,10 @@ public class HdPlugin extends Plugin implements DrawCallbacks {
 				modelHasher.setModel(model);
 				// Disable model batching for models which have been excluded from the scene buffer,
 				// because we want to avoid having to fetch the model override
-				if (configModelBatching && offsetModel.getSceneId() != SceneUploader.EXCLUDED_FROM_SCENE_BUFFER)
+				if (configModelBatching && offsetModel.getSceneId() != SceneUploader.EXCLUDED_FROM_SCENE_BUFFER) {
+					batchHash = modelHasher.vertexHash;
 					modelOffsets = frameModelInfoMap.get(batchHash);
+				}
 			}
 			if (enableDetailedTimers)
 				frameTimer.end(Timer.MODEL_BATCHING);