117HD/RLHD

commit 524af1d9b96bce9b7d89049e68b8334267b4bdab

Fix all duplicate and impostor lights

src/main/java/rs117/hd/scene/LightManager.java
@@ -713,7 +713,7 @@ public class LightManager {
 		if (tileObject instanceof GameObject) {
 			var def = client.getObjectDefinition(id);
 			if (def.getImpostorIds() != null) {
-				// Add a light for every possible impostor object
+				// Add a light for every possible impostor, and make the currently active one visible
 				for (int impostorId : def.getImpostorIds())
 					addObjectLight(sceneContext, tileObject, impostorId, plane, sizeX, sizeY, orientation);
 				return;
@@ -737,13 +737,14 @@ public class LightManager {
 			if (tileObject.getPlane() <= -1)
 				continue;
 
-			// prevent the same light from being spawned more than once per object
+			// prevent duplicate lights from being spawned for the same object & impostor combination
 			long hash = tileObjectHash(tileObject);
 			boolean isDuplicate = sceneContext.lights.stream()
 				.anyMatch(light -> {
 					boolean sameObject = light.object == tileObject || hash == tileObjectHash(light.object);
 					boolean sameLight = light.def == lightDef;
-					return sameObject && sameLight;
+					boolean sameImpostor = light.impostorObjectId == objectId;
+					return sameObject && sameLight && sameImpostor;
 				});
 			if (isDuplicate)
 				continue;

src/main/java/rs117/hd/overlays/TileInfoOverlay.java
@@ -143,7 +143,7 @@ public class TileInfoOverlay extends net.runelite.client.ui.overlay.Overlay impl
 							Tile tile = tiles[z][x][y];
 							boolean shouldDraw = tile != null && (isBridge == 0 || tile.getBridge() != null);
 							if (shouldDraw && drawTileInfo(g, sceneContext, tile)) {
-								if (isBridge == 0 && aabbMarkingStage < 2) {
+								if (aabbMarkingStage < 2) {
 									int tileZ = tile.getRenderLevel();
 									markedWorldPoints[aabbMarkingStage] = sceneContext.extendedSceneToWorld(x, y, tileZ);
 									markedHeights[aabbMarkingStage] = heights[tileZ][x][y];

commit eda853066819a28bab559d4ffd6966a7ae22f524

Fix light duplication issue
Fixes bug introduced by 524af1d9b96bce9b7d89049e68b8334267b4bdab

src/main/java/rs117/hd/scene/LightManager.java
@@ -193,12 +193,12 @@ public class LightManager {
 
 			if (light.object != null) {
 				light.visible = true;
-				if (light.impostorObjectId != 0) {
+				if (light.isImpostor) {
 					var def = client.getObjectDefinition(light.object.getId());
 					if (def.getImpostorIds() != null) {
 						// Only show the light if the impostor is currently active
 						var impostor = def.getImpostor();
-						light.visible = impostor != null && impostor.getId() == light.impostorObjectId;
+						light.visible = impostor != null && impostor.getId() == light.objectId;
 					}
 				}
 			} else if (light.projectile != null) {
@@ -715,18 +715,19 @@ public class LightManager {
 			if (def.getImpostorIds() != null) {
 				// Add a light for every possible impostor, and make the currently active one visible
 				for (int impostorId : def.getImpostorIds())
-					addObjectLight(sceneContext, tileObject, impostorId, plane, sizeX, sizeY, orientation);
+					addObjectLight(sceneContext, tileObject, impostorId, true, plane, sizeX, sizeY, orientation);
 				return;
 			}
 		}
 
-		addObjectLight(sceneContext, tileObject, tileObject.getId(), plane, sizeX, sizeY, orientation);
+		addObjectLight(sceneContext, tileObject, tileObject.getId(), false, plane, sizeX, sizeY, orientation);
 	}
 
 	private void addObjectLight(
 		SceneContext sceneContext,
 		TileObject tileObject,
 		int objectId,
+		boolean isImpostor,
 		int plane,
 		int sizeX,
 		int sizeY,
@@ -743,8 +744,8 @@ public class LightManager {
 				.anyMatch(light -> {
 					boolean sameObject = light.object == tileObject || hash == tileObjectHash(light.object);
 					boolean sameLight = light.def == lightDef;
-					boolean sameImpostor = light.impostorObjectId == objectId;
-					return sameObject && sameLight && sameImpostor;
+					boolean sameObjectId = light.objectId == objectId;
+					return sameObject && sameLight && sameObjectId;
 				});
 			if (isDuplicate)
 				continue;
@@ -752,8 +753,9 @@ public class LightManager {
 			int localPlane = tileObject.getPlane();
 			Light light = new Light(lightDef);
 			light.plane = localPlane;
-			if (objectId != tileObject.getId()) {
-				light.impostorObjectId = objectId;
+			light.objectId = objectId;
+			if (isImpostor) {
+				light.isImpostor = true;
 				light.visible = false;
 			}
 

src/main/java/rs117/hd/scene/lights/Light.java
@@ -23,9 +23,9 @@ public class Light
 	public float despawnDelay;
 
 	public float elapsedTime;
-	public int impostorObjectId;
 	public boolean visible = true;
-	public boolean markedForRemoval = false;
+	public boolean isImpostor;
+	public boolean markedForRemoval;
 	public float scheduledDespawnTime = -1;
 
 	public WorldPoint worldPoint;
@@ -34,13 +34,14 @@ public class Light
 	public int z;
 	public int plane;
 	public int distanceSquared = 0;
-	public boolean belowFloor = false;
-	public boolean aboveFloor = false;
+	public boolean belowFloor;
+	public boolean aboveFloor;
 
 	public Actor actor;
 	public Projectile projectile;
 	public TileObject object;
 	public GraphicsObject graphicsObject;
+	public int objectId = -1;
 	public int spotAnimId = -1;
 
 	public Light(LightDefinition def) {