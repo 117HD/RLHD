1ec38a5436d8f2a3b1123c8416581e66237bb5aa
Mark7625 <72366279+Mark7625@users.noreply.github.com>
2024-03-24 21:38:53 +0000

    Revert "Update FinishingSpotHandler.java"
    This reverts commit 0c797b773145d5c190d3de6ebf64caef316a957d.

--- src/main/java/rs117/hd/scene/FinishingSpotHandler.java
+++ src/main/java/rs117/hd/scene/FinishingSpotHandler.java
@@ -24,7 +24,7 @@ public class FinishingSpotHandler {
 		1544, 2146, 2653, 2654, 2655, 3317, 3417, 3418, 3419, 3657, 3913, 3914, 3915, 4079, 4080, 4081, 4082,
 		4316, 4476, 4477, 4710, 4711, 4712, 4713, 4714, 5233, 5234, 5820, 5821, 6731, 6825, 7155, 7199, 7200,
 		7323, 7459, 7460, 7461, 7462, 7463, 7464, 7465, 7466, 7467, 7468, 7469, 7470, 7946, 7947, 8524, 8525,
-		8526, 8527, 9171, 9172, 9173, 9174, 9478, 12267 , 4928
+		8526, 8527, 9171, 9172, 9173, 9174, 9478, 12267
 	);
 	private static final int FISHING_SPOT_MODEL = 41238;
 	private static final int FISHING_SPOT_ANIMATION = 10793;
@@ -80,13 +80,9 @@ public class FinishingSpotHandler {
 			Tile tile = client.getScene().getTiles()[npc.getWorldLocation().getPlane()][pos.getSceneX()][pos.getSceneY()];
 			WaterType waterType = tileOverrideManager.getOverride(client.getScene(), tile).waterType;
 
-			if (npc.getId() == 4928) {
-				createRuneLiteObject1(Color.decode("#ec9f13"));
-			} else {
-				RuneLiteObject fishingSpot = createRuneLiteObject(waterType.fishingColor);
-				fishingSpot.setLocation(npc.getLocalLocation(), 0);
-				npcIndexToModel.put(npc.getIndex(), fishingSpot);
-			}
+			RuneLiteObject fishingSpot = createRuneLiteObject(waterType.fishingColor);
+			fishingSpot.setLocation(npc.getLocalLocation(), 0);
+			npcIndexToModel.put(npc.getIndex(), fishingSpot);
 		}
 	}
 
@@ -97,22 +93,6 @@ public class FinishingSpotHandler {
 		fishingSpot.setActive(true);
 		fishingSpot.setShouldLoop(true);
 		ModelData modelData = client.loadModelData(FISHING_SPOT_MODEL).cloneVertices();
-		ModelData data = color != null ? modelData.cloneColors() : modelData;
-		if (color != null) {
-			applyColorToModel(data, color);
-		}
-		fishingSpot.setModel(data.light());
-
-		return fishingSpot;
-	}
-
-	private RuneLiteObject createRuneLiteObject1(Color color) {
-		RuneLiteObject fishingSpot = client.createRuneLiteObject();
-		fishingSpot.setAnimation(client.loadAnimation(525));
-		fishingSpot.setDrawFrontTilesFirst(false);
-		fishingSpot.setActive(true);
-		fishingSpot.setShouldLoop(true);
-		ModelData modelData = client.loadModelData(2331).cloneVertices();
 		ModelData data = color != null ? modelData.cloneColors() : modelData;
 		if (color != null) {
 			applyColorToModel(data, color);
