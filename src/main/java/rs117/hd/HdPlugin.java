117HD/RLHD

commit ff92de505b3fd32453160cf7f5395d7c0735a0f0

Move uniforms into UBOs (#585)
* Abstract CPU UBO Class

A Java Class to help represent a UBO in GLSL.
Builds an array of UBOEntry which can be written to, marked as dirty and at the point of upload to the GPU it will maintain alignment between different Scalar types to match std140 layout rules.

* Only upload the CPU ByteBuffer if we've written anything to it, otherwise we don't need to bind the buffer and upload.

* Move Scene Uniforms into Global UBO

Moved all SceneUniforms into a single UBO, since it contains common Uniforms that can be utilised between different shader passes

* Moved UI Uniforms into UI UBO

* Refactored Shadow Shaders to use Global UBO rather than duplicating the Uniforms

* Added Exception logging
Fixed UBO Underwriting, Alloc to Pow2 to make sure we always have enough space

* Refactored UniformBuffer away from Generics due to not being able to use Literals.

Property Class now writes directly to the ByteBuffer, rather than storing a local value.

* Added Int/Float Vec1/2/3/4 Array Support

* Struct & Struct Array Support

* Waterline Tracking

Added LowTide/HighTide to track the range of which bytes have been written, thus allowing us to determine the smallest slice needed to upload to the GPU

* Refactored Light UBO boilerplate to use new UniformBuffer Class

* Added OpenCL Support

Refactored naming

* Removed Camera UBO Boilerplate and refactored it to use the new UniformBuffer Class

* OpenCL Bug Fixes

Refactored Size to use the one on GLBuffer

* WaterTypes UBO Refactor

WaterTypes now uses the UniformBuffer Class

* Make sure structs align to 16 bytes

* Refactor Materials UBO

Materials UBO now uses the UniformBuffer class

src/main/java/rs117/hd/opengl/GlobalUniforms.java
@@ -1,0 +1,56 @@
+package rs117.hd.opengl;
+
+public class GlobalUniforms extends UniformBuffer {
+	public GlobalUniforms() {
+		super("Global");
+	}
+
+	public Property cameraPos = addProperty(PropertyType.FVec3, "cameraPos");
+	public Property expandedMapLoadingChunks = addProperty(PropertyType.Int, "expandedMapLoadingChunks");
+	public Property drawDistance = addProperty(PropertyType.Float, "drawDistance");
+	public Property elapsedTime = addProperty(PropertyType.Float, "elapsedTime");
+
+	public Property colorBlindnessIntensity = addProperty(PropertyType.Float, "colorBlindnessIntensity");
+	public Property gammaCorrection = addProperty(PropertyType.Float, "gammaCorrection");
+	public Property saturation = addProperty(PropertyType.Float, "saturation");
+	public Property contrast = addProperty(PropertyType.Float, "contrast");
+
+	public Property ambientColor = addProperty(PropertyType.FVec3, "ambientColor");
+	public Property ambientStrength = addProperty(PropertyType.Float, "ambientStrength");
+
+	public Property lightColor = addProperty(PropertyType.FVec3, "lightColor");
+	public Property lightStrength = addProperty(PropertyType.Float, "lightStrength");
+
+	public Property underglowColor = addProperty(PropertyType.FVec3, "underglowColor");
+	public Property underglowStrength = addProperty(PropertyType.Float, "underglowStrength");
+
+	public Property projectionMatrix = addProperty(PropertyType.Mat4, "projectionMatrix");
+	public Property lightProjectionMatrix = addProperty(PropertyType.Mat4, "lightProjectionMatrix");
+
+	public Property useFog = addProperty(PropertyType.Int, "useFog");
+	public Property fogDepth = addProperty(PropertyType.Float, "fogDepth");
+	public Property fogColor = addProperty(PropertyType.FVec3, "fogColor");
+	public Property groundFogStart = addProperty(PropertyType.Float, "groundFogStart");
+	public Property groundFogEnd = addProperty(PropertyType.Float, "groundFogEnd");
+	public Property groundFogOpacity = addProperty(PropertyType.Float, "groundFogOpacity");
+
+	public Property pointLightsCount = addProperty(PropertyType.Int, "pointLightsCount");
+	public Property lightningBrightness = addProperty(PropertyType.Float, "lightningBrightness");
+	public Property lightDir = addProperty(PropertyType.FVec3, "lightDir");
+
+	public Property shadowMaxBias = addProperty(PropertyType.Float, "shadowMaxBias");
+	public Property shadowsEnabled = addProperty(PropertyType.Int, "shadowsEnabled");
+
+	public Property waterColorLight = addProperty(PropertyType.FVec3, "waterColorLight");
+	public Property waterColorMid = addProperty(PropertyType.FVec3, "waterColorMid");
+	public Property waterColorDark = addProperty(PropertyType.FVec3, "waterColorDark");
+
+	public Property underwaterEnvironment = addProperty(PropertyType.Int, "underwaterEnvironment");
+	public Property underwaterCaustics = addProperty(PropertyType.Int, "underwaterCaustics");
+	public Property underwaterCausticsColor = addProperty(PropertyType.FVec3, "underwaterCausticsColor");
+	public Property underwaterCausticsStrength = addProperty(PropertyType.Float, "underwaterCausticsStrength");
+
+	public Property colorFilterPrevious = addProperty(PropertyType.Int, "colorFilterPrevious");
+	public Property colorFilter = addProperty(PropertyType.Int, "colorFilter");
+	public Property colorFilterFade = addProperty(PropertyType.FVec3, "colorFilterFade");
+}

src/main/java/rs117/hd/config/MaxDynamicLights.java
@@ -36,6 +36,15 @@ public enum MaxDynamicLights
 	SOME("Some (50)", 50),
 	MANY("Many (100)", 100);
 
+	public static final int MAX_LIGHTS;
+
+	static {
+		int max = 0;
+		for (var e : values())
+			max = Math.max(max, e.value);
+		MAX_LIGHTS = max;
+	}
+
 	private final String name;
 	private final int value;
 

src/main/java/rs117/hd/opengl/CameraBuffer.java
@@ -1,0 +1,16 @@
+package rs117.hd.opengl;
+
+public class CameraBuffer extends UniformBuffer {
+	public CameraBuffer() {
+		super("Camera");
+	}
+
+	public Property yaw = addProperty(PropertyType.Float, "yaw");
+	public Property pitch = addProperty(PropertyType.Float, "pitch");
+	public Property centerX = addProperty(PropertyType.Int, "centerX");
+	public Property centerY = addProperty(PropertyType.Int, "centerY");
+	public Property zoom = addProperty(PropertyType.Int, "zoom");
+	public Property cameraX = addProperty(PropertyType.Float, "cameraX");
+	public Property cameraY = addProperty(PropertyType.Float, "cameraY");
+	public Property cameraZ = addProperty(PropertyType.Float, "cameraZ");
+}

src/main/java/rs117/hd/opengl/LightsBuffer.java
@@ -1,0 +1,16 @@
+package rs117.hd.opengl;
+
+import rs117.hd.config.MaxDynamicLights;
+
+public class LightsBuffer extends UniformBuffer {
+	public LightsBuffer() {
+		super("Lights");
+	}
+
+	public LightStruct[] lights = addStructs(new LightStruct[MaxDynamicLights.MAX_LIGHTS], LightStruct::new);
+
+	public static class LightStruct extends UniformBuffer.StructProperty {
+		public Property position = addProperty(PropertyType.FVec4, "position");
+		public Property color = addProperty(PropertyType.FVec3, "color");
+	}
+}

src/main/java/rs117/hd/opengl/MaterialsBuffer.java
@@ -1,0 +1,29 @@
+package rs117.hd.opengl;
+
+import rs117.hd.data.materials.Material;
+
+public class MaterialsBuffer extends UniformBuffer{
+	public MaterialsBuffer() {
+		super("Materials");
+	}
+
+	public MaterialStruct[] materials = addStructs(new MaterialStruct[Material.values().length], MaterialStruct::new);
+
+	public static class MaterialStruct extends StructProperty {
+		public Property colorMap = addProperty(PropertyType.Int, "colorMap");
+		public Property normalMap = addProperty(PropertyType.Int, "normalMap");
+		public Property displacementMap = addProperty(PropertyType.Int, "displacementMap");
+		public Property roughnessMap = addProperty(PropertyType.Int, "roughnessMap");
+		public Property ambientOcclusionMap = addProperty(PropertyType.Int, "ambientOcclusionMap");
+		public Property flowMap = addProperty(PropertyType.Int, "flowMap");
+		public Property flags = addProperty(PropertyType.Int, "flags");
+		public Property brightness = addProperty(PropertyType.Float, "brightness");
+		public Property displacementScale = addProperty(PropertyType.Float, "displacementScale");
+		public Property specularStrength = addProperty(PropertyType.Float, "specularStrength");
+		public Property specularGloss = addProperty(PropertyType.Float, "specularGloss");
+		public Property flowMapStrength = addProperty(PropertyType.Float, "flowMapStrength");
+		public Property flowMapDuration = addProperty(PropertyType.FVec2, "flowMapDuration");
+		public Property scrollDuration = addProperty(PropertyType.FVec2, "scrollDuration");
+		public Property textureScale = addProperty(PropertyType.FVec3, "textureScale");
+	}
+}

src/main/java/rs117/hd/opengl/UIUniforms.java
@@ -1,0 +1,17 @@
+package rs117.hd.opengl;
+
+public class UIUniforms extends UniformBuffer {
+	public UIUniforms() {
+		super("UI");
+	}
+
+	public Property sourceDimensions = addProperty(PropertyType.IVec2, "sourceDimensions");
+	public Property targetDimensions = addProperty(PropertyType.IVec2, "targetDimensions");
+
+	public Property colorBlindnessIntensity = addProperty(PropertyType.Float, "colorBlindnessIntensity");
+	public Property alphaOverlay = addProperty(PropertyType.FVec4, "alphaOverlay");
+
+	public Property showGammaCalibration = addProperty(PropertyType.Int, "showGammaCalibration");
+	public Property gammaCalibrationTimer = addProperty(PropertyType.Float, "gammaCalibrationTimer");
+	public Property gammaCorrection = addProperty(PropertyType.Float, "gammaCorrection");
+}

src/main/java/rs117/hd/opengl/UniformBuffer.java
@@ -1,0 +1,299 @@
+package rs117.hd.opengl;
+
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.lwjgl.BufferUtils;
+import rs117.hd.HdPlugin;
+import rs117.hd.opengl.compute.OpenCLManager;
+import rs117.hd.utils.HDUtils;
+import rs117.hd.utils.buffer.GLBuffer;
+
+import static org.lwjgl.opencl.CL10.*;
+import static org.lwjgl.opengl.GL31C.*;
+import static org.lwjgl.opengl.GL43C.*;
+
+@Slf4j
+public abstract class UniformBuffer {
+	protected enum PropertyType {
+		Int(4, 4, 1),
+		IVec2(8, 8, 2),
+		IVec3(12, 16, 3),
+		IVec4(16, 16, 4),
+
+		IntArray(4, 16, 1, true),
+		IVec2Array(8, 16, 2, true),
+		IVec3Array(12, 16, 3, true),
+		IVec4Array(16, 16, 4, true),
+
+		Float(4, 4, 1),
+		FVec2(8, 8, 2),
+		FVec3(12, 16, 3),
+		FVec4(16, 16, 4),
+
+		FloatArray(4, 16, 1, true),
+		FVec2Array(8, 16, 2, true),
+		FVec3Array(12, 16, 3, true),
+		FVec4Array(16, 16, 4, true),
+
+		Mat3(36, 16, 9),
+		Mat4(64, 16, 16);
+
+		private final int size;
+		private final int alignment;
+		private final int elementSize;
+		private final int elementCount;
+		private final boolean isArray;
+
+		PropertyType(int size, int alignment, int elementCount) {
+			this.size = size;
+			this.alignment = alignment;
+			this.elementSize = size / elementCount;
+			this.elementCount = elementCount;
+			this.isArray = false;
+		}
+
+		PropertyType(int size, int alignment, int elementCount, boolean isArray) {
+			this.size = size;
+			this.alignment = alignment;
+			this.elementSize = size / elementCount;
+			this.elementCount = elementCount;
+			this.isArray = isArray;
+		}
+	}
+
+	@AllArgsConstructor
+	@RequiredArgsConstructor
+	public static class Property {
+		private UniformBuffer owner;
+		private int position;
+		private final PropertyType type;
+		private final String name;
+
+		public final void set(int value) {
+			if (type != PropertyType.Int) {
+				log.warn("UBO {} - Incorrect Setter(int) called for Property: {}", owner.glBuffer.name, name);
+				return;
+			}
+
+			if (owner.data.getInt(position) != value) {
+				owner.data.putInt(position, value);
+				owner.markWaterLine(position, type.size);
+			}
+		}
+
+		public final void set(int... values) {
+			if (type != PropertyType.IVec2 && type != PropertyType.IVec3 && type != PropertyType.IVec4) {
+				log.warn("UBO {} - Incorrect Setter(int[]) called for Property: {}", owner.glBuffer.name, name);
+				return;
+			}
+
+			if (values == null) {
+				log.warn("UBO {} - Setter(int[]) was provided with null value for Property: {}", owner.glBuffer.name, name);
+				return;
+			}
+
+			if ((type.isArray && (values.length % type.elementCount) != 0) || values.length != type.elementCount) {
+				log.warn(
+					"UBO {} - Setter(int[]) was provided with incorrect number of elements for Property: {}",
+					owner.glBuffer.name,
+					name
+				);
+				return;
+			}
+
+			if (owner.data == null) {
+				log.warn("UBO {} - Hasn't been initialized yet!", owner.glBuffer.name);
+				return;
+			}
+
+			int elementCount = type.isArray ? values.length : type.elementCount;
+			for (int elementIdx = 0, offset = position; elementIdx < elementCount; elementIdx++, offset += type.elementSize) {
+				if (owner.data.getInt(offset) != values[elementIdx]) {
+					owner.data.putInt(offset, values[elementIdx]);
+					owner.markWaterLine(offset, type.elementSize);
+				}
+			}
+		}
+
+		public final void set(float value) {
+			if (type != PropertyType.Float) {
+				log.warn("UBO {} - Incorrect Setter(float) called for Property: {}", owner.glBuffer.name, name);
+				return;
+			}
+
+			if (owner.data.getFloat(position) != value) {
+				owner.data.putFloat(position, value);
+				owner.markWaterLine(position, type.size);
+			}
+		}
+
+		public final void set(float... values) {
+			if (type != PropertyType.FVec2 &&
+				type != PropertyType.FVec3 &&
+				type != PropertyType.FVec4 &&
+				type != PropertyType.Mat3 &&
+				type != PropertyType.Mat4
+			) {
+				log.warn("UBO {} - Incorrect Setter(float[]) called for Property: {}", owner.glBuffer.name, name);
+				return;
+			}
+
+			if (values == null) {
+				log.warn("UBO {} - Setter(float[]) was provided with null value for Property: {}", owner.glBuffer.name, name);
+				return;
+			}
+
+			if ((type.isArray && (values.length % type.elementCount) != 0) || values.length != type.elementCount) {
+				log.warn(
+					"UBO {} - Setter(float[]) was provided with incorrect number of elements for Property: {}",
+					owner.glBuffer.name,
+					name
+				);
+				return;
+			}
+
+			if (owner.data == null) {
+				log.warn("UBO {} - Hasn't been initialized yet!", owner.glBuffer.name);
+				return;
+			}
+
+			int elementCount = type.isArray ? values.length : type.elementCount;
+			for (int elementIdx = 0, offset = position; elementIdx < elementCount; elementIdx++, offset += type.elementSize) {
+				if (owner.data.getFloat(offset) != values[elementIdx]) {
+					owner.data.putFloat(offset, values[elementIdx]);
+					owner.markWaterLine(offset, type.elementSize);
+				}
+			}
+		}
+	}
+
+	public interface CreateStructProperty<T extends StructProperty> {
+		T create();
+	}
+
+	public abstract static class StructProperty {
+		protected List<Property> properties = new ArrayList<>();
+
+		protected final Property addProperty(PropertyType type, String name) {
+			Property property = new Property(type, name);
+			properties.add(property);
+			return property;
+		}
+	}
+
+	@Getter
+	private final GLBuffer glBuffer;
+	private ByteBuffer data;
+
+	private int dirtyLowTide = Integer.MAX_VALUE;
+	private int dirtyHighTide = Integer.MIN_VALUE;
+
+	public UniformBuffer(String name) {
+		glBuffer = new GLBuffer("UBO " + name);
+	}
+
+	protected final <T extends StructProperty> T addStruct(T newStructProp) {
+		for (Property property : newStructProp.properties)
+			appendToBuffer(property);
+
+		// Structs need to align to 16 bytes
+		glBuffer.size += (int) (16 - (glBuffer.size % 16)) % 16;
+
+		newStructProp.properties.clear();
+		return newStructProp;
+	}
+
+	protected final <T extends StructProperty> T[] addStructs(T[] newStructPropArray, CreateStructProperty<T> createFunction) {
+		for (int i = 0; i < newStructPropArray.length; i++) {
+			newStructPropArray[i] = createFunction.create();
+			addStruct(newStructPropArray[i]);
+		}
+
+		return newStructPropArray;
+	}
+
+	protected Property addProperty(PropertyType type, String name) {
+		return appendToBuffer(new Property(type, name));
+	}
+
+	private Property appendToBuffer(Property property) {
+		property.owner = this;
+
+		int Padding = (int) (property.type.alignment - (glBuffer.size % property.type.alignment)) % property.type.alignment;
+		property.position = (int) glBuffer.size + Padding;
+
+		glBuffer.size += property.type.size + Padding;
+
+		return property;
+	}
+
+	private void markWaterLine(int position, int size) {
+		dirtyLowTide = Math.min(dirtyLowTide, position);
+		dirtyHighTide = Math.max(dirtyHighTide, position + size);
+	}
+
+	public final void initialize(int uniformBlockIndex, OpenCLManager openCLManager) {
+		initialize(uniformBlockIndex);
+
+		if (openCLManager != null)
+			openCLManager.recreateCLBuffer(glBuffer, CL_MEM_READ_ONLY);
+	}
+
+	public final void initialize(int UniformBlockIndex) {
+		if (data != null)
+			destroy();
+
+		glBuffer.size = HDUtils.ceilPow2(glBuffer.size);
+		data = BufferUtils.createByteBuffer((int) glBuffer.size);
+		glBuffer.glBufferId = glGenBuffers();
+
+		glBindBuffer(GL_UNIFORM_BUFFER, glBuffer.glBufferId);
+		glBufferData(GL_UNIFORM_BUFFER, glBuffer.size, GL_STATIC_DRAW);
+		glBufferSubData(GL_UNIFORM_BUFFER, 0, data);
+		glBindBuffer(GL_UNIFORM_BUFFER, 0);
+
+		glBindBufferBase(GL_UNIFORM_BUFFER, UniformBlockIndex, glBuffer.glBufferId);
+
+		if (HdPlugin.glCaps.OpenGL43 && log.isDebugEnabled())
+			glObjectLabel(GL_BUFFER, glBuffer.glBufferId, glBuffer.name);
+	}
+
+	public final void upload() {
+		if (data == null || dirtyHighTide <= 0 || dirtyLowTide >= glBuffer.size)
+			return;
+
+		data.position(dirtyLowTide);
+		data.limit(dirtyHighTide);
+
+		glBindBuffer(GL_UNIFORM_BUFFER, glBuffer.glBufferId);
+		glBufferSubData(GL_UNIFORM_BUFFER, dirtyLowTide, data);
+
+		glBindBuffer(GL_UNIFORM_BUFFER, 0);
+
+		data.position(0);
+		data.limit((int) glBuffer.size);
+
+		dirtyLowTide = Integer.MAX_VALUE;
+		dirtyHighTide = Integer.MIN_VALUE;
+	}
+
+	public final void destroy() {
+		if (data == null)
+			return;
+
+		glDeleteBuffers(glBuffer.glBufferId);
+
+		data = null;
+		glBuffer.glBufferId = -1;
+
+		if (glBuffer.clBuffer != 0) {
+			clReleaseMemObject(glBuffer.clBuffer);
+			glBuffer.clBuffer = 0;
+		}
+	}
+}

commit fdfa16b5d6899eac46166cbd03f9017b20ed4045

Fix color filters & reduce fade duration
Color filters were accidentally broken in
ff92de505b3fd32453160cf7f5395d7c0735a0f0.

src/main/java/rs117/hd/HdPlugin.java
@@ -186,7 +186,7 @@ public class HdPlugin extends Plugin implements DrawCallbacks {
 
 	public static float BUFFER_GROWTH_MULTIPLIER = 2; // can be less than 2 if trying to conserve memory
 
-	private static final float COLOR_FILTER_FADE_DURATION = 3000;
+	private static final float COLOR_FILTER_FADE_DURATION = 500;
 
 	private static final int[] eightIntWrite = new int[8];
 

src/main/java/rs117/hd/opengl/GlobalUniforms.java
@@ -52,5 +52,5 @@ public class GlobalUniforms extends UniformBuffer {
 
 	public Property colorFilterPrevious = addProperty(PropertyType.Int, "colorFilterPrevious");
 	public Property colorFilter = addProperty(PropertyType.Int, "colorFilter");
-	public Property colorFilterFade = addProperty(PropertyType.FVec3, "colorFilterFade");
+	public Property colorFilterFade = addProperty(PropertyType.Float, "colorFilterFade");
 }