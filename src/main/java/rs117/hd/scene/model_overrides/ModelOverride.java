117HD/RLHD

commit 34e74c0ae437c587a31350fe3120663b422a1b2f

Add alpha support to color-based model overrides

src/main/java/rs117/hd/scene/model_overrides/ModelOverride.java
@@ -81,11 +81,11 @@ public class ModelOverride
 
 	public transient boolean isDummy;
 	public transient Map<AABB, ModelOverride> areaOverrides;
-	public transient HslPredicate hslCondition;
+	public transient AhslPredicate ahslCondition;
 
 	@FunctionalInterface
-	public interface HslPredicate {
-		boolean test(int hsl);
+	public interface AhslPredicate {
+		boolean test(int ahsl);
 	}
 
 	public void normalize(VanillaShadowMode vanillaShadowMode) {
@@ -137,7 +137,7 @@ public class ModelOverride
 		if (colorOverrides != null) {
 			for (var override : colorOverrides) {
 				override.normalize(vanillaShadowMode);
-				override.hslCondition = parseHslConditions(override.colors);
+				override.ahslCondition = parseAhslConditions(override.colors);
 			}
 		}
 
@@ -196,7 +196,7 @@ public class ModelOverride
 			colors,
 			isDummy,
 			areaOverrides,
-			hslCondition
+			ahslCondition
 		);
 	}
 
@@ -205,9 +205,9 @@ public class ModelOverride
 		this.isDummy = isDummy;
 	}
 
-	private HslPredicate parseHslConditions(JsonElement element) {
+	private AhslPredicate parseAhslConditions(JsonElement element) {
 		if (element == null)
-			return hsl -> false;
+			return ahsl -> false;
 
 		JsonArray arr;
 		if (element.isJsonArray()) {
@@ -217,7 +217,7 @@ public class ModelOverride
 			arr.add(element);
 		}
 
-		HslPredicate combinedPredicate = null;
+		AhslPredicate combinedPredicate = null;
 
 		for (var el : arr) {
 			if (el.isJsonNull())
@@ -227,15 +227,15 @@ public class ModelOverride
 				continue;
 			}
 
-			HslPredicate condition;
+			AhslPredicate condition;
 			var prim = el.getAsJsonPrimitive();
 			if (prim.isBoolean()) {
 				boolean bool = prim.getAsBoolean();
-				condition = hsl -> bool;
+				condition = ahsl -> bool;
 			} else if (prim.isNumber()) {
 				try {
 					int targetHsl = prim.getAsInt();
-					condition = hsl -> hsl == targetHsl;
+					condition = ahsl -> ahsl == (targetHsl & 0xFFFF);
 				} catch (Exception ex) {
 					log.warn("Expected integer, but got {} in override '{}'", el, description);
 					continue;
@@ -245,7 +245,7 @@ public class ModelOverride
 
 				if (Props.DEVELOPMENT) {
 					// Ensure all variables are defined
-					final Set<String> knownVariables = Set.of("h", "s", "l", "hsl");
+					final Set<String> knownVariables = Set.of("a", "h", "s", "l", "hsl", "ahsl");
 					for (var variable : expr.variables)
 						if (!knownVariables.contains(variable))
 							throw new IllegalStateException(
@@ -253,17 +253,21 @@ public class ModelOverride
 				}
 
 				var predicate = expr.toPredicate();
-				condition = hsl -> predicate.test(key -> {
+				condition = ahsl -> predicate.test(key -> {
 					switch (key) {
-						default:
-						case "hsl":
-							return hsl;
+						case "a":
+							return ahsl >>> 16 & 0xFF;
 						case "h":
-							return hsl >>> 10 & 0x3F;
+							return ahsl >>> 10 & 0x3F;
 						case "s":
-							return hsl >>> 7 & 0x7;
+							return ahsl >>> 7 & 0x7;
 						case "l":
-							return hsl & 0x7F;
+							return ahsl & 0x7F;
+						case "ahsl":
+							return ahsl;
+						case "hsl":
+						default:
+							return ahsl & 0xFFFF;
 					}
 				});
 			} else {
@@ -275,12 +279,12 @@ public class ModelOverride
 				combinedPredicate = condition;
 			} else {
 				var prev = combinedPredicate;
-				combinedPredicate = hsl -> prev.test(hsl) || condition.test(hsl);
+				combinedPredicate = ahsl -> prev.test(ahsl) || condition.test(ahsl);
 			}
 		}
 
 		if (combinedPredicate == null)
-			return hsl -> false;
+			return ahsl -> false;
 
 		return combinedPredicate;
 	}

src/main/java/rs117/hd/model/ModelPusher.java
@@ -325,6 +325,8 @@ public class ModelPusher {
 			if (plugin.enableDetailedTimers)
 				frameTimer.begin(Timer.MODEL_PUSHING_UV);
 
+			int[] faceColors = model.getFaceColors1();
+			byte[] faceTransparencies = model.getFaceTransparencies();
 			for (int face = 0; face < faceCount; face++) {
 				UvType uvType = UvType.GEOMETRY;
 				Material material = baseMaterial;
@@ -347,9 +349,9 @@ public class ModelPusher {
 				}
 
 				if (modelOverride.colorOverrides != null) {
-					int hsl = model.getFaceColors1()[face];
+					int ahsl = (faceTransparencies == null ? 0xFF : 0xFF - (faceTransparencies[face] & 0xFF)) << 16 | faceColors[face];
 					for (var override : modelOverride.colorOverrides) {
-						if (override.hslCondition.test(hsl)) {
+						if (override.ahslCondition.test(ahsl)) {
 							faceOverride = override;
 							material = faceOverride.baseMaterial;
 							break;

src/main/java/rs117/hd/overlays/TileInfoOverlay.java
@@ -57,8 +57,8 @@ import rs117.hd.utils.HDUtils;
 import rs117.hd.utils.ModelHash;
 import rs117.hd.utils.Vector;
 
-import static net.runelite.api.Constants.SCENE_SIZE;
 import static net.runelite.api.Constants.*;
+import static net.runelite.api.Constants.SCENE_SIZE;
 import static net.runelite.api.Perspective.*;
 import static rs117.hd.HdPlugin.ORTHOGRAPHIC_ZOOM;
 import static rs117.hd.scene.SceneContext.SCENE_OFFSET;
@@ -817,20 +817,24 @@ public class TileInfoOverlay extends Overlay implements MouseListener, MouseWhee
 							"<col=#ff0000>maybe dynamic</col>"
 				);
 			case MODE_MODEL_INFO:
-				var colors =
-					Arrays.stream(model.getFaceColors1())
-						.distinct()
-						.sorted()
-						.mapToObj(hsl -> {
-							var rawhsl = ColorUtils.unpackRawHsl(hsl);
-							return String.format(
-								"<col=%s>%5d [%2d %1d %3d] </col>",
-								ColorUtils.srgbToHex(ColorUtils.packedHslToSrgb(hsl)),
-								hsl, rawhsl[0], rawhsl[1], rawhsl[2]
-							);
-						})
-						.toArray(String[]::new);
-
+				int[] faceColors = model.getFaceColors1();
+				byte[] faceTransparencies = model.getFaceTransparencies();
+				int[] faceAhsl = new int[model.getFaceCount()];
+				for (int i = 0; i < faceAhsl.length; i++)
+					faceAhsl[i] = (faceTransparencies == null ? 0xFF : 0xFF - (faceTransparencies[i] & 0xFF)) << 16 | faceColors[i];
+				var colors = Arrays.stream(faceAhsl)
+					.distinct()
+					.sorted()
+					.mapToObj(ahsl -> {
+						var hsl = ColorUtils.unpackRawHsl(ahsl);
+						var alpha = ahsl >> 16;
+						return String.format(
+							"<col=%s>%5d [%3d %2d %1d %3d]</col>",
+							String.format("#%08x", ColorUtils.packSrgb(ColorUtils.packedHslToSrgb(ahsl)) << 8 | alpha),
+							ahsl & 0xFFFF, alpha, hsl[0], hsl[1], hsl[2]
+						);
+					})
+					.toArray(String[]::new);
 
 				int columns = clamp((int) Math.round(Math.sqrt(colors.length / 5f)), 3, 8);
 				int rows = (int) Math.ceil(colors.length / (float) columns);
@@ -1038,20 +1042,20 @@ public class TileInfoOverlay extends Overlay implements MouseListener, MouseWhee
 			end = m.end();
 
 			var hex = m.group(1);
-			int i = Integer.parseInt(hex, 16);
+			int i = (int) Long.parseLong(hex, 16);
 			int r, g, b, a = 0xFF;
 			if (hex.length() == 3) {
-				r = (i >> 8) * 2;
-				g = (i >> 4 & 0xF) * 2;
+				r = (i >>> 8) * 2;
+				g = (i >>> 4 & 0xF) * 2;
 				b = (i & 0xF) * 2;
 			} else if (hex.length() == 6) {
-				r = i >> 16;
-				g = i >> 8 & 0xFF;
+				r = i >>> 16;
+				g = i >>> 8 & 0xFF;
 				b = i & 0xFF;
 			} else if (hex.length() == 8) {
-				r = i >> 24;
-				g = i >> 16 & 0xFF;
-				b = i >> 8 & 0xFF;
+				r = i >>> 24;
+				g = i >>> 16 & 0xFF;
+				b = i >>> 8 & 0xFF;
 				a = i & 0xFF;
 			} else {
 				g2d.drawString(m.group(0), x, y);

commit e9e68c75cd88fea976cd2461940aee094da293d7

Fix raw HSL color overrides
Broken by 34e74c0ae437c587a31350fe3120663b422a1b2f.

src/main/java/rs117/hd/scene/model_overrides/ModelOverride.java
@@ -235,7 +235,7 @@ public class ModelOverride
 			} else if (prim.isNumber()) {
 				try {
 					int targetHsl = prim.getAsInt();
-					condition = ahsl -> ahsl == (targetHsl & 0xFFFF);
+					condition = ahsl -> (ahsl & 0xFFFF) == targetHsl;
 				} catch (Exception ex) {
 					log.warn("Expected integer, but got {} in override '{}'", el, description);
 					continue;