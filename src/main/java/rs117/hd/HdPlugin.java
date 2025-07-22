117HD/RLHD

commit 4962c8e7505dd952c23baf79b242d0409ecc1abd

Fix buffer sizing broken by GL buffer refactor
`updateBuffer` was incorrectly sizing buffers 4 times too large after
commit a238ece12ea098d0e409f70ee185978450c092a1.

src/main/java/rs117/hd/HdPlugin.java
@@ -3157,8 +3157,10 @@ public class HdPlugin extends Plugin implements DrawCallbacks {
 	}
 
 	private void updateBuffer(@Nonnull GLBuffer glBuffer, int target, int offset, @Nonnull IntBuffer data) {
-		glBuffer.ensureCapacity(offset, data.remaining());
-		glBufferSubData(target, offset * 4L, data);
+		long byteOffset = 4L * offset;
+		long numBytes = 4L * data.remaining();
+		glBuffer.ensureCapacity(byteOffset, numBytes);
+		glBufferSubData(target, byteOffset, data);
 	}
 
 	private void updateBuffer(@Nonnull GLBuffer glBuffer, int target, @Nonnull FloatBuffer data) {
@@ -3166,8 +3168,10 @@ public class HdPlugin extends Plugin implements DrawCallbacks {
 	}
 
 	private void updateBuffer(@Nonnull GLBuffer glBuffer, int target, int offset, @Nonnull FloatBuffer data) {
-		glBuffer.ensureCapacity(offset, data.remaining());
-		glBufferSubData(target, offset * 4L, data);
+		long byteOffset = 4L * offset;
+		long numBytes = 4L * data.remaining();
+		glBuffer.ensureCapacity(offset, numBytes);
+		glBufferSubData(target, byteOffset, data);
 	}
 
 	@Subscribe(priority = -1) // Run after the low detail plugin

src/main/java/rs117/hd/utils/buffer/GLBuffer.java
@@ -69,18 +69,18 @@ public class GLBuffer
 		ensureCapacity(0, numBytes);
 	}
 
-	public void ensureCapacity(int offset, long numBytes) {
-		long size = 4L * (offset + numBytes);
+	public void ensureCapacity(long byteOffset, long numBytes) {
+		long size = byteOffset + numBytes;
 		if (size <= this.size) {
 			glBindBuffer(target, id);
 			return;
 		}
 
 		size = HDUtils.ceilPow2(size);
-		if (log.isTraceEnabled())
-			log.trace("Buffer resize: {} {}", this, String.format("%.2f MB -> %.2f MB", this.size / 1e6, size / 1e6));
+		if (log.isDebugEnabled() && size > 1e6)
+			log.debug("Resizing buffer '{}'\t{}", name, String.format("%.2f MB -> %.2f MB", this.size / 1e6, size / 1e6));
 
-		if (offset > 0) {
+		if (byteOffset > 0) {
 			// Create a new buffer and copy the old data to it
 			int oldBuffer = id;
 			id = glGenBuffers();
@@ -88,7 +88,7 @@ public class GLBuffer
 			glBufferData(target, size, usage);
 
 			glBindBuffer(GL_COPY_READ_BUFFER, oldBuffer);
-			glCopyBufferSubData(GL_COPY_READ_BUFFER, target, 0, 0, offset * 4L);
+			glCopyBufferSubData(GL_COPY_READ_BUFFER, target, 0, 0, byteOffset);
 			glDeleteBuffers(oldBuffer);
 		} else {
 			glBindBuffer(target, id);

src/main/java/rs117/hd/utils/buffer/SharedGLBuffer.java
@@ -29,8 +29,8 @@ public class SharedGLBuffer extends GLBuffer {
 	}
 
 	@Override
-	public void ensureCapacity(int offset, long numBytes) {
-		super.ensureCapacity(offset, numBytes);
+	public void ensureCapacity(long byteOffset, long numBytes) {
+		super.ensureCapacity(byteOffset, numBytes);
 		if (OpenCLManager.context == 0)
 			return;
 

commit 582287b9216bd8432f24637e7d7bd8653ffa7a0d

Move buffer uploads to `GLBuffer`
This also fixes a bug introduced in
4962c8e7505dd952c23baf79b242d0409ecc1abd, where one of the calls to
`GLBuffer#ensureCapacity` accidentally didn't pass the byte offset.

src/main/java/rs117/hd/HdPlugin.java
@@ -48,7 +48,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.inject.Inject;
 import javax.swing.SwingUtilities;
@@ -176,7 +175,6 @@ public class HdPlugin extends Plugin implements DrawCallbacks {
 	public static final int MAX_DISTANCE = EXTENDED_SCENE_SIZE;
 	public static final int GROUND_MIN_Y = 350; // how far below the ground models extend
 	public static final int MAX_FOG_DEPTH = 100;
-	public static final int SCALAR_BYTES = 4;
 	public static final int VERTEX_SIZE = 4; // 4 ints per vertex
 	public static final int UV_SIZE = 4; // 4 floats per vertex
 	public static final int NORMAL_SIZE = 4; // 4 floats per vertex
@@ -1564,37 +1562,22 @@ public class HdPlugin extends Plugin implements DrawCallbacks {
 			sceneContext.stagingBufferVertices.flip();
 			sceneContext.stagingBufferUvs.flip();
 			sceneContext.stagingBufferNormals.flip();
-			updateBuffer(
-				hStagingBufferVertices,
-				GL_ARRAY_BUFFER,
-				dynamicOffsetVertices * VERTEX_SIZE,
-				sceneContext.stagingBufferVertices.getBuffer()
-			);
-			updateBuffer(
-				hStagingBufferUvs,
-				GL_ARRAY_BUFFER,
-				dynamicOffsetUvs * UV_SIZE,
-				sceneContext.stagingBufferUvs.getBuffer()
-			);
-			updateBuffer(
-				hStagingBufferNormals,
-				GL_ARRAY_BUFFER,
-				dynamicOffsetVertices * NORMAL_SIZE,
-				sceneContext.stagingBufferNormals.getBuffer()
-			);
+			hStagingBufferVertices.upload(sceneContext.stagingBufferVertices, dynamicOffsetVertices * 4L * VERTEX_SIZE);
+			hStagingBufferUvs.upload(sceneContext.stagingBufferUvs, dynamicOffsetUvs * 4L * UV_SIZE);
+			hStagingBufferNormals.upload(sceneContext.stagingBufferNormals, dynamicOffsetVertices * 4L * NORMAL_SIZE);
 			sceneContext.stagingBufferVertices.clear();
 			sceneContext.stagingBufferUvs.clear();
 			sceneContext.stagingBufferNormals.clear();
 
 			// Model buffers
 			modelPassthroughBuffer.flip();
-			updateBuffer(hModelPassthroughBuffer, GL_ARRAY_BUFFER, modelPassthroughBuffer.getBuffer());
+			hModelPassthroughBuffer.upload(modelPassthroughBuffer);
 			modelPassthroughBuffer.clear();
 
 			for (int i = 0; i < modelSortingBuffers.length; i++) {
 				var buffer = modelSortingBuffers[i];
 				buffer.flip();
-				updateBuffer(hModelSortingBuffers[i], GL_ARRAY_BUFFER, buffer.getBuffer());
+				hModelSortingBuffers[i].upload(buffer);
 				buffer.clear();
 			}
 
@@ -2444,21 +2427,9 @@ public class HdPlugin extends Plugin implements DrawCallbacks {
 		sceneContext.stagingBufferVertices.flip();
 		sceneContext.stagingBufferUvs.flip();
 		sceneContext.stagingBufferNormals.flip();
-		updateBuffer(
-			hStagingBufferVertices,
-			GL_ARRAY_BUFFER,
-			sceneContext.stagingBufferVertices.getBuffer()
-		);
-		updateBuffer(
-			hStagingBufferUvs,
-			GL_ARRAY_BUFFER,
-			sceneContext.stagingBufferUvs.getBuffer()
-		);
-		updateBuffer(
-			hStagingBufferNormals,
-			GL_ARRAY_BUFFER,
-			sceneContext.stagingBufferNormals.getBuffer()
-		);
+		hStagingBufferVertices.upload(sceneContext.stagingBufferVertices);
+		hStagingBufferUvs.upload(sceneContext.stagingBufferUvs);
+		hStagingBufferNormals.upload(sceneContext.stagingBufferNormals);
 		sceneContext.stagingBufferVertices.clear();
 		sceneContext.stagingBufferUvs.clear();
 		sceneContext.stagingBufferNormals.clear();
@@ -3150,28 +3121,6 @@ public class HdPlugin extends Plugin implements DrawCallbacks {
 		if (useLowMemoryMode)
 			return 0;
 		return config.expandedMapLoadingChunks();
-	}
-
-	private void updateBuffer(@Nonnull GLBuffer glBuffer, int target, @Nonnull IntBuffer data) {
-		updateBuffer(glBuffer, target, 0, data);
-	}
-
-	private void updateBuffer(@Nonnull GLBuffer glBuffer, int target, int offset, @Nonnull IntBuffer data) {
-		long byteOffset = 4L * offset;
-		long numBytes = 4L * data.remaining();
-		glBuffer.ensureCapacity(byteOffset, numBytes);
-		glBufferSubData(target, byteOffset, data);
-	}
-
-	private void updateBuffer(@Nonnull GLBuffer glBuffer, int target, @Nonnull FloatBuffer data) {
-		updateBuffer(glBuffer, target, 0, data);
-	}
-
-	private void updateBuffer(@Nonnull GLBuffer glBuffer, int target, int offset, @Nonnull FloatBuffer data) {
-		long byteOffset = 4L * offset;
-		long numBytes = 4L * data.remaining();
-		glBuffer.ensureCapacity(offset, numBytes);
-		glBufferSubData(target, byteOffset, data);
 	}
 
 	@Subscribe(priority = -1) // Run after the low detail plugin

src/main/java/rs117/hd/scene/SceneUploader.java
@@ -52,7 +52,6 @@ import static net.runelite.api.Constants.*;
 import static net.runelite.api.Constants.SCENE_SIZE;
 import static net.runelite.api.Perspective.*;
 import static rs117.hd.HdPlugin.NORMAL_SIZE;
-import static rs117.hd.HdPlugin.SCALAR_BYTES;
 import static rs117.hd.HdPlugin.UV_SIZE;
 import static rs117.hd.HdPlugin.VERTEX_SIZE;
 import static rs117.hd.scene.SceneContext.SCENE_OFFSET;
@@ -143,8 +142,8 @@ public class SceneUploader {
 			String.format(
 				"%.2f",
 				(
-					sceneContext.getVertexOffset() * (VERTEX_SIZE + NORMAL_SIZE) * SCALAR_BYTES +
-					sceneContext.getUvOffset() * UV_SIZE * SCALAR_BYTES
+					sceneContext.getVertexOffset() * 4L * (VERTEX_SIZE + NORMAL_SIZE) +
+					sceneContext.getUvOffset() * 4L * UV_SIZE
 				) / 1e6
 			)
 		);

src/main/java/rs117/hd/utils/buffer/GLBuffer.java
@@ -24,6 +24,9 @@
  */
 package rs117.hd.utils.buffer;
 
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.lwjgl.opengl.*;
@@ -70,36 +73,82 @@ public class GLBuffer
 	}
 
 	public void ensureCapacity(long byteOffset, long numBytes) {
-		long size = byteOffset + numBytes;
-		if (size <= this.size) {
+		numBytes += byteOffset;
+		if (numBytes <= size) {
 			glBindBuffer(target, id);
 			return;
 		}
 
-		size = HDUtils.ceilPow2(size);
-		if (log.isDebugEnabled() && size > 1e6)
-			log.debug("Resizing buffer '{}'\t{}", name, String.format("%.2f MB -> %.2f MB", this.size / 1e6, size / 1e6));
+		numBytes = HDUtils.ceilPow2(numBytes);
+		if (log.isDebugEnabled() && numBytes > 1e6)
+			log.debug("Resizing buffer '{}'\t{}", name, String.format("%.2f MB -> %.2f MB", size / 1e6, numBytes / 1e6));
 
 		if (byteOffset > 0) {
 			// Create a new buffer and copy the old data to it
 			int oldBuffer = id;
 			id = glGenBuffers();
 			glBindBuffer(target, id);
-			glBufferData(target, size, usage);
+			glBufferData(target, numBytes, usage);
 
 			glBindBuffer(GL_COPY_READ_BUFFER, oldBuffer);
 			glCopyBufferSubData(GL_COPY_READ_BUFFER, target, 0, 0, byteOffset);
 			glDeleteBuffers(oldBuffer);
 		} else {
 			glBindBuffer(target, id);
-			glBufferData(target, size, usage);
+			glBufferData(target, numBytes, usage);
 		}
 
-		this.size = size;
+		size = numBytes;
 
-		if (HdPlugin.GL_CAPS.OpenGL43 && log.isDebugEnabled()) {
+		if (log.isDebugEnabled() && HdPlugin.GL_CAPS.OpenGL43) {
 			GL43C.glObjectLabel(GL43C.GL_BUFFER, id, name);
 			checkGLErrors();
 		}
+	}
+
+	public void upload(ByteBuffer data) {
+		upload(data, 0);
+	}
+
+	public void upload(ByteBuffer data, long byteOffset) {
+		long numBytes = data.remaining();
+		ensureCapacity(byteOffset, numBytes);
+		glBufferSubData(target, byteOffset, data);
+	}
+
+	public void upload(IntBuffer data) {
+		upload(data, 0);
+	}
+
+	public void upload(IntBuffer data, long byteOffset) {
+		long numBytes = 4L * data.remaining();
+		ensureCapacity(byteOffset, numBytes);
+		glBufferSubData(target, byteOffset, data);
+	}
+
+	public void upload(FloatBuffer data) {
+		upload(data, 0);
+	}
+
+	public void upload(FloatBuffer data, long byteOffset) {
+		long numBytes = 4L * data.remaining();
+		ensureCapacity(byteOffset, numBytes);
+		glBufferSubData(target, byteOffset, data);
+	}
+
+	public void upload(GpuIntBuffer data) {
+		upload(data.getBuffer());
+	}
+
+	public void upload(GpuIntBuffer data, long byteOffset) {
+		upload(data.getBuffer(), byteOffset);
+	}
+
+	public void upload(GpuFloatBuffer data) {
+		upload(data.getBuffer());
+	}
+
+	public void upload(GpuFloatBuffer data, long byteOffset) {
+		upload(data.getBuffer(), byteOffset);
 	}
 }