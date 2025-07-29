551acef317240ef3763b74ed62e1f50cc6b68a8b
Hooder <ahooder@protonmail.com>
2025-07-29 20:38:04 +0200

    Remove array types & fix mat3

--- src/main/java/rs117/hd/opengl/uniforms/UniformBuffer.java
+++ src/main/java/rs117/hd/opengl/uniforms/UniformBuffer.java
@@ -3,7 +3,6 @@ package rs117.hd.opengl.uniforms;
 import java.nio.Buffer;
 import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
-import java.nio.IntBuffer;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.AllArgsConstructor;
@@ -26,44 +25,24 @@ public abstract class UniformBuffer<GLBUFFER extends GLBuffer> {
 		IVec3(12, 16, 3),
 		IVec4(16, 16, 4),
 
-		IntArray(4, 16, 1, true),
-		IVec2Array(8, 16, 2, true),
-		IVec3Array(12, 16, 3, true),
-		IVec4Array(16, 16, 4, true),
-
 		Float(4, 4, 1),
 		FVec2(8, 8, 2),
 		FVec3(12, 16, 3),
 		FVec4(16, 16, 4),
 
-		FloatArray(4, 16, 1, true),
-		FVec2Array(8, 16, 2, true),
-		FVec3Array(12, 16, 3, true),
-		FVec4Array(16, 16, 4, true),
-
-		Mat3(36, 16, 9),
+		Mat3(48, 16, 9),
 		Mat4(64, 16, 16);
 
 		private final int size;
 		private final int alignment;
 		private final int elementSize;
 		private final int elementCount;
-		private final boolean isArray;
 
 		PropertyType(int size, int alignment, int elementCount) {
 			this.size = size;
 			this.alignment = alignment;
 			this.elementSize = size / elementCount;
 			this.elementCount = elementCount;
-			this.isArray = false;
-		}
-
-		PropertyType(int size, int alignment, int elementCount, boolean isArray) {
-			this.size = size;
-			this.alignment = alignment;
-			this.elementSize = size / elementCount;
-			this.elementCount = elementCount;
-			this.isArray = isArray;
 		}
 	}
 
@@ -175,36 +154,6 @@ public abstract class UniformBuffer<GLBUFFER extends GLBuffer> {
 				case IVec4:
 					set(values[0], values[1], values[2], values[3]);
 					break;
-				case IntArray:
-				case IVec2Array:
-				case IVec3Array:
-				case IVec4Array:
-					if (address == 0) {
-						log.warn("{} - Hasn't been initialized yet!", owner.glBuffer.name);
-						return;
-					}
-
-					if ((type.isArray && (values.length % type.elementCount) != 0) || values.length != type.elementCount) {
-						log.warn(
-							"{} - Setter(float[]) was provided with incorrect number of elements for Property: {}",
-							owner.glBuffer.name,
-							name
-						);
-						return;
-					}
-
-					final int newHash = ModelHasher.fastIntHash(values, values.length);
-					if (hash == newHash)
-						return;
-					hash = newHash;
-
-					final int elementCount = type.isArray ? values.length : type.elementCount;
-					final IntCopyBuffer copyBuffer = owner.intCopyBuffer.ensureCapacity(elementCount);
-					copyBuffer.data.put(values, 0, elementCount);
-					copyBuffer.copy(address, elementCount);
-
-					owner.markWaterLine(position, type.elementSize * elementCount);
-					break;
 				default:
 					log.warn("{} - Incorrect Setter(float[]) called for Property: {}", owner.glBuffer.name, name);
 					break;
@@ -309,18 +258,13 @@ public abstract class UniformBuffer<GLBUFFER extends GLBuffer> {
 				case FVec4:
 					set(values[0], values[1], values[2], values[3]);
 					break;
-				case FloatArray:
-				case FVec2Array:
-				case FVec3Array:
-				case FVec4Array:
-				case Mat3:
-				case Mat4:
+				case Mat3: {
 					if (address == 0) {
 						log.warn("{} - Hasn't been initialized yet!", owner.glBuffer.name);
 						return;
 					}
 
-					if ((type.isArray && (values.length % type.elementCount) != 0) || values.length != type.elementCount) {
+					if (values.length != type.elementCount) {
 						log.warn(
 							"{} - Setter(float[]) was provided with incorrect number of elements for Property: {}",
 							owner.glBuffer.name,
@@ -334,13 +278,42 @@ public abstract class UniformBuffer<GLBUFFER extends GLBuffer> {
 						return;
 					hash = newHash;
 
-					final int elementCount = type.isArray ? values.length : type.elementCount;
+					int elementCount = 12;
 					final FloatCopyBuffer copyBuffer = owner.floatCopyBuffer.ensureCapacity(elementCount);
-					copyBuffer.data.put(values, 0, elementCount);
+					for (int i = 0; i < 3; i++)
+						copyBuffer.data.put(values, i * 3, 3).put(0);
 					copyBuffer.copy(address, elementCount);
 
 					owner.markWaterLine(position, type.elementSize * elementCount);
 					break;
+				}
+				case Mat4: {
+					if (address == 0) {
+						log.warn("{} - Hasn't been initialized yet!", owner.glBuffer.name);
+						return;
+					}
+
+					if (values.length != type.elementCount) {
+						log.warn(
+							"{} - Setter(float[]) was provided with incorrect number of elements for Property: {}",
+							owner.glBuffer.name,
+							name
+						);
+						return;
+					}
+
+					final int newHash = ModelHasher.fastFloatHash(values);
+					if (hash == newHash)
+						return;
+					hash = newHash;
+
+					final FloatCopyBuffer copyBuffer = owner.floatCopyBuffer.ensureCapacity(type.elementCount);
+					copyBuffer.data.put(values, 0, type.elementCount);
+					copyBuffer.copy(address, type.elementCount);
+
+					owner.markWaterLine(position, type.elementSize * type.elementCount);
+					break;
+				}
 				default:
 					log.warn("{} - Incorrect Setter(float[]) called for Property: {}", owner.glBuffer.name, name);
 					break;
@@ -356,17 +329,6 @@ public abstract class UniformBuffer<GLBUFFER extends GLBuffer> {
 		public final void copy(long dstAddress, int elementCount) {
 			MemoryUtil.memCopy(address, dstAddress, elementCount * elementSize);
 			data.clear();
-		}
-	}
-
-	static class IntCopyBuffer extends CopyBuffer<IntBuffer> {
-		private IntCopyBuffer ensureCapacity(int size) {
-			if (data == null || data.capacity() < size) {
-				data = BufferUtils.createIntBuffer(size);
-				address = MemoryUtil.memAddress(data);
-				elementSize = Integer.BYTES;
-			}
-			return this;
 		}
 	}
 
@@ -402,7 +364,6 @@ public abstract class UniformBuffer<GLBUFFER extends GLBuffer> {
 	private int dirtyLowTide = Integer.MAX_VALUE;
 	private int dirtyHighTide = 0;
 	private ByteBuffer data;
-	private final IntCopyBuffer intCopyBuffer = new IntCopyBuffer();
 	private final FloatCopyBuffer floatCopyBuffer = new FloatCopyBuffer();
 	private final List<Property> properties = new ArrayList<>();
 
