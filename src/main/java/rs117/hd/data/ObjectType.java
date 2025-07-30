09896be8e103deb44ecd515f51096e9e1182b129
Hooder <ahooder@protonmail.com>
2025-07-30 14:21:16 +0200

    Add comment about where the object type names came from

--- src/main/java/rs117/hd/data/ObjectType.java
+++ src/main/java/rs117/hd/data/ObjectType.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum ObjectType {
+	// Sourced from https://github.com/abextm/cache2/blob/2562ab769e4042667d6a10c3ab795b9622be6049/cache2-ts/src/types.ts#L119-L145
 	WallStraight(0),
 	WallDiagonalCorner(1),
 	WallCorner(2),
