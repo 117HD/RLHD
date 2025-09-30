cfc33c9806d0cb2bb290483ec5badb4e70a29878
Hooder <ahooder@protonmail.com>
2025-09-30 12:07:05 +0200

    Cleanup

--- src/main/resources/rs117/hd/uniforms/ui.glsl
+++ src/main/resources/rs117/hd/uniforms/ui.glsl
@@ -5,3 +5,11 @@ layout(std140) uniform UBOUI {
     ivec2 targetDimensions;
     vec4 alphaOverlay;
 };
+
+#include UI_SCALING_MODE
+#define UI_SCALING_MODE_NEAREST 0
+#define UI_SCALING_MODE_LINEAR 1
+#define UI_SCALING_MODE_MITCHELL 2
+#define UI_SCALING_MODE_CATROM 3
+#define UI_SCALING_MODE_XBR 4
+#define UI_SCALING_MODE_PIXEL 5
