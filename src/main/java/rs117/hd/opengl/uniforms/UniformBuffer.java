837ac9c07400ee9af89fb59477e75a6c22e252b9
Hooder <ahooder@protonmail.com>
2025-10-18 23:24:52 +0200

    Remove unnecessary varargs allocations

--- src/main/java/rs117/hd/opengl/uniforms/UniformBuffer.java
+++ src/main/java/rs117/hd/opengl/uniforms/UniformBuffer.java
@@ -82,6 +82,58 @@ public abstract class UniformBuffer<GLBUFFER extends GLBuffer> {
 			owner.markWaterLine(position, type.size);
 		}
 
+		public final void set(int x) {
+			if (isUninitialized())
+				return;
+
+			if (type != PropertyType.Int) {
+				log("Int setter was used with the wrong property type: " + type);
+				return;
+			}
+
+			owner.dataInt.position(offset).put(x);
+			owner.markWaterLine(position, type.size);
+		}
+
+		public final void set(int x, int y) {
+			if (isUninitialized())
+				return;
+
+			if (type != PropertyType.IVec2) {
+				log("Int setter was used with the wrong property type: " + type);
+				return;
+			}
+
+			owner.dataInt.position(offset).put(x).put(y);
+			owner.markWaterLine(position, type.size);
+		}
+
+		public final void set(int x, int y, int z) {
+			if (isUninitialized())
+				return;
+
+			if (type != PropertyType.IVec3) {
+				log("Int setter was used with the wrong property type: " + type);
+				return;
+			}
+
+			owner.dataInt.position(offset).put(x).put(y).put(z);
+			owner.markWaterLine(position, type.size);
+		}
+
+		public final void set(int x, int y, int z, int w) {
+			if (isUninitialized())
+				return;
+
+			if (type != PropertyType.IVec4) {
+				log("Int setter was used with the wrong property type: " + type);
+				return;
+			}
+
+			owner.dataInt.position(offset).put(x).put(y).put(z).put(w);
+			owner.markWaterLine(position, type.size);
+		}
+
 		public final void set(float... values) {
 			if (isUninitialized())
 				return;
@@ -113,6 +165,58 @@ public abstract class UniformBuffer<GLBUFFER extends GLBuffer> {
 			} else {
 				owner.dataFloat.put(values);
 			}
+			owner.markWaterLine(position, type.size);
+		}
+
+		public final void set(float x) {
+			if (isUninitialized())
+				return;
+
+			if (type != PropertyType.Float) {
+				log("Float setter was used with the wrong property type: " + type);
+				return;
+			}
+
+			owner.dataFloat.position(offset).put(x);
+			owner.markWaterLine(position, type.size);
+		}
+
+		public final void set(float x, float y) {
+			if (isUninitialized())
+				return;
+
+			if (type != PropertyType.FVec2) {
+				log("Float setter was used with the wrong property type: " + type);
+				return;
+			}
+
+			owner.dataFloat.position(offset).put(x).put(y);
+			owner.markWaterLine(position, type.size);
+		}
+
+		public final void set(float x, float y, float z) {
+			if (isUninitialized())
+				return;
+
+			if (type != PropertyType.FVec3) {
+				log("Float setter was used with the wrong property type: " + type);
+				return;
+			}
+
+			owner.dataFloat.position(offset).put(x).put(y).put(z);
+			owner.markWaterLine(position, type.size);
+		}
+
+		public final void set(float x, float y, float z, float w) {
+			if (isUninitialized())
+				return;
+
+			if (type != PropertyType.FVec4) {
+				log("Float setter was used with the wrong property type: " + type);
+				return;
+			}
+
+			owner.dataFloat.position(offset).put(x).put(y).put(z).put(w);
 			owner.markWaterLine(position, type.size);
 		}
 	}
