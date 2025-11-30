117HD/RLHD

commit c33c1751434e57d72a8687a3d2e1f295735c9386

Fix shadows for dynamic objects with alpha

src/main/java/rs117/hd/renderer/zone/ZoneRenderer.java
@@ -33,7 +33,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CountDownLatch;
-import java.util.stream.Collectors;
 import javax.annotation.Nullable;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
@@ -65,7 +64,6 @@ import rs117.hd.scene.ModelOverrideManager;
 import rs117.hd.scene.ProceduralGenerator;
 import rs117.hd.scene.areas.Area;
 import rs117.hd.scene.lights.Light;
-import rs117.hd.scene.materials.Material;
 import rs117.hd.scene.model_overrides.ModelOverride;
 import rs117.hd.utils.Camera;
 import rs117.hd.utils.ColorUtils;
@@ -967,7 +965,7 @@ public class ZoneRenderer implements Renderer {
 				this.level,
 				plugin.configRoofShadows ? 3 : maxLevel,
 				level,
-				sceneCamera,
+				directionalCamera,
 				plugin.configRoofShadows ? Collections.emptySet() : hideRoofIds
 			);
 		}
@@ -1050,40 +1048,26 @@ public class ZoneRenderer implements Renderer {
 
 		int preOrientation = HDUtils.getModelPreOrientation(HDUtils.getObjectConfig(tileObject));
 
-		byte[] transparencies = m.getFaceTransparencies();
-		short[] faceTextures = m.getFaceTextures();
-		boolean hasAlpha = false;
-		if (transparencies != null || faceTextures != null) {
-			for (int face = 0; face < m.getFaceCount(); ++face) {
-				boolean alpha =
-					transparencies != null && transparencies[face] != 0 ||
-					faceTextures != null && Material.hasVanillaTransparency(faceTextures[face]);
-				if (alpha) {
-					hasAlpha = true;
-					break;
-				}
-			}
-		}
+		int offset = ctx.sceneContext.sceneOffset >> 3;
+		int zx = (x >> 10) + offset;
+		int zz = (z >> 10) + offset;
+		Zone zone = ctx.zones[zx][zz];
 
 		int size = m.getFaceCount() * 3 * VAO.VERT_SIZE;
-		if (!hasAlpha) {
+		boolean hasAlpha = m.getFaceTransparencies() != null;
+		if (hasAlpha) {
 			VAO o = vaoO.get(size);
-			sceneUploader.uploadTempModel(m, modelOverride, preOrientation, orient, x, y, z, o.vbo.vb);
-		} else {
-			m.calculateBoundsCylinder();
-			VAO o = vaoO.get(size), a = vaoA.get(size);
+			VAO a = vaoA.get(size);
 			int start = a.vbo.vb.position();
-			facePrioritySorter.uploadSortedModel(worldProjection, m, modelOverride, preOrientation, orient, x, y, z, o.vbo.vb, a.vbo.vb);
+			sceneUploader.uploadTempModel(m, modelOverride, preOrientation, orient, x, y, z, o.vbo.vb, a.vbo.vb);
 			int end = a.vbo.vb.position();
-
 			if (end > start) {
-				int offset = ctx.sceneContext.sceneOffset >> 3;
-				int zx = (x >> 10) + offset;
-				int zz = (z >> 10) + offset;
-				Zone zone = ctx.zones[zx][zz];
 				// renderable modelheight is typically not set here because DynamicObject doesn't compute it on the returned model
 				zone.addTempAlphaModel(a.vao, start, end, tileObject.getPlane(), x & 1023, y, z & 1023);
 			}
+		} else {
+			VAO o = vaoO.get(size);
+			sceneUploader.uploadTempModel(m, modelOverride, preOrientation, orient, x, y, z, o.vbo.vb, o.vbo.vb);
 		}
 	}
 
@@ -1133,7 +1117,6 @@ public class ZoneRenderer implements Renderer {
 					log.debug("error drawing entity", ex);
 				}
 				int end = a.vbo.vb.position();
-
 				if (end > start) {
 					zone.addTempAlphaModel(
 						a.vao,
@@ -1157,6 +1140,7 @@ public class ZoneRenderer implements Renderer {
 					preOrientation,
 					orientation,
 					x, y, z,
+					o.vbo.vb,
 					o.vbo.vb
 				);
 			}
@@ -1168,6 +1152,7 @@ public class ZoneRenderer implements Renderer {
 				preOrientation,
 				orientation,
 				x, y, z,
+				o.vbo.vb,
 				o.vbo.vb
 			);
 		}

src/main/java/rs117/hd/renderer/zone/SceneUploader.java
@@ -579,9 +579,8 @@ class SceneUploader {
 			uploadStaticModel(model, modelOverride, preOrientation, orient, x - basex, y, z - basez, vertexBuffer, ab);
 		} else if (r instanceof DynamicObject) {
 			model = ((DynamicObject) r).getModelZbuf();
-			if (model != null) {
+			if (model != null)
 				uploadStaticModel(model, modelOverride, preOrientation, orient, x - basex, y, z - basez, vertexBuffer, ab);
-			}
 		}
 		int endpos = zone.vboA != null ? zone.vboA.vb.position() : 0;
 		if (endpos > pos) {
@@ -1112,10 +1111,11 @@ class SceneUploader {
 	private int uploadStaticModel(
 		Model model,
 		ModelOverride modelOverride,
-		int preOrientation, int orientation, int x, int y, int z, GpuIntBuffer vertexBuffer, GpuIntBuffer ab
+		int preOrientation, int orientation, int x, int y, int z,
+		GpuIntBuffer opaqueBuffer, GpuIntBuffer alphaBuffer
 	) {
-		final int vertexCount = model.getVerticesCount();
 		final int triangleCount = model.getFaceCount();
+		final int vertexCount = model.getVerticesCount();
 
 		final float[] vertexX = model.getVerticesX();
 		final float[] vertexY = model.getVerticesY();
@@ -1135,8 +1135,8 @@ class SceneUploader {
 		final int[] texIndices2 = model.getTexIndices2();
 		final int[] texIndices3 = model.getTexIndices3();
 
-		final byte[] transparencies = model.getFaceTransparencies();
 		final byte[] bias = model.getFaceBias();
+		final byte[] transparencies = model.getFaceTransparencies();
 
 		int orientSin = 0;
 		int orientCos = 0;
@@ -1320,7 +1320,7 @@ class SceneUploader {
 				packedAlphaBiasHsl |= 1 << 20;
 			}
 
-			GpuIntBuffer vb = alpha ? ab : vertexBuffer;
+			GpuIntBuffer vb = alpha ? alphaBuffer : opaqueBuffer;
 			vb.putVertex(
 				vx1, vy1, vz1, packedAlphaBiasHsl | color1,
 				modelUvs[0], modelUvs[1], modelUvs[2], materialData,
@@ -1354,7 +1354,8 @@ class SceneUploader {
 		int x,
 		int y,
 		int z,
-		IntBuffer opaqueBuffer
+		IntBuffer opaqueBuffer,
+		IntBuffer alphaBuffer
 	) {
 		final int triangleCount = model.getFaceCount();
 		final int vertexCount = model.getVerticesCount();
@@ -1388,23 +1389,9 @@ class SceneUploader {
 		float orientSine = 0;
 		float orientCosine = 0;
 		if (orientation != 0) {
-			orientSine = SINE[mod(orientation, 2048)] / 65536f;
-			orientCosine = COSINE[mod(orientation, 2048)] / 65536f;
-		}
-
-		boolean isVanillaTextured = faceTextures != null;
-		boolean isVanillaUVMapped =
-			isVanillaTextured && // Vanilla UV mapped models don't always have sensible UVs for untextured faces
-			model.getTextureFaces() != null;
-
-		Material baseMaterial = modelOverride.baseMaterial;
-		Material textureMaterial = modelOverride.textureMaterial;
-		boolean disableTextures = !plugin.configModelTextures && !modelOverride.forceMaterialChanges;
-		if (disableTextures) {
-			if (baseMaterial.modifiesVanillaTexture)
-				baseMaterial = Material.NONE;
-			if (textureMaterial.modifiesVanillaTexture)
-				textureMaterial = Material.NONE;
+			orientation = mod(orientation, 2048);
+			orientSine = SINE[orientation] / 65536f;
+			orientCosine = COSINE[orientation] / 65536f;
 		}
 
 		for (int v = 0; v < vertexCount; ++v) {
@@ -1425,6 +1412,21 @@ class SceneUploader {
 			modelLocalX[v] = vertexX;
 			modelLocalY[v] = vertexY;
 			modelLocalZ[v] = vertexZ;
+		}
+
+		boolean isVanillaTextured = faceTextures != null;
+		boolean isVanillaUVMapped =
+			isVanillaTextured && // Vanilla UV mapped models don't always have sensible UVs for untextured faces
+			model.getTextureFaces() != null;
+
+		Material baseMaterial = modelOverride.baseMaterial;
+		Material textureMaterial = modelOverride.textureMaterial;
+		boolean disableTextures = !plugin.configModelTextures && !modelOverride.forceMaterialChanges;
+		if (disableTextures) {
+			if (baseMaterial.modifiesVanillaTexture)
+				baseMaterial = Material.NONE;
+			if (textureMaterial.modifiesVanillaTexture)
+				textureMaterial = Material.NONE;
 		}
 
 		int len = 0;
@@ -1552,26 +1554,45 @@ class SceneUploader {
 				}
 			}
 
-			int alphaBias = 0;
-			alphaBias |= bias != null ? (bias[face] & 0xff) << 16 : 0;
+			boolean alpha =
+				transparencies != null && transparencies[face] != 0 ||
+				faceTextures != null && Material.hasVanillaTransparency(faceTextures[face]);
 
+			int packedAlphaBiasHsl = 0;
+			packedAlphaBiasHsl |= transparencies != null ? (transparencies[face] & 0xff) << 24 : 0;
+			packedAlphaBiasHsl |= bias != null ? (bias[face] & 0xff) << 16 : 0;
+
+			boolean isTextured = faceTextures != null && faceTextures[face] != -1;
+			if (isTextured) {
+				// Without overriding the color for textured faces, vanilla shading remains pretty noticeable even after
+				// the approximate reversal above. Ardougne rooftops is a good example, where vanilla shading results in a
+				// weird-looking tint. The brightness clamp afterward is required to reduce the over-exposure introduced.
+				color1 = color2 = color3 = 90;
+			}
+
+			if (isTextured || !modelOverride.undoVanillaShading) {
+				// Let the shader know vanilla shading reversal should be skipped for this face
+				packedAlphaBiasHsl |= 1 << 20;
+			}
+
+			IntBuffer vb = alpha ? alphaBuffer : opaqueBuffer;
 			GpuIntBuffer.putFloatVertex(
-				opaqueBuffer,
-				vx1, vy1, vz1, alphaBias | color1,
+				vb,
+				vx1, vy1, vz1, packedAlphaBiasHsl | color1,
 				modelUvs[0], modelUvs[1], modelUvs[2], materialData,
 				modelNormals[0], modelNormals[1], modelNormals[2], 0
 			);
 
 			GpuIntBuffer.putFloatVertex(
-				opaqueBuffer,
-				vx2, vy2, vz2, alphaBias | color2,
+				vb,
+				vx2, vy2, vz2, packedAlphaBiasHsl | color2,
 				modelUvs[4], modelUvs[5], modelUvs[6], materialData,
 				modelNormals[3], modelNormals[4], modelNormals[5], 0
 			);
 
 			GpuIntBuffer.putFloatVertex(
-				opaqueBuffer,
-				vx3, vy3, vz3, alphaBias | color3,
+				vb,
+				vx3, vy3, vz3, packedAlphaBiasHsl | color3,
 				modelUvs[8], modelUvs[9], modelUvs[10], materialData,
 				modelNormals[6], modelNormals[7], modelNormals[8], 0
 			);

src/main/java/rs117/hd/renderer/zone/Zone.java
@@ -549,6 +549,8 @@ class Zone {
 				distanceToFaces[fz][distanceFaceCount[fz]++] = bufferIdx++;
 			}
 
+			ZoneRenderer.eboAlphaStaging.ensureCapacity(bufferIdx * 3);
+
 			byte[] faceRenderPriorities = m.renderPriorities;
 			final int start = m.startpos / (VERT_SIZE >> 2); // ints to verts
 			if (faceRenderPriorities == null) {
@@ -557,7 +559,6 @@ class Zone {
 					if (cnt > 0) {
 						final char[] faces = distanceToFaces[i];
 
-						ZoneRenderer.eboAlphaStaging.ensureCapacity(cnt * 3);
 						ZoneRenderer.alphaFaceCount += cnt;
 						for (int faceIdx = 0; faceIdx < cnt; ++faceIdx) {
 							final int face = faces[faceIdx];
@@ -592,7 +593,6 @@ class Zone {
 					final int priNum = numOfPriority[pri];
 					final int[] priFaces = orderedFaces[pri];
 
-					ZoneRenderer.eboAlphaStaging.ensureCapacity(priNum * 3);
 					ZoneRenderer.alphaFaceCount += priNum;
 					for (int faceIdx = 0; faceIdx < priNum; ++faceIdx) {
 						final int face = priFaces[faceIdx];

commit 9859c93aadcdc675d96e167d90707a429279a251

Fix alpha sorting for dynamic objects
This was broken in c33c1751434e57d72a8687a3d2e1f295735c9386.

src/main/java/rs117/hd/renderer/zone/FacePrioritySorter.java
@@ -47,8 +47,8 @@ class FacePrioritySorter {
 	static final char[] distanceFaceCount;
 	static final char[][] distanceToFaces;
 
-	private static final float[] modelCanvasX;
-	private static final float[] modelCanvasY;
+	private static final float[] modelProjectedX;
+	private static final float[] modelProjectedY;
 
 	private static final float[] modelLocalX;
 	private static final float[] modelLocalY;
@@ -87,8 +87,8 @@ class FacePrioritySorter {
 		distanceFaceCount = new char[MAX_DIAMETER];
 		distanceToFaces = new char[MAX_DIAMETER][ZSORT_GROUP_SIZE];
 
-		modelCanvasX = new float[MAX_VERTEX_COUNT];
-		modelCanvasY = new float[MAX_VERTEX_COUNT];
+		modelProjectedX = new float[MAX_VERTEX_COUNT];
+		modelProjectedY = new float[MAX_VERTEX_COUNT];
 
 		modelLocalX = new float[MAX_VERTEX_COUNT];
 		modelLocalY = new float[MAX_VERTEX_COUNT];
@@ -107,9 +107,6 @@ class FacePrioritySorter {
 		for (int i = 0; i < 8; i++)
 			MAX_BRIGHTNESS_LOOKUP_TABLE[i] = (int) (127 - 72 * Math.pow(i / 7f, .05));
 	}
-
-	@Inject
-	private Client client;
 
 	@Inject
 	private HdPlugin plugin;
@@ -141,10 +138,6 @@ class FacePrioritySorter {
 
 		final int[] faceColors3 = model.getFaceColors3();
 		final byte[] faceRenderPriorities = model.getFaceRenderPriorities();
-
-		final int centerX = client.getCenterX();
-		final int centerY = client.getCenterY();
-		final int zoom = client.get3dZoom();
 
 		orientation = mod(orientation, 2048);
 		orientSin = SINE[orientation];
@@ -180,11 +173,12 @@ class FacePrioritySorter {
 				return 0;
 			}
 
-			modelCanvasX[v] = centerX + p[0] * zoom / p[2];
-			modelCanvasY[v] = centerY + p[1] * zoom / p[2];
+			modelProjectedX[v] = p[0];
+			modelProjectedY[v] = p[1];
 			distances[v] = (int) p[2] - zero;
 		}
 
+		model.calculateBoundsCylinder();
 		final int diameter = model.getDiameter();
 		final int radius = model.getRadius();
 		if (diameter >= 6000) {
@@ -202,12 +196,12 @@ class FacePrioritySorter {
 			final int v3 = indices3[i];
 
 			final float
-				aX = modelCanvasX[v1],
-				aY = modelCanvasY[v1],
-				bX = modelCanvasX[v2],
-				bY = modelCanvasY[v2],
-				cX = modelCanvasX[v3],
-				cY = modelCanvasY[v3];
+				aX = modelProjectedX[v1],
+				aY = modelProjectedY[v1],
+				bX = modelProjectedX[v2],
+				bY = modelProjectedY[v2],
+				cX = modelProjectedX[v3],
+				cY = modelProjectedY[v3];
 			// Back-face culling
 			if ((aX - bX) * (cY - bY) - (cX - bX) * (aY - bY) <= 0)
 				continue;
@@ -276,12 +270,7 @@ class FacePrioritySorter {
 				dynFaceDistances = eq11;
 			}
 
-			int currFaceDistance;
-			if (drawnFaces < numDynFaces) {
-				currFaceDistance = dynFaceDistances[drawnFaces];
-			} else {
-				currFaceDistance = -1000;
-			}
+			int currFaceDistance = drawnFaces < numDynFaces ? dynFaceDistances[drawnFaces] : -1000;
 
 			for (int pri = 0; pri < 10; ++pri) {
 				while (pri == 0 && currFaceDistance > avg12) {
@@ -295,11 +284,7 @@ class FacePrioritySorter {
 						dynFaceDistances = eq11;
 					}
 
-					if (drawnFaces < numDynFaces) {
-						currFaceDistance = dynFaceDistances[drawnFaces];
-					} else {
-						currFaceDistance = -1000;
-					}
+					currFaceDistance = drawnFaces < numDynFaces ? dynFaceDistances[drawnFaces] : -1000;
 				}
 
 				while (pri == 3 && currFaceDistance > avg34) {
@@ -313,11 +298,7 @@ class FacePrioritySorter {
 						dynFaceDistances = eq11;
 					}
 
-					if (drawnFaces < numDynFaces) {
-						currFaceDistance = dynFaceDistances[drawnFaces];
-					} else {
-						currFaceDistance = -1000;
-					}
+					currFaceDistance = drawnFaces < numDynFaces ? dynFaceDistances[drawnFaces] : -1000;
 				}
 
 				while (pri == 5 && currFaceDistance > avg68) {
@@ -331,11 +312,7 @@ class FacePrioritySorter {
 						dynFaceDistances = eq11;
 					}
 
-					if (drawnFaces < numDynFaces) {
-						currFaceDistance = dynFaceDistances[drawnFaces];
-					} else {
-						currFaceDistance = -1000;
-					}
+					currFaceDistance = drawnFaces < numDynFaces ? dynFaceDistances[drawnFaces] : -1000;
 				}
 
 				final int priNum = numOfPriority[pri];
@@ -358,11 +335,7 @@ class FacePrioritySorter {
 					dynFaceDistances = eq11;
 				}
 
-				if (drawnFaces < numDynFaces) {
-					currFaceDistance = dynFaceDistances[drawnFaces];
-				} else {
-					currFaceDistance = -1000;
-				}
+				currFaceDistance = drawnFaces < numDynFaces ? dynFaceDistances[drawnFaces] : -1000;
 			}
 		}
 

src/main/java/rs117/hd/renderer/zone/ZoneRenderer.java
@@ -1080,7 +1080,7 @@ public class ZoneRenderer implements Renderer {
 
 	@Override
 	public void drawDynamic(
-		Projection worldProjection,
+		Projection projection,
 		Scene scene,
 		TileObject tileObject,
 		Renderable r,
@@ -1154,7 +1154,13 @@ public class ZoneRenderer implements Renderer {
 			VAO o = vaoO.get(size, ctx.vboM);
 			VAO a = vaoA.get(size, ctx.vboM);
 			int start = a.vbo.vb.position();
-			sceneUploader.uploadTempModel(m, modelOverride, preOrientation, orient, x, y, z, o.vbo.vb, a.vbo.vb);
+
+			if (zone.inSceneFrustum) {
+				facePrioritySorter.uploadSortedModel(projection, m, modelOverride, preOrientation, orient, x, y, z, o.vbo.vb, a.vbo.vb);
+			} else {
+				sceneUploader.uploadTempModel(m, modelOverride, preOrientation, orient, x, y, z, o.vbo.vb, a.vbo.vb);
+			}
+
 			int end = a.vbo.vb.position();
 			if (end > start) {
 				// renderable modelheight is typically not set here because DynamicObject doesn't compute it on the returned model
@@ -1209,7 +1215,6 @@ public class ZoneRenderer implements Renderer {
 				VAO a = vaoA.get(size, ctx.vboM);
 
 				int start = a.vbo.vb.position();
-				m.calculateBoundsCylinder();
 				try {
 					facePrioritySorter.uploadSortedModel(
 						worldProjection,