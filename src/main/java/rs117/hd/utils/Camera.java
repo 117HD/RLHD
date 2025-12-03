117HD/RLHD

commit 5972b8be466667cf7005fb6c9173df5f55271f76

Formatting & minor cleanup

src/main/java/rs117/hd/utils/Camera.java
@@ -175,7 +175,11 @@ public class Camera {
 	}
 
 	public float distanceTo(float x, float y, float z) {
-		return distance(position, vec(x, y, z));
+		return sqrt(position[0] * x + position[1] * y + position[2] * z);
+	}
+
+	public float squaredDistanceTo(float x, float y, float z) {
+		return position[0] * x + position[1] * y + position[2] * z;
 	}
 
 	public Camera translate(float[] translation) {

src/main/java/rs117/hd/HdPlugin.java
@@ -181,20 +181,20 @@ public class HdPlugin extends Plugin {
 
 	public static final float COLOR_FILTER_FADE_DURATION = 500;
 
-	private static final int[] RENDERBUFFER_FORMATS_SRGB = {
+	public static final int[] RENDERBUFFER_FORMATS_SRGB = {
 		GL_SRGB8,
 		GL_SRGB8_ALPHA8 // should be guaranteed
 	};
-	private static final int[] RENDERBUFFER_FORMATS_SRGB_WITH_ALPHA = {
+	public static final int[] RENDERBUFFER_FORMATS_SRGB_WITH_ALPHA = {
 		GL_SRGB8_ALPHA8 // should be guaranteed
 	};
-	private static final int[] RENDERBUFFER_FORMATS_LINEAR = {
+	public static final int[] RENDERBUFFER_FORMATS_LINEAR = {
 		GL_RGB8,
 		GL_RGBA8,
 		GL_RGB, // should be guaranteed
 		GL_RGBA // should be guaranteed
 	};
-	private static final int[] RENDERBUFFER_FORMATS_LINEAR_WITH_ALPHA = {
+	public static final int[] RENDERBUFFER_FORMATS_LINEAR_WITH_ALPHA = {
 		GL_RGBA8,
 		GL_RGBA // should be guaranteed
 	};
@@ -379,7 +379,7 @@ public class HdPlugin extends Plugin {
 	public boolean configCharacterDisplacement;
 	public boolean configTiledLighting;
 	public boolean configTiledLightingImageLoadStore;
-	public float configDetailDrawDistance;
+	public int configDetailDrawDistance;
 	public DynamicLights configDynamicLights;
 	public ShadowMode configShadowMode;
 	public SeasonalTheme configSeasonalTheme;
@@ -438,6 +438,19 @@ public class HdPlugin extends Plugin {
 	@Provides
 	HdPluginConfig provideConfig(ConfigManager configManager) {
 		return configManager.getConfig(HdPluginConfig.class);
+	}
+
+	@Override
+	public void configure(Binder binder) {
+		// Bind manually constructed instances of singletons to avoid recursive loading issues
+		for (var clazz : LAZY_SINGLETONS) {
+			try {
+				// noinspection unchecked
+				binder.bind((Class<Object>) clazz).toInstance(clazz.getDeclaredConstructor().newInstance());
+			} catch (Exception ex) {
+				throw new RuntimeException("Failed to instantiate singleton " + clazz, ex);
+			}
+		}
 	}
 
 	@Override

src/main/java/rs117/hd/overlays/FrameTimerOverlay.java
@@ -179,9 +179,9 @@ public class FrameTimerOverlay extends OverlayPanel implements FrameTimer.Listen
 			long subSceneUploadTime = 0;
 			long subSceneSwapTime = 0;
 
-			for(int worldViewId = 0; worldViewId < MAX_WORLDVIEWS; worldViewId++) {
-				WorldViewContext subscene = sceneManager.context(worldViewId);
-				if(subscene != null) {
+			for (int worldViewId = 0; worldViewId < MAX_WORLDVIEWS; worldViewId++) {
+				WorldViewContext subscene = sceneManager.getContext(worldViewId);
+				if (subscene != null) {
 					subSceneCount++;
 					subSceneLoadTime += subscene.loadTime;
 					subSceneUploadTime += subscene.uploadTime;
@@ -189,7 +189,7 @@ public class FrameTimerOverlay extends OverlayPanel implements FrameTimer.Listen
 				}
 			}
 
-			if(subSceneCount > 0) {
+			if (subSceneCount > 0) {
 				addTiming("Avg SubScene Load", subSceneLoadTime / subSceneCount, false);
 				addTiming("Avg SubScene Upload", subSceneUploadTime / subSceneCount, false);
 				addTiming("Avg SubScene Swap", subSceneSwapTime / subSceneCount, false);
@@ -279,7 +279,7 @@ public class FrameTimerOverlay extends OverlayPanel implements FrameTimer.Listen
 		} else {
 			component.setRight(result);
 		}
-		
+
 		panelComponent.getChildren().add(component);
 	}
 }

src/main/java/rs117/hd/renderer/legacy/LegacyRenderer.java
@@ -18,10 +18,7 @@ import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.*;
 import net.runelite.api.events.*;
 import net.runelite.api.hooks.*;
-import net.runelite.client.callback.ClientThread;
-import net.runelite.client.eventbus.EventBus;
 import net.runelite.client.eventbus.Subscribe;
-import net.runelite.client.plugins.PluginManager;
 import net.runelite.client.ui.DrawManager;
 import org.lwjgl.opengl.*;
 import rs117.hd.HdPlugin;
@@ -46,15 +43,9 @@ import rs117.hd.renderer.Renderer;
 import rs117.hd.scene.AreaManager;
 import rs117.hd.scene.EnvironmentManager;
 import rs117.hd.scene.FishingSpotReplacer;
-import rs117.hd.scene.GamevalManager;
-import rs117.hd.scene.GroundMaterialManager;
 import rs117.hd.scene.LightManager;
-import rs117.hd.scene.MaterialManager;
 import rs117.hd.scene.ModelOverrideManager;
 import rs117.hd.scene.ProceduralGenerator;
-import rs117.hd.scene.TextureManager;
-import rs117.hd.scene.TileOverrideManager;
-import rs117.hd.scene.WaterTypeManager;
 import rs117.hd.scene.areas.Area;
 import rs117.hd.scene.lights.Light;
 import rs117.hd.scene.model_overrides.ModelOverride;
@@ -93,16 +84,7 @@ public class LegacyRenderer implements Renderer {
 	private Client client;
 
 	@Inject
-	private ClientThread clientThread;
-
-	@Inject
-	private EventBus eventBus;
-
-	@Inject
 	private DrawManager drawManager;
-
-	@Inject
-	private PluginManager pluginManager;
 
 	@Inject
 	private HdPlugin plugin;
@@ -114,9 +96,6 @@ public class LegacyRenderer implements Renderer {
 	private OpenCLManager clManager;
 
 	@Inject
-	private GamevalManager gamevalManager;
-
-	@Inject
 	private AreaManager areaManager;
 
 	@Inject
@@ -126,25 +105,7 @@ public class LegacyRenderer implements Renderer {
 	private EnvironmentManager environmentManager;
 
 	@Inject
-	private TextureManager textureManager;
-
-	@Inject
-	private MaterialManager materialManager;
-
-	@Inject
-	private WaterTypeManager waterTypeManager;
-
-	@Inject
-	private GroundMaterialManager groundMaterialManager;
-
-	@Inject
-	private TileOverrideManager tileOverrideManager;
-
-	@Inject
 	private ModelOverrideManager modelOverrideManager;
-
-	@Inject
-	private ProceduralGenerator proceduralGenerator;
 
 	@Inject
 	private LegacySceneUploader sceneUploader;
@@ -260,8 +221,8 @@ public class LegacyRenderer implements Renderer {
 		if (vaoScene != 0)
 			glDeleteVertexArrays(vaoScene);
 		vaoScene = 0;
-		
-		jobSystem.shutdown();
+
+		jobSystem.destroy();
 
 		destroyBuffers();
 		destroyTileHeightMap();

src/main/java/rs117/hd/renderer/zone/SceneManager.java
@@ -10,7 +10,6 @@ import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Collectors;
-import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -32,27 +31,19 @@ import rs117.hd.scene.areas.AABB;
 import rs117.hd.scene.areas.Area;
 import rs117.hd.utils.NpcDisplacementCache;
 import rs117.hd.utils.jobs.JobGenericTask;
-import rs117.hd.utils.jobs.JobSystem;
 
 import static net.runelite.api.Constants.*;
 import static net.runelite.api.Perspective.SCENE_SIZE;
 import static rs117.hd.HdPlugin.checkGLErrors;
 import static rs117.hd.utils.MathUtils.*;
 
-@Singleton
 @Slf4j
+@Singleton
 public class SceneManager {
-	private static final int ZONE_DEFER_DIST_START = 3;
-
 	public static final int MAX_WORLDVIEWS = 4096;
-
 	public static final int NUM_ZONES = EXTENDED_SCENE_SIZE >> 3;
 
-	@Inject
-	private HdPlugin plugin;
-
-	@Inject
-	private HdPluginConfig config;
+	private static final int ZONE_DEFER_DIST_START = 3;
 
 	@Inject
 	private Client client;
@@ -61,25 +52,28 @@ public class SceneManager {
 	private ClientThread clientThread;
 
 	@Inject
-	private ProceduralGenerator proceduralGenerator;
+	private HdPlugin plugin;
 
 	@Inject
-	private EnvironmentManager environmentManager;
-
-	@Inject
-	private NpcDisplacementCache npcDisplacementCache;
+	private HdPluginConfig config;
 
 	@Inject
 	private AreaManager areaManager;
 
 	@Inject
+	private EnvironmentManager environmentManager;
+
+	@Inject
 	private LightManager lightManager;
 
 	@Inject
-	private FishingSpotReplacer fishingSpotReplacer;
+	private ProceduralGenerator proceduralGenerator;
 
 	@Inject
-	private JobSystem jobSystem;
+	private NpcDisplacementCache npcDisplacementCache;
+
+	@Inject
+	private FishingSpotReplacer fishingSpotReplacer;
 
 	@Inject
 	private FrameTimer frameTimer;
@@ -87,6 +81,7 @@ public class SceneManager {
 	private UBOWorldViews uboWorldViews;
 
 	private final Map<Integer, Integer> nextRoofChanges = new HashMap<>();
+	@Getter
 	private final WorldViewContext root = new WorldViewContext(this, null, null, null);
 	private final WorldViewContext[] subs = new WorldViewContext[MAX_WORLDVIEWS];
 	private final List<SortedZone> sortedZones = new ArrayList<>();
@@ -98,7 +93,7 @@ public class SceneManager {
 	public final ReentrantLock loadingLock = new ReentrantLock();
 
 	public boolean isTopLevelValid() {
-		return root != null && root.sceneContext != null;
+		return root.sceneContext != null;
 	}
 
 	@Nullable
@@ -106,20 +101,17 @@ public class SceneManager {
 		return root.sceneContext;
 	}
 
-	@Nonnull
-	public WorldViewContext getRoot() { return root; }
-
 	public boolean isRoot(WorldViewContext context) { return root == context; }
 
-	public WorldViewContext context(Scene scene) {
-		return context(scene.getWorldViewId());
+	public WorldViewContext getContext(Scene scene) {
+		return getContext(scene.getWorldViewId());
 	}
 
-	public WorldViewContext context(WorldView wv) {
-		return context(wv.getId());
+	public WorldViewContext getContext(WorldView wv) {
+		return getContext(wv.getId());
 	}
 
-	public WorldViewContext context(int worldViewId) {
+	public WorldViewContext getContext(int worldViewId) {
 		if (worldViewId != -1)
 			return subs[worldViewId];
 		if (root.sceneContext == null)
@@ -131,7 +123,7 @@ public class SceneManager {
 		this.uboWorldViews = uboWorldViews;
 	}
 
-	public void shutdown() {
+	public void destroy() {
 		root.free();
 
 		for (int i = 0; i < subs.length; i++) {
@@ -154,14 +146,14 @@ public class SceneManager {
 		updateAreaHiding();
 		frameTimer.end(Timer.UPDATE_AREA_HIDING);
 
-		if(reloadRequested && loadingLock.getHoldCount() == 0) {
+		if (reloadRequested && loadingLock.getHoldCount() == 0) {
 			reloadRequested = false;
 
 			completeAllStreaming();
 			try {
 				loadingLock.lock();
 
-				if(!generateSceneDataTask.isDone())
+				if (!generateSceneDataTask.isDone())
 					generateSceneDataTask.waitForCompletion();
 				generateSceneDataTask.queue();
 
@@ -172,7 +164,6 @@ public class SceneManager {
 
 				root.sceneLoadGroup.complete();
 				root.streamingGroup.complete();
-
 			} finally {
 				loadingLock.unlock();
 				log.debug("loadingLock unlocked - holdCount: {}", loadingLock.getHoldCount());
@@ -183,9 +174,9 @@ public class SceneManager {
 		boolean queuedWork = root.update(plugin.deltaTime);
 
 		WorldView wv = client.getTopLevelWorldView();
-		if(wv != null) {
+		if (wv != null) {
 			for (WorldEntity we : wv.worldEntities()) {
-				WorldViewContext ctx = context(we.getWorldView());
+				WorldViewContext ctx = getContext(we.getWorldView());
 				if (ctx != null) {
 					anyScenesLoading = anyScenesLoading || ctx.isLoading;
 					queuedWork = ctx.update(plugin.deltaTime) || queuedWork;
@@ -193,13 +184,13 @@ public class SceneManager {
 			}
 		}
 
-		if(plugin.isInHouse && queuedWork)
+		if (plugin.isInHouse && queuedWork)
 			root.streamingGroup.complete();
 	}
 
 	private void updateAreaHiding() {
 		Player localPlayer = client.getLocalPlayer();
-		if(!isTopLevelValid() || localPlayer == null || root.isLoading)
+		if (!isTopLevelValid() || localPlayer == null || root.isLoading)
 			return;
 
 		var lp = localPlayer.getLocalLocation();
@@ -259,17 +250,17 @@ public class SceneManager {
 				subs[worldViewId].free();
 				subs[worldViewId] = null;
 			}
-		} else if(worldViewId == root.worldViewId) {
+		} else if (worldViewId == root.worldViewId) {
 			root.free();
 		}
 	}
 
 	public void reloadScene() {
-		if(!plugin.isActive() || reloadRequested)
+		if (!plugin.isActive() || reloadRequested)
 			return;
 
 		reloadRequested = true;
-		log.debug("Reload scene requested");
+		log.debug("Scene reload requested");
 	}
 
 	public boolean isLoadingScene() { return nextSceneContext != null; }
@@ -279,9 +270,9 @@ public class SceneManager {
 		root.streamingGroup.complete();
 
 		WorldView wv = client.getTopLevelWorldView();
-		if(wv != null) {
+		if (wv != null) {
 			for (WorldEntity we : wv.worldEntities()) {
-				WorldViewContext ctx = context(we.getWorldView());
+				WorldViewContext ctx = getContext(we.getWorldView());
 				if (ctx != null) {
 					ctx.sceneLoadGroup.complete();
 					ctx.streamingGroup.complete();
@@ -291,12 +282,12 @@ public class SceneManager {
 	}
 
 	public void invalidateZone(Scene scene, int zx, int zz) {
-		WorldViewContext ctx = context(scene);
+		WorldViewContext ctx = getContext(scene);
 		if (ctx == null)
 			return;
 
 		Zone zone = ctx.zones[zx][zz];
-		if(zone.rebuild)
+		if (zone.rebuild)
 			return;
 
 		zone.rebuild = true;
@@ -323,17 +314,13 @@ public class SceneManager {
 	@Getter
 	private final JobGenericTask generateSceneDataTask = JobGenericTask.build(
 		"ProceduralGenerator::generateSceneData",
-		(task) -> {
-			proceduralGenerator.generateSceneData(nextSceneContext != null ? nextSceneContext : root.sceneContext);
-		}
+		(task) -> proceduralGenerator.generateSceneData(nextSceneContext != null ? nextSceneContext : root.sceneContext)
 	);
 
 	@Getter
 	private final JobGenericTask loadSceneLightsTask = JobGenericTask.build(
 		"lightManager::loadSceneLights",
-		(task) -> {
-			lightManager.loadSceneLights(nextSceneContext, root.sceneContext);
-		}
+		(task) -> lightManager.loadSceneLights(nextSceneContext, root.sceneContext)
 	);
 
 	private final JobGenericTask calculateRoofChangesTask = JobGenericTask.build(
@@ -581,7 +568,7 @@ public class SceneManager {
 		calculateRoofChangesTask.waitForCompletion();
 
 		WorldViewContext ctx = root;
-		if(!nextRoofChanges.isEmpty()) {
+		if (!nextRoofChanges.isEmpty()) {
 			for (int x = 0; x < ctx.sizeX; ++x) {
 				for (int z = 0; z < ctx.sizeZ; ++z) {
 					Zone zone = nextZones[x][z];
@@ -608,13 +595,13 @@ public class SceneManager {
 		long sceneUploadTimeStart = sw.elapsed(TimeUnit.NANOSECONDS);
 		int blockingCount = root.sceneLoadGroup.getPendingCount();
 		root.sceneLoadGroup.complete();
-		if(plugin.isInHouse)
+		if (plugin.isInHouse)
 			root.streamingGroup.complete();
 
 		int totalOpaque = 0;
 		int totalAlpha = 0;
-		for(int x = 0; x < NUM_ZONES; ++x) {
-			for(int z = 0; z < NUM_ZONES; ++z) {
+		for (int x = 0; x < NUM_ZONES; ++x) {
+			for (int z = 0; z < NUM_ZONES; ++z) {
 				totalOpaque += nextZones[x][z].bufLen;
 				totalAlpha += nextZones[x][z].bufLenA;
 			}
@@ -623,9 +610,15 @@ public class SceneManager {
 		root.uploadTime = sw.elapsed(TimeUnit.NANOSECONDS) - sceneUploadTimeStart;
 		log.debug(
 			"upload time {} reused {} deferred {} map {} sceneLoad {} len opaque {} size opaque {} KiB len alpha {} size alpha {} KiB",
-			TimeUnit.MILLISECONDS.convert(root.uploadTime, TimeUnit.NANOSECONDS), nextSceneContext.totalReused, nextSceneContext.totalDeferred, nextSceneContext.totalMapZones, blockingCount,
-			totalOpaque, ((long) totalOpaque * Zone.VERT_SIZE * 3) / KiB,
-			totalAlpha, ((long) totalAlpha * Zone.VERT_SIZE * 3) / KiB
+			TimeUnit.MILLISECONDS.convert(root.uploadTime, TimeUnit.NANOSECONDS),
+			nextSceneContext.totalReused,
+			nextSceneContext.totalDeferred,
+			nextSceneContext.totalMapZones,
+			blockingCount,
+			totalOpaque,
+			(totalOpaque * Zone.VERT_SIZE * 3L) / KiB,
+			totalAlpha,
+			(totalAlpha * Zone.VERT_SIZE * 3L) / KiB
 		);
 
 		for (int x = 0; x < ctx.sizeX; ++x) {
@@ -689,7 +682,7 @@ public class SceneManager {
 		final WorldViewContext ctx = new WorldViewContext(this, worldView, sceneContext, uboWorldViews);
 		subs[worldViewId] = ctx;
 
-		for(int x = 0; x <  ctx.sizeX; ++x)
+		for (int x = 0; x < ctx.sizeX; ++x)
 			for (int z = 0; z < ctx.sizeZ; ++z)
 				ZoneUploadTask.build(ctx, sceneContext, ctx.zones[x][z], x, z)
 					.setExecuteAsync(plugin.configZoneStreaming)
@@ -699,7 +692,7 @@ public class SceneManager {
 	}
 
 	private void swapSubScene(Scene scene) {
-		WorldViewContext ctx = context(scene);
+		WorldViewContext ctx = getContext(scene);
 		if (ctx == null)
 			return;
 
@@ -721,7 +714,7 @@ public class SceneManager {
 
 		public static SortedZone getZone(Zone zone, int x, int z, float dist) {
 			SortedZone sorted = POOL.poll();
-			if(sorted == null)
+			if (sorted == null)
 				sorted = new SortedZone();
 			sorted.zone = zone;
 			sorted.x = x;

src/main/java/rs117/hd/renderer/zone/SceneUploader.java
@@ -48,7 +48,6 @@ import rs117.hd.scene.water_types.WaterType;
 import rs117.hd.utils.HDUtils;
 import rs117.hd.utils.ModelHash;
 import rs117.hd.utils.buffer.GpuIntBuffer;
-import rs117.hd.utils.jobs.JobSystem;
 
 import static net.runelite.api.Constants.*;
 import static net.runelite.api.Perspective.*;
@@ -71,9 +70,6 @@ public class SceneUploader {
 	private HdPlugin plugin;
 
 	@Inject
-	private Client client;
-
-	@Inject
 	public GamevalManager gamevalManager;
 
 	@Inject
@@ -88,9 +84,6 @@ public class SceneUploader {
 	@Inject
 	public ProceduralGenerator proceduralGenerator;
 
-	@Inject
-	private JobSystem jobSystem;
-
 	private int basex, basez, rid, level;
 
 	private final Set<Integer> roofIds = new HashSet<>();
@@ -103,7 +96,7 @@ public class SceneUploader {
 	private int[][][] tileHeights;
 
 	private final int[] worldPos = new int[3];
-	private final int[][] vertices = new int[][] {new int[3], new int[3], new int[3], new int[3]};
+	private final int[][] vertices = new int[4][3];
 	private final int[] vertexKeys = new int[4];
 	private final float[] workingSpace = new float[9];
 	private final float[] modelUvs = new float[12];
@@ -118,7 +111,7 @@ public class SceneUploader {
 	private final int[] modelLocalZI = new int[MAX_VERTEX_COUNT];
 
 	public void setScene(Scene scene) {
-		if(scene == currentScene) {
+		if (scene == currentScene) {
 			return;
 		}
 
@@ -391,8 +384,8 @@ public class SceneUploader {
 			if (gameObject == null || !gameObject.getSceneMinLocation().equals(t.getSceneLocation()))
 				continue;
 
-			// Detect if GameObject is a temp by checking its hash
-			if(ModelHash.getIdOrIndex(gameObject.getHash()) == 0xFFFFFFFF)
+			// Detect if GameObject is a temp object
+			if (ModelHash.getIdOrIndex(gameObject.getHash()) == 0xFFFFFFFF)
 				continue;
 
 			ModelOverride modelOverride = modelOverrideManager.getOverride(gameObject, worldPos);
@@ -578,8 +571,8 @@ public class SceneUploader {
 			if (gameObject == null || !renderCallbackManager.drawObject(ctx.scene, gameObject))
 				continue;
 
-			// Detect if GameObject is a temp by checking its hash
-			if(ModelHash.getIdOrIndex(gameObject.getHash()) == 0xFFFFFFFF)
+			// Detect if GameObject is a temp object
+			if (ModelHash.getIdOrIndex(gameObject.getHash()) == 0xFFFFFFFF)
 				continue;
 
 			Point min = gameObject.getSceneMinLocation();
@@ -679,14 +672,15 @@ public class SceneUploader {
 			);
 		} catch (Throwable ex) {
 			log.warn(
-					"Error uploading static {} {} (ID {}), override=\"{}\", opaque={}, alpha={}",
-					ModelHash.getTypeName(ModelHash.getUuidType(uuid)),
-					gamevalManager.getObjectName(id),
-					id,
-					modelOverride.description,
-					opaqueBuffer,
-					alphaBuffer,
-					ex);
+				"Error uploading static {} {} (ID {}), override=\"{}\", opaque={}, alpha={}",
+				ModelHash.getTypeName(ModelHash.getUuidType(uuid)),
+				gamevalManager.getObjectName(id),
+				id,
+				modelOverride.description,
+				opaqueBuffer,
+				alphaBuffer,
+				ex
+			);
 		}
 
 		int alphaEnd = zone.vboA != null ? zone.vboA.vb.position() : 0;

src/main/java/rs117/hd/renderer/zone/VBO.java
@@ -56,7 +56,7 @@ public class VBO {
 		}
 		if (buf == null)
 			throw new RuntimeException("unable to map GL buffer " + bufId + " size " + size);
-		if(buf != mappedBuffer) {
+		if (buf != mappedBuffer) {
 			mappedBuffer = buf;
 			vb = mappedBuffer.asIntBuffer();
 		}

src/main/java/rs117/hd/renderer/zone/WorldViewContext.java
@@ -31,7 +31,12 @@ public class WorldViewContext {
 	final JobGroup<ZoneUploadTask> sceneLoadGroup = new JobGroup<>(true, false);
 	final JobGroup<ZoneUploadTask> streamingGroup = new JobGroup<>(false, false);
 
-	WorldViewContext(SceneManager sceneManager, @Nullable WorldView worldView, @Nullable ZoneSceneContext sceneContext, UBOWorldViews uboWorldViews) {
+	WorldViewContext(
+		SceneManager sceneManager,
+		@Nullable WorldView worldView,
+		@Nullable ZoneSceneContext sceneContext,
+		UBOWorldViews uboWorldViews
+	) {
 		this.sceneManager = sceneManager;
 		this.worldViewId = worldView == null ? -1 : worldView.getId();
 		this.sceneContext = sceneContext;
@@ -59,13 +64,13 @@ public class WorldViewContext {
 	void handleZoneSwap(float deltaTime, int zx, int zz) {
 		Zone curZone = zones[zx][zz];
 		ZoneUploadTask uploadTask = curZone.zoneUploadTask;
-		if(uploadTask == null)
+		if (uploadTask == null)
 			return;
 
-		if(!uploadTask.isQueued()) {
-			if(deltaTime > 0.0f && uploadTask.delay >= 0.0f) {
+		if (!uploadTask.isQueued()) {
+			if (deltaTime > 0.0f && uploadTask.delay >= 0.0f) {
 				uploadTask.delay -= deltaTime;
-				if(uploadTask.delay <= 0.0f) {
+				if (uploadTask.delay <= 0.0f) {
 					log.trace("queueing zone({}): [{}-{},{}]", uploadTask.zone.hashCode(), worldViewId, zx, zz);
 					uploadTask.delay = -1.0f;
 					uploadTask.queue(streamingGroup, sceneManager.getGenerateSceneDataTask());
@@ -74,9 +79,9 @@ public class WorldViewContext {
 			return;
 		}
 
-		if(uploadTask.isDone()) {
+		if (uploadTask.isDone()) {
 			curZone.zoneUploadTask = null;
-			if(uploadTask.ranToCompletion() && !uploadTask.wasCancelled()) {
+			if (uploadTask.ranToCompletion() && !uploadTask.wasCancelled()) {
 				log.trace("swapping zone({}): [{}-{},{}]", uploadTask.zone.hashCode(), worldViewId, zx, zz);
 
 				Zone PrevZone = curZone;
@@ -93,21 +98,21 @@ public class WorldViewContext {
 	}
 
 	boolean update(float deltaTime) {
-		if(isLoading)
+		if (isLoading)
 			return false;
 
 		Zone cullZone;
-		while((cullZone = pendingCull.poll()) != null) {
+		while ((cullZone = pendingCull.poll()) != null) {
 			log.trace("Culling zone({})", cullZone.hashCode());
 			cullZone.free();
 		}
 
 		boolean queuedWork = false;
-		for(int x = 0; x < sizeX; x++) {
-			for(int z = 0; z < sizeZ; z++) {
+		for (int x = 0; x < sizeX; x++) {
+			for (int z = 0; z < sizeZ; z++) {
 				handleZoneSwap(deltaTime, x, z);
 
-				if(zones[x][z].rebuild) {
+				if (zones[x][z].rebuild) {
 					zones[x][z].rebuild = false;
 					invalidateZone(x, z);
 					queuedWork = true;
@@ -135,7 +140,7 @@ public class WorldViewContext {
 				zones[x][z].free();
 
 		Zone cullZone;
-		while((cullZone = pendingCull.poll()) != null)
+		while ((cullZone = pendingCull.poll()) != null)
 			cullZone.free();
 
 		if (vboM != null)
@@ -155,8 +160,15 @@ public class WorldViewContext {
 	void invalidateZone(int zx, int zz) {
 		Zone curZone = zones[zx][zz];
 		float prevUploadDelay = -1.0f;
-		if(curZone.zoneUploadTask != null) {
-			log.trace("Invalidate Zone({}) - Cancelled upload task: [{}-{},{}] task zone({})", curZone.hashCode(), worldViewId, zx, zz, curZone.zoneUploadTask.zone.hashCode());
+		if (curZone.zoneUploadTask != null) {
+			log.trace(
+				"Invalidate Zone({}) - Cancelled upload task: [{}-{},{}] task zone({})",
+				curZone.hashCode(),
+				worldViewId,
+				zx,
+				zz,
+				curZone.zoneUploadTask.zone.hashCode()
+			);
 			prevUploadDelay = curZone.zoneUploadTask.delay;
 			curZone.zoneUploadTask.cancel();
 			curZone.zoneUploadTask.release();
@@ -167,7 +179,7 @@ public class WorldViewContext {
 
 		curZone.zoneUploadTask = ZoneUploadTask.build(this, sceneContext, newZone, zx, zz);
 		curZone.zoneUploadTask.delay = prevUploadDelay;
-		if(curZone.zoneUploadTask.delay < 0.0f)
+		if (curZone.zoneUploadTask.delay < 0.0f)
 			curZone.zoneUploadTask.queue(streamingGroup, sceneManager.getGenerateSceneDataTask());
 	}
 }

commit 389a3af087b251355c3fe9e0870285e2cc496295

Fix distance culling broken by 5972b8be

src/main/java/rs117/hd/utils/Camera.java
@@ -179,7 +179,10 @@ public class Camera {
 	}
 
 	public float squaredDistanceTo(float x, float y, float z) {
-		return position[0] * x + position[1] * y + position[2] * z;
+		float dx = position[0] - x;
+		float dy = position[1] - y;
+		float dz = position[2] - z;
+		return dx * dx + dy * dy + dz * dz;
 	}
 
 	public Camera translate(float[] translation) {