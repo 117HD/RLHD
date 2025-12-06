117HD/RLHD

commit 640c88825b1ccbfdaa9f39a187f8f47becb1fee8

Fix alpha models disappearing on upper planes
Whenever a boat was rendered, it was overwriting the variables for min &
max planes, causing the wrong planes to be checked for alpha models,
since they are drawn after opaque models on boats. This caused the tree
tops on Karamja to disappear at certain camera angles.

src/main/java/rs117/hd/renderer/zone/Zone.java
@@ -3,11 +3,11 @@ package rs117.hd.renderer.zone;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
-import java.util.Set;
 import javax.annotation.Nullable;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -290,10 +290,18 @@ public class Zone {
 		}
 	}
 
-	void renderOpaque(CommandBuffer cmd, int minLevel, int currentLevel, int maxLevel, Set<Integer> hiddenRoofIds) {
+	void renderOpaque(CommandBuffer cmd, WorldViewContext ctx, boolean roofShadows) {
 		drawIdx = 0;
 
-		for (int level = minLevel; level <= maxLevel; ++level) {
+		int currentLevel = ctx.level;
+		int maxLevel = ctx.maxLevel;
+		var hiddenRoofIds = ctx.hideRoofIds;
+		if (roofShadows) {
+			maxLevel = 3;
+			hiddenRoofIds = Collections.emptySet();
+		}
+
+		for (int level = ctx.minLevel; level <= maxLevel; ++level) {
 			int[] rids = this.rids[level];
 			int[] roofStart = this.roofStart[level];
 			int[] roofEnd = this.roofEnd[level];
@@ -607,16 +615,22 @@ public class Zone {
 		CommandBuffer cmd,
 		int zx,
 		int zz,
-		int minLevel,
-		int currentLevel,
-		int maxLevel,
 		int level,
+		WorldViewContext ctx,
 		Camera camera,
-		Set<Integer> hiddenRoofIds,
+		boolean roofShadows,
 		boolean useStaticUnsorted
 	) {
 		if (alphaModels.isEmpty())
 			return;
+
+		int currentLevel = ctx.level;
+		int maxLevel = ctx.maxLevel;
+		var hiddenRoofIds = ctx.hideRoofIds;
+		if (roofShadows) {
+			maxLevel = 3;
+			hiddenRoofIds = Collections.emptySet();
+		}
 
 		cmd.DepthMask(false);
 
@@ -630,7 +644,7 @@ public class Zone {
 			if ((m.flags & AlphaModel.SKIP) != 0 || m.level != level)
 				continue;
 
-			if (level < minLevel || level > maxLevel || level > currentLevel && hiddenRoofIds.contains((int) m.rid))
+			if (level < ctx.minLevel || level > maxLevel || level > currentLevel && hiddenRoofIds.contains((int) m.rid))
 				continue;
 
 			if (lastVao != m.vao || lastzx != (zx - m.zofx) || lastzz != (zz - m.zofz))

src/main/java/rs117/hd/renderer/zone/ZoneRenderer.java
@@ -26,7 +26,6 @@ package rs117.hd.renderer.zone;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Set;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
@@ -145,8 +144,6 @@ public class ZoneRenderer implements Renderer {
 	private final ShadowCasterVolume directionalShadowCasterVolume = new ShadowCasterVolume(directionalCamera);
 
 	private final int[] worldPos = new int[3];
-	private int minLevel, level, maxLevel;
-	private Set<Integer> hideRoofIds;
 
 	private final RenderState renderState = new RenderState();
 	private final CommandBuffer sceneCmd = new CommandBuffer(renderState);
@@ -268,13 +265,16 @@ public class ZoneRenderer implements Renderer {
 		float cameraX, float cameraY, float cameraZ, float cameraPitch, float cameraYaw,
 		int minLevel, int level, int maxLevel, Set<Integer> hideRoofIds
 	) {
-		this.minLevel = minLevel;
-		this.level = level;
-		this.maxLevel = maxLevel;
-		this.hideRoofIds = hideRoofIds;
-
 		WorldViewContext ctx = sceneManager.getContext(scene);
-		if (ctx != null && ctx.uboWorldViewStruct != null)
+		if (ctx == null)
+			return;
+
+		ctx.minLevel = minLevel;
+		ctx.level = level;
+		ctx.maxLevel = maxLevel;
+		ctx.hideRoofIds = hideRoofIds;
+
+		if (ctx.uboWorldViewStruct != null)
 			ctx.uboWorldViewStruct.update();
 
 		if (scene.getWorldViewId() == WorldView.TOPLEVEL) {
@@ -848,17 +848,11 @@ public class ZoneRenderer implements Renderer {
 			return;
 
 		if (!sceneManager.isRoot(ctx) || z.inSceneFrustum)
-			z.renderOpaque(sceneCmd, minLevel, level, maxLevel, hideRoofIds);
+			z.renderOpaque(sceneCmd, ctx, false);
 
 		if (!sceneManager.isRoot(ctx) || z.inShadowFrustum) {
 			directionalCmd.SetShader(fastShadowProgram);
-			z.renderOpaque(
-				directionalCmd,
-				minLevel,
-				level,
-				plugin.configRoofShadows ? 3 : maxLevel,
-				plugin.configRoofShadows ? Collections.emptySet() : hideRoofIds
-			);
+			z.renderOpaque(directionalCmd, ctx, plugin.configRoofShadows);
 		}
 
 		checkGLErrors();
@@ -900,12 +894,10 @@ public class ZoneRenderer implements Renderer {
 				sceneCmd,
 				zx - offset,
 				zz - offset,
-				minLevel,
-				this.level,
-				maxLevel,
 				level,
+				ctx,
 				sceneCamera,
-				hideRoofIds,
+				false,
 				useStaticUnsorted
 			);
 		}
@@ -913,15 +905,13 @@ public class ZoneRenderer implements Renderer {
 		if (!sceneManager.isRoot(ctx) || z.inShadowFrustum) {
 			directionalCmd.SetShader(plugin.configShadowMode == ShadowMode.DETAILED ? detailedShadowProgram : fastShadowProgram);
 			z.renderAlpha(
-				directionalCmd,
+				sceneCmd,
 				zx - offset,
 				zz - offset,
-				minLevel,
-				this.level,
-				plugin.configRoofShadows ? 3 : maxLevel,
 				level,
-				directionalCamera,
-				plugin.configRoofShadows ? Collections.emptySet() : hideRoofIds,
+				ctx,
+				sceneCamera,
+				plugin.configRoofShadows,
 				useStaticUnsorted
 			);
 		}
@@ -1089,7 +1079,7 @@ public class ZoneRenderer implements Renderer {
 			if (end > start) {
 				// level is checked prior to this callback being run, in order to cull clickboxes, but
 				// tileObject.getPlane()>maxLevel if visbelow is set - lower the object to the max level
-				int plane = Math.min(maxLevel, tileObject.getPlane());
+				int plane = Math.min(ctx.maxLevel, tileObject.getPlane());
 				// renderable modelheight is typically not set here because DynamicObject doesn't compute it on the returned model
 				zone.addTempAlphaModel(a.vao, start, end, plane, x & 1023, y, z & 1023);
 			}

src/main/java/rs117/hd/renderer/zone/WorldViewContext.java
@@ -1,5 +1,6 @@
 package rs117.hd.renderer.zone;
 
+import java.util.Set;
 import java.util.concurrent.LinkedBlockingDeque;
 import javax.annotation.Nullable;
 import lombok.extern.slf4j.Slf4j;
@@ -22,6 +23,9 @@ public class WorldViewContext {
 	Zone[][] zones;
 	VBO vboM;
 	boolean isLoading = true;
+
+	int minLevel, level, maxLevel;
+	Set<Integer> hideRoofIds;
 
 	public long loadTime;
 	public long uploadTime;

commit 2373ea44ba0cb96f07446b4f617b89c59aba6940

Fix alpha shadows broken by 640c88825b1ccbfdaa9f39a187f8f47becb1fee8

src/main/java/rs117/hd/renderer/zone/Zone.java
@@ -619,7 +619,7 @@ public class Zone {
 		WorldViewContext ctx,
 		Camera camera,
 		boolean roofShadows,
-		boolean useStaticUnsorted
+		boolean skipSorting
 	) {
 		if (alphaModels.isEmpty())
 			return;
@@ -661,7 +661,7 @@ public class Zone {
 				continue;
 			}
 
-			if (useStaticUnsorted) {
+			if (skipSorting) {
 				lastDrawMode = STATIC_UNSORTED;
 				pushRange(m.startpos, m.endpos);
 				continue;

src/main/java/rs117/hd/renderer/zone/ZoneRenderer.java
@@ -882,7 +882,7 @@ public class ZoneRenderer implements Renderer {
 		int dx = (int) plugin.cameraPosition[0] - ((zx - offset) << 10);
 		int dz = (int) plugin.cameraPosition[2] - ((zz - offset) << 10);
 		// If the zone is at sea, allow incorrect alpha ordering in the distance, for areas like north of Prifddinas
-		boolean useStaticUnsorted = z.onlyWater && dx * dx + dz * dz > ALPHA_ZSORT_CLOSE * ALPHA_ZSORT_CLOSE;
+		boolean skipSorting = z.onlyWater && dx * dx + dz * dz > ALPHA_ZSORT_CLOSE * ALPHA_ZSORT_CLOSE;
 
 		if (level == 0) {
 			z.alphaSort(zx - offset, zz - offset, sceneCamera);
@@ -890,30 +890,12 @@ public class ZoneRenderer implements Renderer {
 		}
 
 		if (!sceneManager.isRoot(ctx) || z.inSceneFrustum) {
-			z.renderAlpha(
-				sceneCmd,
-				zx - offset,
-				zz - offset,
-				level,
-				ctx,
-				sceneCamera,
-				false,
-				useStaticUnsorted
-			);
+			z.renderAlpha(sceneCmd, zx - offset, zz - offset, level, ctx, sceneCamera, false, skipSorting);
 		}
 
 		if (!sceneManager.isRoot(ctx) || z.inShadowFrustum) {
 			directionalCmd.SetShader(plugin.configShadowMode == ShadowMode.DETAILED ? detailedShadowProgram : fastShadowProgram);
-			z.renderAlpha(
-				sceneCmd,
-				zx - offset,
-				zz - offset,
-				level,
-				ctx,
-				sceneCamera,
-				plugin.configRoofShadows,
-				useStaticUnsorted
-			);
+			z.renderAlpha(directionalCmd, zx - offset, zz - offset, level, ctx, directionalCamera, plugin.configRoofShadows, skipSorting);
 		}
 
 		checkGLErrors();