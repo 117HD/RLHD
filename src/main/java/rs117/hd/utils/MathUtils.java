a4865aa7f28f044c393eade327aa30c0b80c6e41
Ruffled <105522716+RuffledPlume@users.noreply.github.com>
2025-12-26 11:55:10 +0000

    Added Normalized value fast path for fp32 -> fp16 conversion

--- src/main/java/rs117/hd/utils/MathUtils.java
+++ src/main/java/rs117/hd/utils/MathUtils.java
@@ -727,7 +727,7 @@ public final class MathUtils {
 	}
 
 	public static int float16(float value) {
-		if (value == 0)
+		if(value == 0.0f)
 			return 0;
 		// float32: (-1)^sign * 2^(exponent - 127) * (1.mantissa)
 		// float16: (-1)^sign * 2^(exponent -  15) * (1.mantissa)
@@ -735,6 +735,13 @@ public final class MathUtils {
 		final int sign = (f >>> 16) & 0x8000;
 		int exponent = ((f >>> 23) & 0xFF) - 127 + 15;
 		int mantissa = f & 0x7FFFFF;
+
+		// Normalized range fast path
+		if (exponent > 0 && exponent < 0x1F) {
+			// round-to-nearest-even
+			mantissa += 0x1000;
+			return sign | (exponent << 10) | (mantissa >> 13);
+		}
 
 		if (exponent <= 0) { // Too small, subnormal
 			if (exponent < -10) // To small to represent, return signed zero
