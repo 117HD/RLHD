601dc577fd23d62bdab01a52e6e901fc61143542
SirFancyBacon <m.johnsondelta@gmail.com>
2026-01-07 13:21:39 -0500

    make automatic code actually select spring

--- src/main/java/rs117/hd/HdPlugin.java
+++ src/main/java/rs117/hd/HdPlugin.java
@@ -1595,6 +1595,11 @@ public class HdPlugin extends Plugin {
 
 			if (configSeasonalHemisphere == SeasonalHemisphere.NORTHERN) {
 				switch (time.getMonth()) {
+					case MARCH:
+					case APRIL:
+					case MAY:
+						configSeasonalTheme = SeasonalTheme.SPRING;
+						break;
 					case SEPTEMBER:
 					case OCTOBER:
 					case NOVEMBER:
@@ -1620,6 +1625,11 @@ public class HdPlugin extends Plugin {
 					case JULY:
 					case AUGUST:
 						configSeasonalTheme = SeasonalTheme.WINTER;
+						break;
+					case SEPTEMBER:
+					case OCTOBER:
+					case NOVEMBER:
+						configSeasonalTheme = SeasonalTheme.SPRING;
 						break;
 					default:
 						configSeasonalTheme = SeasonalTheme.SUMMER;
