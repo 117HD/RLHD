ce248be5c00a238683dc5f679584d3155abf50b3
Ruffled <105522716+RuffledPlume@users.noreply.github.com>
2026-02-26 20:38:58 +0000

    Move APPLE check to initialize so that the check is ready to be read

--- src/main/java/rs117/hd/opengl/shader/DepthShaderProgram.java
+++ src/main/java/rs117/hd/opengl/shader/DepthShaderProgram.java
@@ -5,14 +5,18 @@ import rs117.hd.HdPlugin;
 import static org.lwjgl.opengl.GL20.GL_FRAGMENT_SHADER;
 import static org.lwjgl.opengl.GL20C.GL_FRAGMENT_SHADER_DERIVATIVE_HINT;
 import static org.lwjgl.opengl.GL20C.GL_VERTEX_SHADER;
+import static rs117.hd.HdPlugin.GL_CAPS;
 import static rs117.hd.renderer.zone.ZoneRenderer.TEXTURE_UNIT_TEXTURED_FACES;
 
 public class DepthShaderProgram extends ShaderProgram {
 	public DepthShaderProgram() {
-		super(t -> {
-			t.add(GL_VERTEX_SHADER, "depth_vert.glsl");
-			if(HdPlugin.APPLE)
-				t.add(GL_FRAGMENT_SHADER, "depth_frag.glsl");
-		});
+		super(t -> t.add(GL_VERTEX_SHADER, "depth_vert.glsl"));
+	}
+
+	@Override
+	protected void initialize() {
+		super.initialize();
+		if(HdPlugin.APPLE || !GL_CAPS.OpenGL46)
+			shaderTemplate.add(GL_FRAGMENT_SHADER, "depth_frag.glsl");
 	}
 }
--- src/main/java/rs117/hd/opengl/shader/OcclusionShaderProgram.java
+++ src/main/java/rs117/hd/opengl/shader/OcclusionShaderProgram.java
@@ -4,21 +4,28 @@ import rs117.hd.HdPlugin;
 
 import static org.lwjgl.opengl.GL20.GL_FRAGMENT_SHADER;
 import static org.lwjgl.opengl.GL20C.GL_VERTEX_SHADER;
+import static rs117.hd.HdPlugin.GL_CAPS;
 
 public class OcclusionShaderProgram extends ShaderProgram {
 	public OcclusionShaderProgram() {
-		super(t -> {
-			t.add(GL_VERTEX_SHADER, "occlusion_vert.glsl");
-			if(HdPlugin.APPLE)
-				t.add(GL_FRAGMENT_SHADER, "depth_frag.glsl");
-		});
+		super(t -> t.add(GL_VERTEX_SHADER, "occlusion_vert.glsl"));
+	}
+
+	@Override
+	protected void initialize() {
+		super.initialize();
+		if(HdPlugin.APPLE || !GL_CAPS.OpenGL46)
+			shaderTemplate.add(GL_FRAGMENT_SHADER, "depth_frag.glsl");
 	}
 
 	public static class Debug extends OcclusionShaderProgram {
 		public Uniform1i queryId = addUniform1i("queryId");
 
-		public Debug() {
-			shaderTemplate.remove(GL_FRAGMENT_SHADER).add(GL_FRAGMENT_SHADER, "occlusion_debug_frag.glsl");
+		@Override
+		protected void initialize() {
+			super.initialize();
+			shaderTemplate.remove(GL_FRAGMENT_SHADER);
+			shaderTemplate.add(GL_FRAGMENT_SHADER, "occlusion_debug_frag.glsl");
 		}
 	}
 }
