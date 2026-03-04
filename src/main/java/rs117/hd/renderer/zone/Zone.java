347e2e405ac87aeaf49e616a9a90a6eb23ef15d8
Ruffled <105522716+RuffledPlume@users.noreply.github.com>
2026-03-04 12:15:00 +0000

    Accidental paste error fix

--- src/main/java/rs117/hd/renderer/zone/Zone.java
+++ src/main/java/rs117/hd/renderer/zone/Zone.java
@@ -461,7 +461,7 @@ public class Zone {
 		int dist;
 		int asyncSortIdx = -1;
 
-		static final int SKIP = 1; // temporary model is in a closer zonetotalSortedFaces
+		static final int SKIP = 1; // temporary model is in a closer zone
 		static final int TEMP = 2; // temporary model added to a closer zone
 		static final int SORT_COMPLETED = 4;
 
