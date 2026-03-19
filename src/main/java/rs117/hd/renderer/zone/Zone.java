117HD/RLHD

commit 466c6173e75ef7da59eeda0b3f7beb2a20547853

Fix texture toggles

src/main/java/rs117/hd/renderer/zone/Zone.java
@@ -447,22 +447,23 @@ class Zone {
 			if (color3[f] == -2)
 				continue;
 
-			Material material = modelOverride.baseMaterial;
 			int transparency = transparencies != null ? transparencies[f] & 0xFF : 0;
 			int textureId = faceTextures != null ? faceTextures[f] : -1;
-			boolean isTextured = textureId != -1;
-			if (isTextured) {
-				material = modelOverride.textureMaterial;
-				if (material == Material.NONE)
-					material = materialManager.fromVanillaTexture(textureId);
-			}
 
-			if (modelOverride.materialOverrides != null) {
-				var override = modelOverride.materialOverrides.get(material);
-				if (override != null)
-					material = override.textureMaterial;
-			}
-			if (modelOverride.colorOverrides != null) {
+			Material material = Material.NONE;
+			if (textureId != -1) {
+				if (modelOverride.textureMaterial != Material.NONE) {
+					material = modelOverride.textureMaterial;
+				} else {
+					material = materialManager.fromVanillaTexture(textureId);
+					if (modelOverride.materialOverrides != null) {
+						var override = modelOverride.materialOverrides.get(material);
+						if (override != null) {
+							material = override.textureMaterial;
+						}
+					}
+				}
+			} else if (modelOverride.colorOverrides != null) {
 				int ahsl = (0xFF - transparency) << 16 | color1[f];
 				for (var override : modelOverride.colorOverrides) {
 					if (override.ahslCondition.test(ahsl)) {

src/main/java/rs117/hd/renderer/legacy/LegacyModelPusher.java
@@ -315,27 +315,27 @@ public class LegacyModelPusher {
 			int[] faceColors = model.getFaceColors1();
 			byte[] faceTransparencies = model.getFaceTransparencies();
 			for (int face = 0; face < faceCount; face++) {
+				short textureId = isVanillaTextured ? faceTextures[face] : -1;
 				UvType uvType = UvType.GEOMETRY;
 				Material material = baseMaterial;
+				ModelOverride faceOverride = modelOverride;
 
-				short textureId = isVanillaTextured ? faceTextures[face] : -1;
 				if (textureId != -1) {
 					uvType = UvType.VANILLA;
-					material = textureMaterial;
-					if (material == Material.NONE)
+					if (textureMaterial != Material.NONE) {
+						material = textureMaterial;
+					} else {
 						material = materialManager.fromVanillaTexture(textureId);
-				}
-
-				ModelOverride faceOverride = modelOverride;
-				if (modelOverride.materialOverrides != null) {
-					var override = modelOverride.materialOverrides.get(material);
-					if (override != null) {
-						faceOverride = override;
-						material = faceOverride.textureMaterial;
+						if (modelOverride.materialOverrides != null) {
+							var override = modelOverride.materialOverrides.get(material);
+							if (override != null) {
+								faceOverride = override;
+								material = faceOverride.textureMaterial;
+							}
+						}
 					}
-				}
-				// Color overrides are heavy. Only apply them if the UVs will be cached or don't need caching
-				if (modelOverride.colorOverrides != null && (cacheUvData || !needsCaching)) {
+				} else if (modelOverride.colorOverrides != null && (cacheUvData || !needsCaching)) {
+					// Color overrides are heavy. Only apply them if the UVs will be cached or don't need caching
 					int ahsl = (faceTransparencies == null ? 0xFF : 0xFF - (faceTransparencies[face] & 0xFF)) << 16 | faceColors[face];
 					for (var override : modelOverride.colorOverrides) {
 						if (override.ahslCondition.test(ahsl)) {

src/main/java/rs117/hd/renderer/zone/FacePrioritySorter.java
@@ -406,7 +406,7 @@ class FacePrioritySorter {
 		boolean isVanillaUVMapped =
 			isVanillaTextured && // Vanilla UV mapped models don't always have sensible UVs for untextured faces
 			model.getTextureFaces() != null;
-		boolean isTextured = isVanillaTextured && faceTextures[face] != -1;
+		int textureId = isVanillaTextured ? faceTextures[face] : -1;
 
 		Material baseMaterial = modelOverride.baseMaterial;
 		Material textureMaterial = modelOverride.textureMaterial;
@@ -507,7 +507,7 @@ class FacePrioritySorter {
 		}
 
 		// HSL override is not applied to textured faces
-		if (overrideAmount > 0 && !isTextured) {
+		if (overrideAmount > 0 && textureId == -1) {
 			color1 = SceneUploader.interpolateHSL(color1, overrideHue, overrideSat, overrideLum, overrideAmount);
 			color2 = SceneUploader.interpolateHSL(color2, overrideHue, overrideSat, overrideLum, overrideAmount);
 			color3 = SceneUploader.interpolateHSL(color3, overrideHue, overrideSat, overrideLum, overrideAmount);
@@ -526,30 +526,29 @@ class FacePrioritySorter {
 			texC = triangleC;
 		}
 
+		int transparency = transparencies != null ? transparencies[face] & 0xFF : 0;
+
 		UvType uvType = UvType.GEOMETRY;
 		Material material = baseMaterial;
-
-		int transparency = transparencies != null ? transparencies[face] & 0xFF : 0;
-		int textureId = isVanillaTextured ? faceTextures[face] : -1;
-		if (textureId != -1) {
-			uvType = UvType.VANILLA;
-			material = textureMaterial;
-			if (material == Material.NONE)
-				material = materialManager.fromVanillaTexture(textureId);
-
-			color1 = color2 = color3 = 90;
-		}
-
 		ModelOverride faceOverride = modelOverride;
-		if (modelOverride.materialOverrides != null) {
-			var override = modelOverride.materialOverrides.get(material);
-			if (override != null) {
-				faceOverride = override;
-				material = faceOverride.textureMaterial;
+
+		if (textureId != -1) {
+			color1 = color2 = color3 = 90;
+			uvType = UvType.VANILLA;
+			if (textureMaterial != Material.NONE) {
+				material = textureMaterial;
+			} else {
+				material = materialManager.fromVanillaTexture(textureId);
+				if (modelOverride.materialOverrides != null) {
+					var override = modelOverride.materialOverrides.get(material);
+					if (override != null) {
+						faceOverride = override;
+						material = faceOverride.textureMaterial;
+					}
+				}
 			}
-		}
-		if (modelOverride.colorOverrides != null) {
-			int ahsl = (0xFF - transparency) << 16 | faceColors1[face];
+		} else if (modelOverride.colorOverrides != null) {
+			int ahsl = (0xFF - transparency) << 16 | color1;
 			for (var override : modelOverride.colorOverrides) {
 				if (override.ahslCondition.test(ahsl)) {
 					faceOverride = override;
@@ -565,8 +564,8 @@ class FacePrioritySorter {
 				uvType = isVanillaUVMapped && textureFaces[face] != -1 ? UvType.VANILLA : UvType.GEOMETRY;
 		}
 
-		isTextured = true; // Skip vanilla shading reversal in the shader, since we do it on the CPU
-		int materialData = material.packMaterialData(faceOverride, uvType, false, isTextured);
+		boolean keepShading = true; // Skip vanilla shading reversal in the shader, since we do it on the CPU
+		int materialData = material.packMaterialData(faceOverride, uvType, false, keepShading);
 
 		if (uvType == UvType.VANILLA) {
 			modelUvs[0] = modelLocalX[texA] - vx1;

src/main/java/rs117/hd/renderer/zone/SceneUploader.java
@@ -611,32 +611,34 @@ class SceneUploader {
 		} else if (r instanceof DynamicObject) {
 			model = ((DynamicObject) r).getModelZbuf();
 		}
+		if (model == null)
+			return;
 
-		if (model != null) {
-			try {
-				uploadStaticModel(
-					ctx, tile, model, modelOverride, uuid,
-					preOrientation, orient, x - basex, y, z - basez, opaqueBuffer, alphaBuffer
-				);
-			} catch (Throwable ex) {
-				throw new RuntimeException(
-					String.format(
-						"Error uploading static %s %s (ID %d), override=\"%s\", opaque=%s, alpha=%s",
-						ModelHash.getTypeName(ModelHash.getUuidType(uuid)),
-						gamevalManager.getObjectName(id),
-						id,
-						modelOverride.description,
-						opaqueBuffer,
-						alphaBuffer
-					),
-					ex
-				);
-			}
+		try {
+			uploadStaticModel(
+				ctx, tile, model, modelOverride, uuid,
+				preOrientation, orient,
+				x - basex, y, z - basez,
+				opaqueBuffer,
+				alphaBuffer
+			);
+		} catch (Throwable ex) {
+			throw new RuntimeException(
+				String.format(
+					"Error uploading static %s %s (ID %d), override=\"%s\", opaque=%s, alpha=%s",
+					ModelHash.getTypeName(ModelHash.getUuidType(uuid)),
+					gamevalManager.getObjectName(id),
+					id,
+					modelOverride.description,
+					opaqueBuffer,
+					alphaBuffer
+				),
+				ex
+			);
 		}
 
 		int alphaEnd = zone.vboA != null ? zone.vboA.vb.position() : 0;
 		if (alphaEnd > alphaStart) {
-			assert model != null;
 			if (lx > -1) {
 				lx -= basex >> 7;
 				lz -= basez >> 7;
@@ -1176,8 +1178,10 @@ class SceneUploader {
 		Model model,
 		ModelOverride modelOverride,
 		int uuid,
-		int preOrientation, int orientation, int x, int y, int z,
-		GpuIntBuffer opaqueBuffer, GpuIntBuffer alphaBuffer
+		int preOrientation, int orientation,
+		int x, int y, int z,
+		GpuIntBuffer opaqueBuffer,
+		GpuIntBuffer alphaBuffer
 	) {
 		final int triangleCount = model.getFaceCount();
 		final int vertexCount = model.getVerticesCount();
@@ -1236,8 +1240,8 @@ class SceneUploader {
 			isVanillaTextured && // Vanilla UV mapped models don't always have sensible UVs for untextured faces
 			model.getTextureFaces() != null;
 
-		Material baseMaterial = modelOverride.baseMaterial;
-		Material textureMaterial = modelOverride.textureMaterial;
+		final Material baseMaterial = modelOverride.baseMaterial;
+		final Material textureMaterial = modelOverride.textureMaterial;
 
 		int len = 0;
 		for (int face = 0; face < triangleCount; ++face) {
@@ -1280,18 +1284,10 @@ class SceneUploader {
 				texC = triangleC;
 			}
 
-			UvType uvType = UvType.GEOMETRY;
-			Material material = baseMaterial;
-
 			int transparency = transparencies != null ? transparencies[face] & 0xFF : 0;
 			int textureId = isVanillaTextured ? faceTextures[face] : -1;
 			boolean isTextured = textureId != -1;
 			if (isTextured) {
-				uvType = UvType.VANILLA;
-				material = textureMaterial;
-				if (material == Material.NONE)
-					material = materialManager.fromVanillaTexture(textureId);
-
 				// Without overriding the color for textured faces, vanilla shading remains pretty noticeable even after
 				// the approximate reversal above. Ardougne rooftops is a good example, where vanilla shading results in a
 				// weird-looking tint. The brightness clamp afterward is required to reduce the over-exposure introduced.
@@ -1385,16 +1381,27 @@ class SceneUploader {
 				}
 			}
 
+			UvType uvType = UvType.GEOMETRY;
+			Material material = baseMaterial;
 			ModelOverride faceOverride = modelOverride;
-			if (modelOverride.materialOverrides != null) {
-				var override = modelOverride.materialOverrides.get(material);
-				if (override != null) {
-					faceOverride = override;
-					material = faceOverride.textureMaterial;
+
+			if (isTextured) {
+				color1 = color2 = color3 = 90;
+				uvType = UvType.VANILLA;
+				if (textureMaterial != Material.NONE) {
+					material = textureMaterial;
+				} else {
+					material = materialManager.fromVanillaTexture(textureId);
+					if (modelOverride.materialOverrides != null) {
+						var override = modelOverride.materialOverrides.get(material);
+						if (override != null) {
+							faceOverride = override;
+							material = faceOverride.textureMaterial;
+						}
+					}
 				}
-			}
-			if (modelOverride.colorOverrides != null) {
-				int ahsl = (0xFF - transparency) << 16 | color1s[face];
+			} else if (modelOverride.colorOverrides != null) {
+				int ahsl = (0xFF - transparency) << 16 | color1;
 				for (var override : modelOverride.colorOverrides) {
 					if (override.ahslCondition.test(ahsl)) {
 						faceOverride = override;
@@ -1599,34 +1606,30 @@ class SceneUploader {
 				texC = triangleC;
 			}
 
-			UvType uvType = UvType.GEOMETRY;
-			Material material = baseMaterial;
-
 			int transparency = transparencies != null ? transparencies[face] & 0xFF : 0;
 			int textureId = isVanillaTextured ? faceTextures[face] : -1;
-			boolean isTextured = textureId != -1;
-			if (isTextured) {
-				uvType = UvType.VANILLA;
-				material = textureMaterial;
-				if (material == Material.NONE)
-					material = materialManager.fromVanillaTexture(textureId);
 
-				// Without overriding the color for textured faces, vanilla shading remains pretty noticeable even after
-				// the approximate reversal above. Ardougne rooftops is a good example, where vanilla shading results in a
-				// weird-looking tint. The brightness clamp afterward is required to reduce the over-exposure introduced.
-				color1 = color2 = color3 = 90;
-			}
-
+			UvType uvType = UvType.GEOMETRY;
+			Material material = baseMaterial;
 			ModelOverride faceOverride = modelOverride;
-			if (modelOverride.materialOverrides != null) {
-				var override = modelOverride.materialOverrides.get(material);
-				if (override != null) {
-					faceOverride = override;
-					material = faceOverride.textureMaterial;
+
+			if (textureId != -1) {
+				color1 = color2 = color3 = 90;
+				uvType = UvType.VANILLA;
+				if (textureMaterial != Material.NONE) {
+					material = textureMaterial;
+				} else {
+					material = materialManager.fromVanillaTexture(textureId);
+					if (modelOverride.materialOverrides != null) {
+						var override = modelOverride.materialOverrides.get(material);
+						if (override != null) {
+							faceOverride = override;
+							material = faceOverride.textureMaterial;
+						}
+					}
 				}
-			}
-			if (modelOverride.colorOverrides != null) {
-				int ahsl = (0xFF - transparency) << 16 | color1s[face];
+			} else if (modelOverride.colorOverrides != null) {
+				int ahsl = (0xFF - transparency) << 16 | color1;
 				for (var override : modelOverride.colorOverrides) {
 					if (override.ahslCondition.test(ahsl)) {
 						faceOverride = override;
@@ -1642,7 +1645,7 @@ class SceneUploader {
 					uvType = isVanillaUVMapped && textureFaces[face] != -1 ? UvType.VANILLA : UvType.GEOMETRY;
 			}
 
-			int materialData = material.packMaterialData(faceOverride, uvType, false, isTextured);
+			int materialData = material.packMaterialData(faceOverride, uvType, false, textureId != -1);
 
 			if (uvType == UvType.VANILLA) {
 				modelUvs[0] = modelLocalX[texA] - vx1;

src/main/java/rs117/hd/scene/model_overrides/ModelOverride.java
@@ -90,6 +90,7 @@ public class ModelOverride
 	public transient AhslPredicate ahslCondition;
 	public transient boolean hasTransparency;
 	public transient boolean mightHaveTransparency;
+	public transient boolean modifiesVanillaTexture;
 
 	@FunctionalInterface
 	public interface AhslPredicate {
@@ -135,6 +136,8 @@ public class ModelOverride
 			windDisplacementModifier = clamp(windDisplacementModifier, -3, 3);
 		}
 
+		modifiesVanillaTexture = textureMaterial.modifiesVanillaTexture;
+
 		boolean disableTextures = !plugin.configModelTextures && !forceMaterialChanges;
 		if (disableTextures) {
 			if (baseMaterial.modifiesVanillaTexture)
@@ -158,9 +161,13 @@ public class ModelOverride
 			for (var entry : materialOverrides.entrySet()) {
 				var override = entry.getValue();
 				override.normalize(plugin);
+				if (disableTextures && override.modifiesVanillaTexture)
+					continue;
 				mightHaveTransparency |= override.mightHaveTransparency;
 				normalized.put(entry.getKey(), override);
 			}
+			if (normalized.isEmpty())
+				normalized = null;
 			materialOverrides = normalized;
 		}
 
@@ -235,7 +242,8 @@ public class ModelOverride
 			areaOverrides,
 			ahslCondition,
 			hasTransparency,
-			mightHaveTransparency
+			mightHaveTransparency,
+			modifiesVanillaTexture
 		);
 	}
 

commit 483474da3729e89e15f77e3c9eb0220cbf4ca8d0

Fix `baseMaterial` model overrides for zone alpha models (#1014)
Fixes a bug introduced in 466c6173e75ef7da59eeda0b3f7beb2a20547853 where the `ModelOverride`'s `baseMaterial` wasn't being applied.

src/main/java/rs117/hd/renderer/zone/Zone.java
@@ -555,7 +555,7 @@ public class Zone implements Destructible {
 
 			ModelOverride faceOverride = modelOverride;
 
-			Material material = Material.NONE;
+			Material material = modelOverride.baseMaterial;
 			if (textureId != -1) {
 				if (modelOverride.textureMaterial != Material.NONE) {
 					material = modelOverride.textureMaterial;