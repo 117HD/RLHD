a43fa3c79aad7ddf4ce3872b3461f0f327f1921d
Ruffled <105522716+RuffledPlume@users.noreply.github.com>
2026-04-17 09:48:20 +0100

    Draw dynamic & temp models in order (#1038)
    * Draw Dynamic & Temp Models In Order
    
     * ClientThread will always request DrawIndex to ensure models are drawn in a sorted order as they came in via the RL API
     * RenderThread Models are drawn out of order
    
    * Added Extra Assert to ensure we dont get an invalid drawIdx
    
    * Cleanup
    
    * Minor changes
    
    * Replace `hasAlpha` with `Zone.AlphaModel` requested upfront instead
    
    * shouldSort should be driven purely based on RenderMode
    
    If a model can be drawn with alpha without sorting, then we should do so to save performance
    
    * Minor formatting

--- src/main/java/rs117/hd/renderer/zone/WorldViewContext.java
+++ src/main/java/rs117/hd/renderer/zone/WorldViewContext.java
@@ -125,7 +125,7 @@ public class WorldViewContext {
 			final var POOL = needsStaging ? DYNAMIC_MODEL_VAO_STAGING_POOL : DYNAMIC_MODEL_VAO_POOL;
 			for (int k = 0; k < FRAMES_IN_FLIGHT; k++) {
 				DynamicModelVAO dynamicModelVao = dynamicModelVaos[k][i] = POOL.acquire();
-				if (dynamicModelVao.vao == 0)
+				if (dynamicModelVao.getVao() == 0)
 					dynamicModelVao.initialize();
 				dynamicModelVao.bindMetadataVAO(vboM);
 			}
@@ -139,16 +139,15 @@ public class WorldViewContext {
 	}
 
 	DynamicModelVAO.View beginDraw(int type, int faces) {
-		assert type != VAO_PLAYER : "Players are drawn at specific indices, which can't be safely mixed with this";
 		return dynamicModelVaos[plugin.frame % FRAMES_IN_FLIGHT][type].beginDraw(faces);
 	}
 
-	DynamicModelVAO.View beginPlayerDraw(int playerDrawIndex, int faces) {
-		return dynamicModelVaos[plugin.frame % FRAMES_IN_FLIGHT][VAO_PLAYER].beginPlayerDraw(faces, playerDrawIndex);
+	DynamicModelVAO.View beginDraw(int type, int playerDrawIndex, int faces) {
+		return dynamicModelVaos[plugin.frame % FRAMES_IN_FLIGHT][type].beginDraw(playerDrawIndex, faces);
 	}
 
-	int obtainPlayerDrawIndex() {
-		return dynamicModelVaos[plugin.frame % FRAMES_IN_FLIGHT][VAO_PLAYER].obtainDrawIndex();
+	int obtainDrawIndex(int type) {
+		return dynamicModelVaos[plugin.frame % FRAMES_IN_FLIGHT][type].obtainDrawIndex();
 	}
 
 	void drawAll(int type, CommandBuffer cmd) {
