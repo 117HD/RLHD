d531589a60db3f42aa4fb736c490ff5f365fe507
Ruffled <105522716+RuffledPlume@users.noreply.github.com>
2026-07-29 18:39:32 +0100

    Disable depth writes for alpha models in zones that include water (#1145)

--- src/main/java/rs117/hd/renderer/zone/ZoneRenderer.java
+++ src/main/java/rs117/hd/renderer/zone/ZoneRenderer.java
@@ -978,20 +978,30 @@ public class ZoneRenderer implements Renderer {
 				}
 
 				if (!sceneManager.isRoot(ctx) || z.inSceneFrustum) {
-					// Write color without depth writes
-					sceneCmd.DepthMask(false);
-					sceneCmd.ColorMask(true, true, true, true);
+					if (renderWater) {
+						// Water is currently drawn with depth writes & depth testing enabled, and as such, alpha models and the water plane
+						// can Z-fight depending on draw order. To avoid alpha models above water causing the water surface to fail its
+						// depth test, we disable depth writes for alpha models and rely on correct back to front ordering of the zones
+						sceneCmd.DepthMask(false);
+						z.renderAlpha(sceneCmd, zx - offset, zz - offset, level, ctx, false, false);
+						sceneCmd.DepthMask(true);
+					} else {
+						// Draw alpha models in two passes, first blending colors correctly, then writing depth for subsequent opaque models
+						// to test against. This is necessary because opaque models on higher planes can be drawn later
 
-					z.renderAlpha(sceneCmd, zx - offset, zz - offset, level, ctx, false, false);
+						// Write color without depth writes
+						sceneCmd.DepthMask(false);
+						sceneCmd.ColorMask(true, true, true, true);
+						z.renderAlpha(sceneCmd, zx - offset, zz - offset, level, ctx, false, false);
 
-					// Write depth without color
-					sceneCmd.DepthMask(true);
-					sceneCmd.ColorMask(false, false, false, false);
+						// Write depth without color
+						sceneCmd.DepthMask(true);
+						sceneCmd.ColorMask(false, false, false, false);
+						z.renderAlpha(sceneCmd, zx - offset, zz - offset, level, ctx, true, false);
 
-					z.renderAlpha(sceneCmd, zx - offset, zz - offset, level, ctx, true, false);
-
-					// Restore color writes
-					sceneCmd.ColorMask(true, true, true, true);
+						// Restore color writes
+						sceneCmd.ColorMask(true, true, true, true);
+					}
 				}
 			}
 			frameTimer.end(Timer.DRAW_ZONE_ALPHA);
