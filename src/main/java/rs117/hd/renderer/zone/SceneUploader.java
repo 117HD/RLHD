117HD/RLHD

commit 5a83aa455a7e1f5ec9fee6a9635cc5316287a734

Add support for double-sided faces (#1133)
* Added Double Sided Faces for Static Models

* Add DoubleSidedFaces to TileOverride

Enabled DoubleSidedFaces in GOTR

* Support AlphaModel DoubleSidedFaces

Use `PooledArrayType.INT` and then clone to ensure we only allocate what is needed

* Define backfacing fix for GotR portal more precisely

* Add doubleSidedFaces to the leaves of Avium trees

* Adjust GotR backfacing fix to work with ground blending and fix incorrect material name

* Added DoubleSidedFaces to Material

* Fix Typo

* Added BackFaceBitSet so front faces are prioritised during drawing

BackFaces are drawn first & normal faces are offsetted by the the normal diameter to ensure they are drawn last & therefore over any backfaces they should occlude

* `AlphaModel::SortedFaces` is now borrowed from `PooledArrayType`

 * Borrows in `alphaStaticModelSort`
 * Releases in `postAlphaPass`

* Double Sided AlphaModel Optimization

 * PackedFaces now only contains data for sorted faces
 * `sortStaticModelFacesByDistance` no longer sorts backfaces since they dont require sorting, instead backfaces are filled into a unsorted list which is pre-appended to the sorted array so that they are drawn first

* UnsortedFaces is redundant whilst sorting

* Optimise backside bit check

* Fix Doublesided checks

* Support TileModels

* Enable on Grand Exchange Roof Tiles, Walls & Evergreen trees

* Cleanup

* Tag Shrub leafs as Double Sided

* Add doubleSidedFaces to tree leave textures

* Remove Evergreen Override in favour of material

* Infer FaceTextures as being Tagged as double sided due to materials

* Tag Ardougne Agility Balance Plank

* Tag Varrock rooftops

* Add doubleSidedFaces to the following:

- Magic tree stars
- More varrock roof peices

* Tag Wooden Roofs as double sided

* Tag sailing palm trees as double sided

* Use FaceCount instead of calculating `packedFaceCount`

* Formatting

* Fix Grand Exchange roof area

src/main/java/rs117/hd/renderer/zone/SceneUploader.java
@@ -402,6 +402,9 @@ public class SceneUploader implements AutoCloseable {
 				z.sizeF += 2;
 			} else {
 				z.onlyWater = false;
+
+				if (tileZ != 0 || override.doubleSidedFaces)
+					z.sizeO += 2;
 			}
 		}
 
@@ -434,6 +437,9 @@ public class SceneUploader implements AutoCloseable {
 				z.sizeF += len;
 			} else {
 				z.onlyWater = false;
+
+				if (tileZ != 0 || overlayOverride.doubleSidedFaces || underlayOverride.doubleSidedFaces)
+					z.sizeO += len;
 			}
 		}
 
@@ -701,6 +707,7 @@ public class SceneUploader implements AutoCloseable {
 
 	private void estimateRenderableSize(Zone z, Renderable r, ModelOverride modelOverride) {
 		boolean mightHaveTransparency = modelOverride.mightHaveTransparency;
+		boolean mightBeDoubleSided = modelOverride.mightBeDoubleSided;
 		Model m = null;
 		if (r instanceof Model) {
 			m = (Model) r;
@@ -715,11 +722,19 @@ public class SceneUploader implements AutoCloseable {
 
 		int faceCount = m.getFaceCount();
 		byte[] transparencies = m.getFaceTransparencies();
-		short[] faceTextures = m.getFaceTextures();
+		boolean isVanillaTextured = m.getFaceTextures() != null;
 		byte modelTransparency = m.getTransparency();
-		z.sizeO += faceCount;
+
 		z.sizeF += faceCount;
-		if (transparencies != null || faceTextures != null || modelTransparency != 0 || mightHaveTransparency)
+
+		mightBeDoubleSided |= isVanillaTextured;
+		if (mightBeDoubleSided)
+			faceCount *= 2; // sizeF remains the same, since the double-sided faces will reuse the textureFaceIdx
+
+		z.sizeO += faceCount;
+
+		mightHaveTransparency |= transparencies != null || isVanillaTextured || modelTransparency != 0;
+		if (mightHaveTransparency)
 			z.sizeA += faceCount;
 	}
 
@@ -1062,22 +1077,45 @@ public class SceneUploader implements AutoCloseable {
 			lx2, neHeight, lz2,
 			uvx, uvy, 0,
 			neNormals[0], neNormals[1], neNormals[2],
-			texturedFaceIdx
+			texturedFaceIdx, false
 		);
 
 		vb.putStaticVertex(
 			lx3, nwHeight, lz3,
 			uvx - uvcos, uvy - uvsin, 0,
 			nwNormals[0], nwNormals[1], nwNormals[2],
-			texturedFaceIdx
+			texturedFaceIdx, false
 		);
 
 		vb.putStaticVertex(
 			lx1, seHeight, lz1,
 			uvx + uvsin, uvy - uvcos, 0,
 			seNormals[0], seNormals[1], seNormals[2],
-			texturedFaceIdx
+			texturedFaceIdx, false
 		);
+
+		if (tileZ != 0 || override.doubleSidedFaces) {
+			vb.putStaticVertex(
+				lx1, seHeight, lz1,
+				uvx + uvsin, uvy - uvcos, 0,
+				-seNormals[0], -seNormals[1], -seNormals[2],
+				texturedFaceIdx, true
+			);
+
+			vb.putStaticVertex(
+				lx3, nwHeight, lz3,
+				uvx - uvcos, uvy - uvsin, 0,
+				-nwNormals[0], -nwNormals[1], -nwNormals[2],
+				texturedFaceIdx, true
+			);
+
+			vb.putStaticVertex(
+				lx2, neHeight, lz2,
+				uvx, uvy, 0,
+				-neNormals[0], -neNormals[1], -neNormals[2],
+				texturedFaceIdx, true
+			);
+		}
 
 		texturedFaceIdx = tb.putFace(
 			swColor, seColor, nwColor,
@@ -1089,22 +1127,46 @@ public class SceneUploader implements AutoCloseable {
 			lx0, swHeight, lz0,
 			uvx - uvcos + uvsin, uvy - uvsin - uvcos, 0,
 			swNormals[0], swNormals[1], swNormals[2],
-			texturedFaceIdx
+			texturedFaceIdx, false
 		);
 
 		vb.putStaticVertex(
 			lx1, seHeight, lz1,
 			uvx + uvsin, uvy - uvcos, 0,
 			seNormals[0], seNormals[1], seNormals[2],
-			texturedFaceIdx
+			texturedFaceIdx, false
 		);
 
 		vb.putStaticVertex(
 			lx3, nwHeight, lz3,
 			uvx - uvcos, uvy - uvsin, 0,
 			nwNormals[0], nwNormals[1], nwNormals[2],
-			texturedFaceIdx
+			texturedFaceIdx, false
 		);
+
+		if (tileZ != 0 || override.doubleSidedFaces) {
+			vb.putStaticVertex(
+				lx3, nwHeight, lz3,
+				uvx - uvcos, uvy - uvsin, 0,
+				-nwNormals[0], -nwNormals[1], -nwNormals[2],
+				texturedFaceIdx, true
+			);
+
+			vb.putStaticVertex(
+				lx1, seHeight, lz1,
+				uvx + uvsin, uvy - uvcos, 0,
+				-seNormals[0], -seNormals[1], -seNormals[2],
+				texturedFaceIdx, true
+			);
+
+			vb.putStaticVertex(
+				lx0, swHeight, lz0,
+				uvx - uvcos + uvsin, uvy - uvsin - uvcos, 0,
+				-swNormals[0], -swNormals[1], -swNormals[2],
+				texturedFaceIdx, true
+			);
+		}
+
 
 		writeCache.release();
 	}
@@ -1384,22 +1446,45 @@ public class SceneUploader implements AutoCloseable {
 				lx0, ly0, lz0,
 				uvAx, uvAy, 0,
 				normalsA[0], normalsA[1], normalsA[2],
-				texturedFaceIdx
+				texturedFaceIdx, false
 			);
 
 			vb.putStaticVertex(
 				lx1, ly1, lz1,
 				uvBx, uvBy, 0,
 				normalsB[0], normalsB[1], normalsB[2],
-				texturedFaceIdx
+				texturedFaceIdx, false
 			);
 
 			vb.putStaticVertex(
 				lx2, ly2, lz2,
 				uvCx, uvCy, 0,
 				normalsC[0], normalsC[1], normalsC[2],
-				texturedFaceIdx
+				texturedFaceIdx, false
 			);
+
+			if (tileZ != 0 || override.doubleSidedFaces) {
+				vb.putStaticVertex(
+					lx2, ly2, lz2,
+					uvCx, uvCy, 0,
+					-normalsC[0], -normalsC[1], -normalsC[2],
+					texturedFaceIdx, true
+				);
+
+				vb.putStaticVertex(
+					lx1, ly1, lz1,
+					uvBx, uvBy, 0,
+					-normalsB[0], -normalsB[1], -normalsB[2],
+					texturedFaceIdx, true
+				);
+
+				vb.putStaticVertex(
+					lx0, ly0, lz0,
+					uvAx, uvAy, 0,
+					-normalsA[0], -normalsA[1], -normalsA[2],
+					texturedFaceIdx, true
+				);
+			}
 		}
 		writeCache.release();
 	}
@@ -1750,22 +1835,45 @@ public class SceneUploader implements AutoCloseable {
 				vx1, vy1, vz1,
 				faceUVs[0], faceUVs[1], faceUVs[2],
 				modelNormals[0], modelNormals[1], modelNormals[2],
-				texturedFaceIdx
+				texturedFaceIdx, false
 			);
 
 			vb.putStaticVertex(
 				vx2, vy2, vz2,
 				faceUVs[4], faceUVs[5], faceUVs[6],
 				modelNormals[3], modelNormals[4], modelNormals[5],
-				texturedFaceIdx
+				texturedFaceIdx, false
 			);
 
 			vb.putStaticVertex(
 				vx3, vy3, vz3,
 				faceUVs[8], faceUVs[9], faceUVs[10],
 				modelNormals[6], modelNormals[7], modelNormals[8],
-				texturedFaceIdx
+				texturedFaceIdx, false
 			);
+
+			if (faceOverride.doubleSidedFaces || material.doubleSidedFaces) {
+				vb.putStaticVertex(
+					vx3, vy3, vz3,
+					faceUVs[8], faceUVs[9], faceUVs[10],
+					-modelNormals[6], -modelNormals[7], -modelNormals[8],
+					texturedFaceIdx, true
+				);
+
+				vb.putStaticVertex(
+					vx2, vy2, vz2,
+					faceUVs[4], faceUVs[5], faceUVs[6],
+					-modelNormals[3], -modelNormals[4], -modelNormals[5],
+					texturedFaceIdx, true
+				);
+
+				vb.putStaticVertex(
+					vx1, vy1, vz1,
+					faceUVs[0], faceUVs[1], faceUVs[2],
+					-modelNormals[0], -modelNormals[1], -modelNormals[2],
+					texturedFaceIdx, true
+				);
+			}
 			len += 3;
 		}
 		writeCache.release();
@@ -2401,9 +2509,9 @@ public class SceneUploader implements AutoCloseable {
 			packedMaterial, packedMaterial, packedMaterial,
 			terrainData, terrainData, terrainData
 		);
-		vb.putStaticVertex(x0, y0, z0, u0, v0, 0, 0, -1, 0, faceIdx);
-		vb.putStaticVertex(x1, y1, z1, u1, v1, 0, 0, -1, 0, faceIdx);
-		vb.putStaticVertex(x2, y2, z2, u2, v2, 0, 0, -1, 0, faceIdx);
+		vb.putStaticVertex(x0, y0, z0, u0, v0, 0, 0, -1, 0, faceIdx, false);
+		vb.putStaticVertex(x1, y1, z1, u1, v1, 0, 0, -1, 0, faceIdx, false);
+		vb.putStaticVertex(x2, y2, z2, u2, v2, 0, 0, -1, 0, faceIdx, false);
 	}
 
 	public static void calculateFaceNormalInt(

src/main/java/rs117/hd/renderer/zone/EboAlphaWriterJob.java
@@ -20,14 +20,14 @@ public final class EboAlphaWriterJob extends Job {
 
 			Zone.AlphaModel m;
 			while ((m = alphaModels.poll()) != null) {
-				if (m.sortedFacesLen <= 0 || m.sortedFaces == null)
+				if (m.sortedFacesLen <= 0 || m.tempSortedFaces == null)
 					continue;
 
 				if (eboAlphaBuffer.remaining() < m.sortedFacesLen) {
 					log.warn("Not enough space in eboAlphaBuffer for alpha faces");
 					break;
 				}
-				eboAlphaBuffer.put(m.sortedFaces, 0, m.sortedFacesLen);
+				eboAlphaBuffer.put(m.tempSortedFaces, 0, m.sortedFacesLen);
 			}
 		} finally {
 			eboAlphaView = null;

src/main/java/rs117/hd/renderer/zone/FacePrioritySorter.java
@@ -210,11 +210,13 @@ public final class FacePrioritySorter implements AutoCloseable {
 		if (diameter >= MAX_DIAMETER)
 			return;
 
-		final int faceCount = m.packedFaces.length;
-		ensureCapacity(diameter, faceCount);
+		final int start = m.startpos / (VERT_SIZE >> 2);
+		final int maxFaceCount = m.packedFaces.length + m.doubleSidedCount;
+		ensureCapacity(diameter, maxFaceCount);
 
 		final int[] packedFaces = m.packedFaces;
-		final int[] sortedFaces = m.sortedFaces;
+		final int[] doubleSidedBitSet = m.doubleSidedBitSet;
+		final int[] sortedFaces = m.tempSortedFaces;
 		final int[] zsortHead = this.zsortHead;
 		final int[] zsortTail = this.zsortTail;
 		final int[] zsortNext = this.zsortNext;
@@ -222,8 +224,11 @@ public final class FacePrioritySorter implements AutoCloseable {
 		Arrays.fill(zsortHead, 0, diameter, -1);
 		Arrays.fill(zsortTail, 0, diameter, -1);
 
+		int sortedOffset = 0;
+		int backfaceWord = 0;
 		int minFz = diameter, maxFz = 0;
-		for (int i = 0; i < faceCount; ++i) {
+
+		for (int i = 0, f = 0; i < m.packedFaces.length; ++i, ++f) {
 			final int packed = packedFaces[i];
 			final short x = (short) (packed >> 21);
 			final short y = (short) ((packed << 11) >> 22);
@@ -236,35 +241,43 @@ public final class FacePrioritySorter implements AutoCloseable {
 
 			final int tailFaceIdx = zsortTail[fz];
 			if (tailFaceIdx == -1) {
-				zsortHead[fz] = i;
-				zsortTail[fz] = i;
+				zsortHead[fz] = f;
+				zsortTail[fz] = f;
 				minFz = min(minFz, fz);
 				maxFz = max(maxFz, fz);
 			} else {
-				zsortNext[tailFaceIdx] = i;
-				zsortTail[fz] = i;
+				zsortNext[tailFaceIdx] = f;
+				zsortTail[fz] = f;
 			}
+			zsortNext[f] = -1;
 
-			zsortNext[i] = -1;
+			// Backfaces are not sorted, so we skip over them if this face is marked as double-sided
+			if (doubleSidedBitSet != null) {
+				if ((i & 31) == 0)
+					backfaceWord = doubleSidedBitSet[i >> 5];
+
+				if ((backfaceWord & 1) != 0) {
+					final int faceStart = ++f * 3 + start;
+					sortedFaces[sortedOffset++] = faceStart;
+					sortedFaces[sortedOffset++] = faceStart + 1;
+					sortedFaces[sortedOffset++] = faceStart + 2;
+				}
+				backfaceWord >>>= 1;
+			}
 		}
 
-		final int start = m.startpos / (VERT_SIZE >> 2);
 		for (int i = maxFz; i >= minFz; --i) {
 			for (int f = zsortHead[i]; f != -1; f = zsortNext[f]) {
-				if (f >= faceCount)
+				if (f >= maxFaceCount)
 					continue;
 
-				final int sortedOffset = m.sortedFacesLen;
 				final int faceStart = f * 3 + start;
-				sortedFaces[sortedOffset] = faceStart;
-				sortedFaces[sortedOffset + 1] = faceStart + 1;
-				sortedFaces[sortedOffset + 2] = faceStart + 2;
-				m.sortedFacesLen += 3;
-
-				if (m.sortedFacesLen >= sortedFaces.length)
-					return;
+				sortedFaces[sortedOffset++] = faceStart;
+				sortedFaces[sortedOffset++] = faceStart + 1;
+				sortedFaces[sortedOffset++] = faceStart + 2;
 			}
 		}
+		m.sortedFacesLen = sortedOffset;
 	}
 
 	@Override

src/main/java/rs117/hd/renderer/zone/VertexWriteCache.java
@@ -106,7 +106,7 @@ public final class VertexWriteCache {
 		int x, int y, int z,
 		float u, float v, float w,
 		int nx, int ny, int nz,
-		int textureFaceIdx
+		int textureFaceIdx, boolean windingReversed
 	) {
 		if (stagingPosition + 7 > stagingBuffer.length)
 			flushAndGrow();
@@ -121,7 +121,7 @@ public final class VertexWriteCache {
 		// Unnormalized normals, assumed to be within short max
 		stagingBuffer[stagingPosition + 4] = (ny & 0xFFFF) << 16 | nx & 0xFFFF;
 		stagingBuffer[stagingPosition + 5] = nz & 0xFFFF;
-		stagingBuffer[stagingPosition + 6] = textureFaceIdx;
+		stagingBuffer[stagingPosition + 6] = (windingReversed ? 1 << 31 : 0) | textureFaceIdx;
 
 		this.stagingPosition += 7;
 	}

src/main/java/rs117/hd/renderer/zone/Zone.java
@@ -27,6 +27,7 @@ import rs117.hd.utils.buffer.GLTextureBuffer;
 import rs117.hd.utils.collections.ConcurrentPool;
 import rs117.hd.utils.collections.Int2IntHashMap;
 import rs117.hd.utils.collections.IntHashSet;
+import rs117.hd.utils.collections.PooledArrayType;
 
 import static net.runelite.api.Constants.*;
 import static org.lwjgl.opengl.GL33C.*;
@@ -426,11 +427,13 @@ public class Zone implements Destructible {
 		// only set for static geometry as they require sorting
 		int radius;
 		int[] packedFaces;
-		int[] sortedFaces;
-		int sortedFacesLen;
+		int[] doubleSidedBitSet;
+		char doubleSidedCount;
 
 		int dist;
 		int asyncSortIdx = -1;
+		int sortedFacesLen;
+		int[] tempSortedFaces;
 
 		static final int SKIP = 1; // temporary model is in a closer zone
 		static final int TEMP = 2; // temporary model added to a closer zone
@@ -445,7 +448,7 @@ public class Zone implements Destructible {
 		}
 
 		boolean isTemp() {
-			return packedFaces == null || sortedFaces == null;
+			return packedFaces == null;
 		}
 
 		int calculateDepth(int cx, int cy, int cz, int zx, int zz) {
@@ -557,9 +560,16 @@ public class Zone implements Destructible {
 			shift++;
 		}
 
-		int[] packedFaces = m.packedFaces = new int[(endpos - startpos) / ((3 * VERT_SIZE) >> 2)];
+		final int bucketCapacity = ceil(faceCount / 32.0f);
+
+		final int[] packedFaces = PooledArrayType.INT.borrow(faceCount);
+		final int[] doubleSidedBitSet = PooledArrayType.INT.borrow(bucketCapacity);
+
+		Arrays.fill(doubleSidedBitSet, 0, bucketCapacity, 0);
+
 		int radius = 0;
 		char bufferIdx = 0;
+		char doubleSidedCount = 0;
 		for (int f = 0; f < faceCount; ++f) {
 			if (color3[f] == -2)
 				continue;
@@ -606,26 +616,36 @@ public class Zone implements Destructible {
 			if (!hasAlpha)
 				continue;
 
-			int fx = (((int) (vertexX[indices1[f]] + vertexX[indices2[f]] + vertexX[indices3[f]]) / 3) - cx) >> shift;
-			int fy = (((int) (vertexY[indices1[f]] + vertexY[indices2[f]] + vertexY[indices3[f]]) / 3) - cy) >> shift;
-			int fz = (((int) (vertexZ[indices1[f]] + vertexZ[indices2[f]] + vertexZ[indices3[f]]) / 3) - cz) >> shift;
+			final int fx = (((int) (vertexX[indices1[f]] + vertexX[indices2[f]] + vertexX[indices3[f]]) / 3) - cx) >> shift;
+			final int fy = (((int) (vertexY[indices1[f]] + vertexY[indices2[f]] + vertexY[indices3[f]]) / 3) - cy) >> shift;
+			final int fz = (((int) (vertexZ[indices1[f]] + vertexZ[indices2[f]] + vertexZ[indices3[f]]) / 3) - cz) >> shift;
 
 			radius = Math.max(radius, fx * fx + fy * fy + fz * fz);
-
 			packedFaces[bufferIdx] = ((fx & ((1 << 11) - 1)) << 21)
-									 | ((fy & ((1 << 10) - 1)) << 11)
-									 | (fz & ((1 << 11) - 1));
+			                         | ((fy & ((1 << 10) - 1)) << 11)
+			                         | (fz & ((1 << 11) - 1));
+
+			if (faceOverride.doubleSidedFaces || material.doubleSidedFaces) {
+				doubleSidedBitSet[bufferIdx >> 5] |= 1 << (bufferIdx & 31);
+				doubleSidedCount++;
+			}
+
 			bufferIdx++;
 		}
-
-		m.radius = 2 + (int) Math.sqrt(radius);
-		m.sortedFaces = new int[bufferIdx * 3];
 
 		assert packedFaces.length > 0;
 		// Normally these will be equal, but transparency is used to hide faces in the TzHaar reskin
 		assert bufferIdx <= packedFaces.length : String.format("%d > %d", (int) bufferIdx, packedFaces.length);
 
+		m.radius = 2 + (int) Math.sqrt(radius);
+		m.packedFaces = Arrays.copyOf(packedFaces, bufferIdx);
+		m.doubleSidedBitSet = doubleSidedCount > 0 ? Arrays.copyOf(doubleSidedBitSet, ceil(bufferIdx / 32.0f)) : null;
+		m.doubleSidedCount = doubleSidedCount;
+
 		alphaModels.add(m);
+
+		PooledArrayType.INT.release(packedFaces);
+		PooledArrayType.INT.release(doubleSidedBitSet);
 	}
 
 	synchronized AlphaModel requestTempAlphaModel(ModelOverride modelOverride, int level, int x, int y, int z) {
@@ -649,14 +669,19 @@ public class Zone implements Destructible {
 
 		for (int i = alphaModels.size() - 1; i >= 0; --i) {
 			AlphaModel m = alphaModels.get(i);
+			m.asyncSortIdx = -1;
+			m.flags &= ~(AlphaModel.SKIP | AlphaModel.SORT_COMPLETED);
+
 			if (m.isTemp() || (m.flags & AlphaModel.TEMP) != 0) {
 				alphaModels.remove(i);
 				m.packedFaces = null;
-				m.sortedFaces = null;
+				m.doubleSidedBitSet = null;
 				ALPHA_MODEL_POOL.recycle(m);
 			}
-			m.asyncSortIdx = -1;
-			m.flags &= ~(AlphaModel.SKIP | AlphaModel.SORT_COMPLETED);
+
+			if (m.tempSortedFaces != null)
+				PooledArrayType.INT.release(m.tempSortedFaces);
+			m.tempSortedFaces = null;
 		}
 	}
 
@@ -708,6 +733,7 @@ public class Zone implements Destructible {
 				continue;
 
 			m.dist = dist;
+			m.tempSortedFaces = PooledArrayType.INT.borrow((m.packedFaces.length + m.doubleSidedCount) * 3);
 			alphaSortingJob.addAlphaModel(m);
 		}
 		alphaSortingJob.queue(camera);
@@ -783,7 +809,7 @@ public class Zone implements Destructible {
 					alphaSortingJob.waitForCompletion(10);
 			}
 
-			if (m.sortedFaces == null || m.sortedFacesLen <= 0)
+			if (m.tempSortedFaces == null || m.sortedFacesLen <= 0)
 				continue;
 
 			sortedAlphaFacesUpload.alphaModels.add(m);
@@ -899,9 +925,11 @@ public class Zone implements Destructible {
 				m2.zofz = (byte) (closestZoneZ - zz);
 
 				m2.packedFaces = m.packedFaces;
+				m2.doubleSidedBitSet = m.doubleSidedBitSet;
 				m2.radius = m.radius;
+				m2.doubleSidedCount = m.doubleSidedCount;
 				m2.asyncSortIdx = m.asyncSortIdx;
-				m2.sortedFaces = m.sortedFaces;
+				m2.tempSortedFaces = m.tempSortedFaces;
 				m2.sortedFacesLen = m.sortedFacesLen;
 
 				m2.flags = AlphaModel.TEMP;

src/main/java/rs117/hd/scene/materials/Material.java
@@ -55,6 +55,7 @@ public class Material {
 	@JsonAdapter(Reference.Adapter.class)
 	private Material shadowAlphaMap;
 	public boolean hasTransparency;
+	public boolean doubleSidedFaces;
 	private boolean overrideBaseColor;
 	private boolean unlit;
 	@JsonAdapter(ColorUtils.LinearAdapter.class)

src/main/java/rs117/hd/scene/model_overrides/ModelOverride.java
@@ -75,6 +75,7 @@ public class ModelOverride
 	public boolean castShadows = true;
 	public boolean receiveShadows = true;
 	public boolean terrainVertexSnap = false;
+	public boolean doubleSidedFaces = false;
 	public boolean undoVanillaShading = true;
 	private boolean hideAsWaterEffect = false;
 	public float terrainVertexSnapThreshold = 0.125f;
@@ -118,8 +119,8 @@ public class ModelOverride
 	public transient boolean isGenerated;
 	public transient Map<AABB, ModelOverride> areaOverrides;
 	public transient AhslPredicate ahslCondition;
-	public transient boolean hasTransparency;
 	public transient boolean mightHaveTransparency;
+	public transient boolean mightBeDoubleSided;
 	public transient boolean modifiesVanillaTexture;
 
 	// Transient not volatile, since access order can be random as it'll mean we'll just fall back to the full lookup
@@ -210,11 +211,16 @@ public class ModelOverride
 		if (hideInAreas == null)
 			hideInAreas = new AABB[0];
 
-		hasTransparency = mightHaveTransparency =
+		mightHaveTransparency =
 			baseMaterial.hasTransparency ||
 			textureMaterial.hasTransparency ||
 			modifiesAlpha && minAlpha < 255 ||
 			tzHaarRecolorType != TzHaarRecolorType.NONE;
+
+		mightBeDoubleSided =
+			doubleSidedFaces ||
+			baseMaterial.doubleSidedFaces ||
+			textureMaterial.doubleSidedFaces;
 
 		hide |= hideAsWaterEffect && plugin.configHideVanillaWaterEffects;
 
@@ -226,6 +232,7 @@ public class ModelOverride
 				if (disableTextures && override.modifiesVanillaTexture)
 					continue;
 				mightHaveTransparency |= override.mightHaveTransparency;
+				mightBeDoubleSided |= override.mightBeDoubleSided;
 				normalized.put(entry.getKey(), override);
 			}
 			if (normalized.isEmpty())
@@ -237,6 +244,7 @@ public class ModelOverride
 			for (var override : colorOverrides) {
 				override.normalize(plugin);
 				mightHaveTransparency |= override.mightHaveTransparency;
+				mightBeDoubleSided |= override.mightBeDoubleSided;
 				override.ahslCondition = parseAhslConditions(override.colors);
 			}
 		}
@@ -301,6 +309,7 @@ public class ModelOverride
 			castShadows,
 			receiveShadows,
 			terrainVertexSnap,
+			doubleSidedFaces,
 			undoVanillaShading,
 			hideAsWaterEffect,
 			terrainVertexSnapThreshold,
@@ -338,8 +347,8 @@ public class ModelOverride
 			isGenerated,
 			areaOverrides,
 			ahslCondition,
-			hasTransparency,
 			mightHaveTransparency,
+			mightBeDoubleSided,
 			modifiesVanillaTexture,
 			// Runtime caching fields
 			-1

src/main/java/rs117/hd/scene/tile_overrides/TileOverride.java
@@ -49,6 +49,7 @@ public class TileOverride {
 	public boolean blendedAsOpposite;
 	public boolean forced;
 	public boolean depthTested;
+	public boolean doubleSidedFaces;
 	private int setHue = -1;
 	private int shiftHue;
 	private int minHue;

commit 0978ec2ec1745d6139b895acc4445789f45afc54

Fix water surface tile estimation mismatch
5a83aa455a7e1f5ec9fee6a9635cc5316287a734 accidentally uploaded double
faces also for the water surface on plane 0, which was not accounted for
in estimation.

src/main/java/rs117/hd/renderer/zone/SceneUploader.java
@@ -257,7 +257,7 @@ public class SceneUploader implements AutoCloseable {
 				uploadZoneWater(ctx, zone, mzx, mzz, vb, fb);
 			zone.levelOffsets[Zone.LEVEL_WATER_SURFACE] = vb.position();
 
-			if (ctx.fillGaps && zone.hasGapFiller)
+			if (zone.hasGapFiller)
 				uploadZoneGapFillers(ctx, mzx, mzz, vb, fb);
 			zone.levelOffsets[Zone.LEVEL_GAP_FILLER] = vb.position();
 		}
@@ -713,8 +713,10 @@ public class SceneUploader implements AutoCloseable {
 		} else if (r instanceof DynamicObject) {
 			var dynamic = (DynamicObject) r;
 			m = dynamic.getModelZbuf();
-			if (dynamic.getRecordedObjectComposition() != null)
+			if (dynamic.getRecordedObjectComposition() != null) {
 				mightHaveTransparency = true;
+				mightBeDoubleSided = true;
+			}
 		}
 		if (m == null)
 			return;
@@ -1093,7 +1095,8 @@ public class SceneUploader implements AutoCloseable {
 			texturedFaceIdx, false
 		);
 
-		if (tileZ != 0 || override.doubleSidedFaces) {
+		boolean isDoubleSided = !onlyWaterSurface && (tileZ != 0 || override.doubleSidedFaces);
+		if (isDoubleSided) {
 			vb.putStaticVertex(
 				lx1, seHeight, lz1,
 				uvx + uvsin, uvy - uvcos, 0,
@@ -1143,7 +1146,7 @@ public class SceneUploader implements AutoCloseable {
 			texturedFaceIdx, false
 		);
 
-		if (tileZ != 0 || override.doubleSidedFaces) {
+		if (isDoubleSided) {
 			vb.putStaticVertex(
 				lx3, nwHeight, lz3,
 				uvx - uvcos, uvy - uvsin, 0,
@@ -1165,7 +1168,6 @@ public class SceneUploader implements AutoCloseable {
 				texturedFaceIdx, true
 			);
 		}
-
 
 		writeCache.release();
 	}
@@ -1462,7 +1464,8 @@ public class SceneUploader implements AutoCloseable {
 				texturedFaceIdx, false
 			);
 
-			if (tileZ != 0 || override.doubleSidedFaces) {
+			boolean isDoubleSided = !onlyWaterSurface && (tileZ != 0 || override.doubleSidedFaces);
+			if (isDoubleSided) {
 				vb.putStaticVertex(
 					lx2, ly2, lz2,
 					uvCx, uvCy, 0,