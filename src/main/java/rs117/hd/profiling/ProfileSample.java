e87511374c8bc8b8597a670b980aec594759cfc0
Ruffled <105522716+RuffledPlume@users.noreply.github.com>
2026-09-16 11:08:35 +0100

    Memory Allocation Reduction

--- src/main/java/rs117/hd/profiling/ProfileSample.java
+++ src/main/java/rs117/hd/profiling/ProfileSample.java
@@ -3,10 +3,9 @@ package rs117.hd.profiling;
 import java.util.Arrays;
 
 public class ProfileSample {
-
 	public final long frameTimestamp;
-	public final long[] timers;
-	public final long[] allocations;
+	public final long[] timers = new long[Timer.TIMERS.length];
+	public final long[] allocations = new long[Timer.TIMERS.length];
 	public final Event[] events;
 	public final long totalAsyncTime;
 	public final float cpuLoad;
@@ -14,23 +13,13 @@ public class ProfileSample {
 	public final long freeSystemMemoryKB;
 	public final long gpuUsageKB;
 
-	public ProfileSample(long frameTimestamp, long[] timers, long[] allocations, Event[] events, int eventCount, float cpuLoad, long heapUsageKB, long freeSystemMemoryKB, long gpuUsageKB) {
+	public ProfileSample(long frameTimestamp, long totalAsyncTime, Event[] events, int eventCount, float cpuLoad, long heapUsageKB, long freeSystemMemoryKB, long gpuUsageKB) {
 		this.frameTimestamp = frameTimestamp;
-		this.timers = Arrays.copyOf(timers, timers.length);
-		this.allocations = Arrays.copyOf(allocations, allocations.length);
+		this.totalAsyncTime = totalAsyncTime;
 		this.events = eventCount > 0 ? Arrays.copyOf(events, eventCount) : null;
-		this.totalAsyncTime = calculateTotalAsyncTime(timers);
 		this.cpuLoad = cpuLoad;
 		this.heapUsageKB = heapUsageKB;
 		this.freeSystemMemoryKB = freeSystemMemoryKB;
 		this.gpuUsageKB = gpuUsageKB;
-	}
-
-	private static long calculateTotalAsyncTime(long[] timers) {
-		long asyncTime = 0;
-		for(int i = 0; i < timers.length; i++)
-			if(Timer.TIMERS[i].isAsyncCpuTimer())
-				asyncTime += timers[i];
-		return asyncTime;
 	}
 }
